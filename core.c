44f06a0ed7ecebbe9fbe32db2dd8c1cbae8a0a73
SirMathhman <sirmathhman@gmail.com>
2020-09-11 15:17:35 -0400

    Add core

---
+++ core.c
@@ -0,0 +1,5 @@
+//
+// Created by SirMathhman on 9/11/2020.
+//
+
+#include "core.h"
---
+++ core.h
@@ -0,0 +1,8 @@
+//
+// Created by SirMathhman on 9/11/2020.
+//
+
+#ifndef MAGMA_CORE_H
+#define MAGMA_CORE_H
+
+#endif //MAGMA_CORE_H
