a356e81c061e82278d884fa4de3ede4815bbcf67
SirMathhman <sirmathhman@gmail.com>
2020-10-04 14:21:27 -0400

    Rename

--- core.c
+++ core.c
@@ -5,17 +5,17 @@
 #include "core.h"
 
 Exception thrown_ = {null, null};
-Bool wasThrown = false;
+Bool wasThrown_ = false;
 
 Exception throw_(Exception exception) {
     Exception previous = thrown_;
     thrown_ = exception;
-    wasThrown = true;
+    wasThrown_ = true;
     return previous;
 }
 
 void *catch_(void *(*action)(Exception *)) {
-    if (wasThrown) {
+    if (wasThrown_) {
         return action(&thrown_);
     } else {
         return null;
