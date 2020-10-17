4afdf044ef1200a93c5351b6a49d1610245a8c6c
SirMathhman <sirmathhman@gmail.com>
2020-10-17 14:26:43 -0400

    Add Option system

--- app.c
+++ app.c
@@ -7,3 +7,37 @@
 int run(int argc, char **argv) {
     return 0;
 }
+
+Any* thrown_;
+
+void throw(Any *value) {
+    thrown_=value;
+}
+
+Option Option_(Any *super, void *(*orElse)(struct Option *, Any *)) {
+    Option result = {super, orElse};
+    return result;
+}
+
+Any* Some_orElse(Option* this, Any* other){
+    Some* super = this->super;
+    return super->value;
+}
+
+Option Option_Some(Some* this) {
+    return Option_(this, Some_orElse);
+}
+
+Some Some_(Any *value) {
+    Some result = {value, Option_Some};
+    return result;
+}
+
+Option catch() {
+    Some some = Some_(thrown_);
+    return some.Option(&some);
+}
+
+
+
+
--- app.h
+++ app.h
@@ -5,6 +5,27 @@
 #ifndef MAGMA_APP_H
 #define MAGMA_APP_H
 
+typedef void Any;
+typedef int bool;
+
 int run(int argc, char **argv);
+
+typedef struct Option {
+    Any* super;
+    Any *(*orElse)(struct Option* this, Any *other);
+} Option;
+
+Option Option_(Any* super, Any *(*orElse)(struct Option* this, Any *other));
+
+typedef struct Some {
+    Any* value;
+    Option (*Option)(struct Some* this);
+} Some;
+
+Some Some_(Any* value);
+
+void throw(Any* value);
+
+Option catch();
 
 #endif //MAGMA_APP_H
