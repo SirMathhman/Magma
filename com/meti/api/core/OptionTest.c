513c32d094fa07c9c707217fcbe42e3c07fea50c
SirMathhman <sirmathhman@gmail.com>
2020-10-23 21:38:14 -0400

    Extract OptionTest.c

---
+++ com/meti/api/core/OptionTest.c
@@ -0,0 +1,65 @@
+//
+// Created by mathm on 10/23/2020.
+//
+
+#include "OptionTest.h"
+#include "../test/Assert.h"
+#include "Option.h"
+
+void testSomeIsPresent() {
+    int value = 5;
+    Some some = Some_(&value);
+    Option option = some.Option(&some);
+    assertTrue("Some.isPresent", option.isPresent(&option));
+}
+
+void testSomeIsEmpty() {
+    int value = 5;
+    Some some = Some_(&value);
+    Option option = some.Option(&some);
+    assertFalse("Some.isEmpty", option.isEmpty(&option));
+}
+
+void testSomeOrElse() {
+    int value = 420;
+    int other = 421;
+    Some some = Some_(&value);
+    Option option = some.Option(&some);
+    assertSame("Some.orElse", &value, option.orElse(&option, &other));
+}
+
+void testNoneIsPresent() {
+    None none = None_();
+    Option option = none.Option(&none);
+    assertFalse("None.isPresent", option.isPresent(&option));
+}
+
+void testNoneIsEmpty() {
+    None none = None_();
+    Option option = none.Option(&none);
+    assertTrue("None.isEmpty", option.isEmpty(&option));
+}
+
+void testNoneOrElse() {
+    int other = 421;
+    None none = None_();
+    Option option = none.Option(&none);
+    assertSame("None.orElse", &other, option.orElse(&option, &other));
+}
+
+void testSome() {
+    testSomeIsPresent();
+    testSomeIsEmpty();
+    testSomeOrElse();
+}
+
+void testNone() {
+    testNoneIsPresent();
+    testNoneIsEmpty();
+    testNoneOrElse();
+}
+
+void testOption() {
+    testSome();
+    testNone();
+}
---
+++ com/meti/api/core/OptionTest.h
@@ -0,0 +1,10 @@
+//
+// Created by mathm on 10/23/2020.
+//
+
+#ifndef MAGMA_OPTIONTEST_H
+#define MAGMA_OPTIONTEST_H
+
+void testOption();
+
+#endif //MAGMA_OPTIONTEST_H
