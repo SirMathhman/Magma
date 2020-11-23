88048a1f8dd656b3e96b459d914b589c3ac7444d
SirMathhman <sirmathhman@gmail.com>
2020-11-23 15:32:28 -0500

    Add Main.mg

--- Main.c
+++ Main.c
@@ -3,7 +3,17 @@
 //
 
 #include "Main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int main(){
+int main() {
+    FILE *file = fopen("Main.mg", "r");
+    if(file) {
+        fclose(file);
+    } else {
+        printf("Failed to open Main.mg: %s", strerror(errno));
+    }
     return 0;
 }
