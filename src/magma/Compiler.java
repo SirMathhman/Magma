1e81fe9d76fecaec751cb4ab774f35ffa4537e2c
SirMathhman <sirmathhman@gmail.com>
2024-07-20 21:22:35 -0400

    Multiple OK

--- src/magma/Compiler.java
+++ src/magma/Compiler.java
@@ -1,6 +1,7 @@
 package magma;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Compiler {
@@ -13,21 +14,27 @@ public class Compiler {
     }
 
     static String compile(String input) throws CompileException {
-        var lines = new ArrayList<String>();
-        var segments = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            var c = input.charAt(i);
-            segments.append(c);
-            if (c == ';') {
-                lines.add(segments.toString());
-                segments = new StringBuilder();
-            }
-        }
+        var segments = split(input);
+
         var output = new StringBuilder();
-        for (String line : lines) {
+        for (var line : segments) {
             output.append(compileRootMember(line));
         }
         return output.toString();
+    }
+
+    private static List<String> split(String input) {
+        var state = new State();
+        for (int i = 0; i < input.length(); i++) {
+            state = splitAtChar(state, input.charAt(i));
+        }
+
+        return state.advance().segments;
+    }
+
+    private static State splitAtChar(State state, char c) {
+        var appended = state.append(c);
+        return c == ';' ? appended.advance() : appended;
     }
 
     private static String compileRootMember(String input) throws CompileException {
@@ -43,5 +50,29 @@ public class Compiler {
         var name = afterKeyword.substring(0, afterKeyword.length() - STATEMENT_END.length());
 
         return Optional.of(renderImport(name));
+    }
+
+    private static class State {
+        private final List<String> segments;
+        private final StringBuilder buffer;
+
+        private State() {
+            this(new StringBuilder(), new ArrayList<>());
+        }
+
+        private State(StringBuilder buffer, List<String> segments) {
+            this.buffer = buffer;
+            this.segments = segments;
+        }
+
+        private State append(char c) {
+            return new State(this.buffer.append(c), this.segments);
+        }
+
+        private State advance() {
+            var copy = new ArrayList<>(segments);
+            copy.add(buffer.toString());
+            return new State(new StringBuilder(), copy);
+        }
     }
 }
