4eb61f635c0771d4abf30a383f22e68e332f9f72
SirMathhman <sirmathhman@gmail.com>
2024-07-20 21:12:42 -0400

    Imports

--- src/magma/Compiler.java
+++ src/magma/Compiler.java
@@ -1,10 +1,28 @@
 package magma;
 
+import java.util.Optional;
+
 public class Compiler {
     public static final String PACKAGE_KEYWORD_WITH_SPACE = "package ";
+    public static final String STATEMENT_END = ";";
+    public static final String IMPORT_KEYWORD_WITH_SPACE = "import ";
+
+    public static String renderImport(String name) {
+        return IMPORT_KEYWORD_WITH_SPACE + name + STATEMENT_END;
+    }
 
     static String compile(String input) throws CompileException {
         if (input.isEmpty() || input.startsWith(PACKAGE_KEYWORD_WITH_SPACE)) return "";
-        else throw new CompileException("Invalid input", input);
+        return compileImport(input).orElseThrow(() -> new CompileException("Invalid input", input));
+    }
+
+    private static Optional<String> compileImport(String input) {
+        if (!input.startsWith(IMPORT_KEYWORD_WITH_SPACE + "test" + STATEMENT_END)) return Optional.empty();
+        var afterKeyword = input.substring(IMPORT_KEYWORD_WITH_SPACE.length());
+
+        if (!afterKeyword.endsWith(STATEMENT_END)) return Optional.empty();
+        var name = afterKeyword.substring(0, afterKeyword.length() - STATEMENT_END.length());
+
+        return Optional.of(renderImport(name));
     }
 }
--- test/magma/CompilerTest.java
+++ test/magma/CompilerTest.java
@@ -1,20 +1,37 @@
 package magma;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static magma.Compiler.STATEMENT_END;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CompilerTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"first", "second"})
-    void packageName(String name) throws CompileException {
-        var value = Compiler.compile(renderPackageStatement(name));
-        assertEquals("", value);
+    private static void assertCompile(String input, String output) {
+        try {
+            var value = Compiler.compile(input);
+            assertEquals(output, value);
+        } catch (CompileException e) {
+            fail(e);
+        }
     }
 
     private static String renderPackageStatement(String name) {
-        return Compiler.PACKAGE_KEYWORD_WITH_SPACE + name + ";";
+        return Compiler.PACKAGE_KEYWORD_WITH_SPACE + name + STATEMENT_END;
+    }
+
+    @Test
+    void importName() {
+        var input = Compiler.renderImport("test");
+        assertCompile(input, input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void packageName(String name) {
+        assertCompile(renderPackageStatement(name), "");
     }
 }
