8f82089b5f30deb517c297183461f60188fe16ec
SirMathhman <sirmathhman@gmail.com>
2024-07-21 10:55:12 -0400

    OK

--- test/magma/app/compile/CompilerTest.java
+++ test/magma/app/compile/CompilerTest.java
@@ -1,7 +1,6 @@
 package magma.app.compile;
 
 import magma.app.compile.rule.Node;
-import magma.app.compile.rule.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -32,23 +31,36 @@ class CompilerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"First", "Second"})
-    void interfaceName(String name) {
-        assertCompile(INTERFACE_RULE.generate(new Node(Optional.empty(), Map.of(NAME, name))).findValue().orElseThrow(), STRUCT_RULE.generate(new Node(Optional.empty(), Map.of(NAME, name))).findValue().orElseThrow());
+    void interfaceName(String name) throws CompileException {
+        var node = new Node().with(NAME, name);
+
+        assertCompile(INTERFACE_RULE.generate(node.retype(INTERFACE)).$(),
+                STRUCT_RULE.generate(node.retype(STRUCT)).$());
     }
 
     @Test
-    void interfacePublic() {
-        var map = Map.of(MODIFIERS, EXPORT_KEYWORD_WITH_SPACE, NAME, TEST_UPPER_SYMBOL);
-        assertCompile(INTERFACE_RULE.generate(new Node(Optional.empty(), Map.of(MODIFIERS, PUBLIC_KEYWORD_WITH_SPACE, NAME, TEST_UPPER_SYMBOL))).findValue().orElseThrow(), STRUCT_RULE.generate(new Node(Optional.empty(), map)).findValue().orElseThrow());
+    void interfacePublic() throws CompileException {
+        var node = new Node()
+                .retype(INTERFACE)
+                .with(MODIFIERS, PUBLIC_KEYWORD_WITH_SPACE)
+                .with(NAME, TEST_UPPER_SYMBOL);
+        var interfaceString = INTERFACE_RULE.generate(node).$();
+
+        var structNode = new Node()
+                .retype(STRUCT)
+                .with(MODIFIERS, EXPORT_KEYWORD_WITH_SPACE)
+                .with(NAME, TEST_UPPER_SYMBOL);
+        var structString = STRUCT_RULE.generate(structNode).$();
+
+        assertCompile(interfaceString, structString);
     }
 
     @Test
-    void rootMemberMultiple() {
-        Rule rule = createImportRule();
-        Rule rule1 = createImportRule();
-        assertCompile(renderPackageStatement(TEST_LOWER_SYMBOL) + rule1.generate(new Node(Optional.empty(), Map.of(SEGMENTS, TEST_LOWER_SYMBOL))).findValue()
-                .orElseThrow(), rule.generate(new Node(Optional.empty(), Map.of(SEGMENTS, TEST_LOWER_SYMBOL))).findValue()
-                .orElseThrow());
+    void rootMemberMultiple() throws CompileException {
+        var rule = createImportRule();
+        var node = new Node().retype(IMPORT).with(SEGMENTS, TEST_LOWER_SYMBOL);
+        var importString = rule.generate(node).$();
+        assertCompile(renderPackageStatement(TEST_LOWER_SYMBOL) + importString, importString);
     }
 
     @Test
