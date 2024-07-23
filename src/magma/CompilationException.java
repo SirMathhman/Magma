0931e6aca6f12579a59247ea362e5a7687983628
SirMathhman <sirmathhman@gmail.com>
2024-07-22 20:46:56 -0400

    Nuke again

--- src/magma/CompilationException.java
+++
@@ -1,7 +0,0 @@
-package magma;
-
-public class CompilationException extends Exception {
-    public CompilationException(String message, String context) {
-        super(message + ": " + context);
-    }
-}
