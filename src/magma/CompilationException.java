701f7361c990c5236b53e53da4c1d4db9f8526e0
SirMathhman <sirmathhman@gmail.com>
2024-07-22 20:51:40 -0400

    Bug

--- src/magma/CompilationException.java
+++ src/magma/CompilationException.java
@@ -2,6 +2,6 @@ package magma;
 
 public class CompilationException extends Exception {
     public CompilationException(String message, String context) {
-        super(context + ": " + context);
+        super(message + ": " + context);
     }
 }
