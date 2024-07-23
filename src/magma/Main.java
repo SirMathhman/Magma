591ad678d839644f7cf4f111c5fa8ae9bbf21707
SirMathhman <sirmathhman@gmail.com>
2024-07-23 12:06:43 -0400

    Add depth

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -24,43 +24,42 @@ public class Main {
 
     private static String compile(String root) throws CompileException {
         var segments = Splitter.split(root);
-        var compiledSegments = compileRootMembers(segments);
-        var builder = generateRoot(compiledSegments);
-        return builder.toString();
+        var compiledSegments = compileRootMembers(segments, 0);
+        return generateBlock(compiledSegments, 0);
     }
 
-    private static StringBuilder generateRoot(List<String> compiledSegments) {
+    private static String generateBlock(List<String> compiledSegments, int depth) {
         var builder = new StringBuilder();
         for (int i = 0; i < compiledSegments.size(); i++) {
             var segment = compiledSegments.get(i);
-            var prefix = i == 0 ? "" : "\n";
+            var prefix = (i == 0 && depth == 0) ? "" : "\n";
             builder.append(prefix).append(segment);
         }
-        return builder;
+        return builder.toString();
     }
 
-    private static List<String> compileRootMembers(List<String> segments) throws CompileException {
+    private static List<String> compileRootMembers(List<String> segments, int depth) throws CompileException {
         var compiledSegments = new ArrayList<String>();
         for (String segment : segments) {
             var stripped = segment.strip();
             if(stripped.isEmpty()) continue;
 
-            var compiled = compileRootMember(stripped);
+            var compiled = compileRootMember(stripped, depth + 1);
             if (!compiled.isEmpty()) compiledSegments.add(compiled);
         }
         return compiledSegments;
     }
 
-    private static String compileRootMember(String segment) throws CompileException {
+    private static String compileRootMember(String segment, int depth) throws CompileException {
         if (segment.startsWith("package ")) return "";
         if (segment.startsWith("import ")) return segment;
 
-        return compileClass(segment)
+        return compileClass(segment, depth)
                 .orElseGet(() -> new Err<>(new CompileException("Unknown root member", segment)))
                 .unwrap();
     }
 
-    private static Optional<Result<String, CompileException>> compileClass(String rootMember) {
+    private static Optional<Result<String, CompileException>> compileClass(String rootMember, int depth) {
         var classIndex = rootMember.indexOf("class ");
         if (classIndex == -1) return Optional.empty();
 
@@ -84,30 +83,30 @@ public class Main {
             if(stripped.isEmpty()) continue;
 
             newClassMembers = newClassMembers
-                    .and(() -> compileClassMember(stripped))
+                    .and(() -> compileClassMember(stripped, depth + 1))
                     .mapValue(tuple -> tuple.left().add(tuple.right()));
         }
 
-        return Optional.of(newClassMembers.mapValue(value -> renderFunction(name, newModifiers + "class ", value)));
+        return Optional.of(newClassMembers.mapValue(value -> renderFunction(name, newModifiers + "class ", value, depth)));
     }
 
-    private static String renderFunction(String name, String newModifiers, JavaList<String> content) {
-        return newModifiers + "def " + name + "() => {" + String.join("", content.list()) + "}";
+    private static String renderFunction(String name, String newModifiers, JavaList<String> content, int depth) {
+        return newModifiers + "def " + name + "() => {" + generateBlock(content.list(), depth) + "}";
     }
 
-    private static Result<String, CompileException> compileClassMember(String classMember) {
-        return compileMethod(classMember)
+    private static Result<String, CompileException> compileClassMember(String classMember, int depth) {
+        return compileMethod(classMember, depth)
                 .orElseGet(() -> new Err<>(new CompileException("Invalid class member", classMember)));
     }
 
-    private static Optional<Result<String, CompileException>> compileMethod(String classMember) {
+    private static Optional<Result<String, CompileException>> compileMethod(String classMember, int depth) {
         var separator = classMember.indexOf('(');
         if (separator == -1) return Optional.empty();
 
         var before = classMember.substring(0, separator).strip();
         var space = before.lastIndexOf(" ");
         var name = before.substring(space + 1).strip();
-        return Optional.of(new Ok<>(renderFunction(name, "", new JavaList<>())));
+        return Optional.of(new Ok<>(renderFunction(name, "", new JavaList<>(), depth)));
     }
 
     private static Path resolve(String extension) {
