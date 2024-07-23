e009e05a6a302dd3a6b9ca98b448d1d15378629e
SirMathhman <sirmathhman@gmail.com>
2024-07-22 22:19:51 -0400

    Reduce nesting

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,17 +25,7 @@ public class Main {
     }
 
     private static String compile(String input) throws CompileException {
-        var segments = new ArrayList<String>();
-        var buffer = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            var c = input.charAt(i);
-            buffer.append(c);
-            if (c == ';') {
-                segments.add(buffer.toString());
-                buffer = new StringBuilder();
-            }
-        }
-        segments.add(buffer.toString());
+        var segments = split(input);
 
         var output = new StringBuilder();
         for (String segment : segments) {
@@ -44,7 +35,50 @@ public class Main {
         return output.toString();
     }
 
+    private static List<String> split(String input) {
+        var current = new State();
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            current = splitAtChar(current, c);
+        }
+
+        return current.advance().segments;
+    }
+
+    private static State splitAtChar(State state, char c) {
+        var appended = state.append(c);
+        if (c == ';') {
+            return appended.advance();
+        } else {
+            return appended;
+        }
+    }
+
     private static String compileRootMember(String rootMember) throws CompileException {
         throw new CompileException("Unknown root member", rootMember);
+    }
+
+    private static class State {
+        private final List<String> segments;
+        private final StringBuilder buffer;
+
+        private State(StringBuilder buffer, List<String> segments) {
+            this.buffer = buffer;
+            this.segments = segments;
+        }
+
+        public State() {
+            this(new StringBuilder(), new ArrayList<>());
+        }
+
+        private State append(char c) {
+            return new State(buffer.append(c), segments);
+        }
+
+        private State advance() {
+            var copy = new ArrayList<>(segments);
+            copy.add(buffer.toString());
+            return new State(new StringBuilder(), copy);
+        }
     }
 }
