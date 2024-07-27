8f6a432fc4a37165edd48cd4938b08b238ab1127
SirMathhman <sirmathhman@gmail.com>
2024-07-27 16:51:38 -0400

    Statements OK

--- src/magma/app/Compiler.java
+++ src/magma/app/Compiler.java
@@ -2,11 +2,45 @@ package magma.app;
 
 import magma.compile.CompileException;
 
-public class Compiler {
-    String compile(String input) throws CompileException {
-        if (input.isEmpty() || input.startsWith("package ")) return "";
-        if (input.startsWith("import ")) return input;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
-        throw new CompileException("Invalid root", input);
+public class Compiler {
+    public static final String PACKAGE_KEYWORD_WITH_SPACE = "package ";
+    public static final String IMPORT_KEYWORD_WITH_SPACE = "import ";
+    public static final char STATEMENT_END = ';';
+
+    private static String compileRootMember(String input) throws CompileException {
+        if (input.isEmpty() || input.startsWith(PACKAGE_KEYWORD_WITH_SPACE)) return "";
+        if (input.startsWith(IMPORT_KEYWORD_WITH_SPACE)) return input;
+
+        throw new CompileException("Invalid root member", input);
+    }
+
+    String compile(String input) throws CompileException {
+        var rootMembers = split(input).toList();
+
+        var output = new StringBuilder();
+        for (String rootMember : rootMembers) {
+            output.append(compileRootMember(rootMember));
+        }
+
+        return output.toString();
+    }
+
+    private static Stream<String> split(String input) {
+        var rootMembers = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var length = input.length();
+        for (int i = 0; i < length; i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == STATEMENT_END) {
+                rootMembers.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        rootMembers.add(buffer.toString());
+        return rootMembers.stream();
     }
 }
--- test/magma/app/CompilerTest.java
+++ test/magma/app/CompilerTest.java
@@ -2,11 +2,17 @@ package magma.app;
 
 import magma.compile.CompileException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static magma.app.Compiler.IMPORT_KEYWORD_WITH_SPACE;
+import static magma.app.Compiler.PACKAGE_KEYWORD_WITH_SPACE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class CompilerTest {
+    public static final String TEST_LOWER_BOUND = "test";
+
     private static void assertCompile(String input, String expected) {
         try {
             var actual = new Compiler().compile(input);
@@ -16,14 +22,30 @@ class CompilerTest {
         }
     }
 
-    @Test
-    void importStatement() {
-        var statement = "import first;";
-        assertCompile(statement, statement);
+    private static String renderPackage(String name) {
+        return PACKAGE_KEYWORD_WITH_SPACE + name + Compiler.STATEMENT_END;
+    }
+
+    private static String renderImport(String name) {
+        return IMPORT_KEYWORD_WITH_SPACE + name + Compiler.STATEMENT_END;
     }
 
     @Test
-    void packageStatement() {
-        assertCompile("package test;", "");
+    void rootMemberMultiple() {
+        var renderedImport = renderImport("foo");
+        assertCompile(renderPackage(TEST_LOWER_BOUND) + renderedImport, renderedImport);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void importStatement(String name) {
+        var statement = renderImport(name);
+        assertCompile(statement, statement);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"first", "second"})
+    void packageStatement(String name) {
+        assertCompile(renderPackage(name), "");
     }
 }
