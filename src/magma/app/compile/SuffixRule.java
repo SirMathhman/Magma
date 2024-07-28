a3af044ad51bf24354180ca61e04ed06deb5a832
SirMathhman <sirmathhman@gmail.com>
2024-07-28 18:45:17 -0400

    Impl

--- src/magma/app/compile/SuffixRule.java
+++ src/magma/app/compile/SuffixRule.java
@@ -4,6 +4,12 @@ import java.util.Optional;
 
 public record SuffixRule(Rule child, String suffix) implements Rule {
     @Override
+    public Optional<Node> parse(String input) {
+        if(input.endsWith(suffix)) return child.parse(input.substring(0, input.length() - suffix.length()));
+        return Optional.empty();
+    }
+
+    @Override
     public Optional<String> generate(Node node) {
         return child().generate(node).map(value -> value + suffix());
     }
