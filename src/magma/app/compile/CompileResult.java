9abf0e3a6604a21e97a90be83840bf41a21738a5
SirMathhman <sirmathhman@gmail.com>
2024-07-29 10:54:58 -0400

    Wrappers OK

--- src/magma/app/compile/CompileResult.java
+++ src/magma/app/compile/CompileResult.java
@@ -5,8 +5,12 @@ import magma.api.Result;
 import java.util.Collections;
 import java.util.List;
 
-public record CompileResult<T>(Result<T, CompileException> result, List<Result<T, CompileException>> children) {
+public record CompileResult<T>(Result<T, CompileException> result, List<CompileResult<T>> children) {
     public CompileResult(Result<T, CompileException> result) {
         this(result, Collections.emptyList());
+    }
+
+    public boolean isValid() {
+        return result.isOk();
     }
 }
--- src/magma/app/compile/DisjunctionRule.java
+++ src/magma/app/compile/DisjunctionRule.java
@@ -7,43 +7,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public record DisjunctionRule(List<Rule> rules) implements Rule {
-    private Result<Node, CompileException> parse1(String input) {
-        var errors = new ArrayList<Result<Node, CompileException>>();
+    @Override
+    public CompileResult<Node> parse(String input) {
+        var errors = new ArrayList<CompileResult<Node>>();
         for (Rule rule : rules) {
-            var parsed = rule.parse(input).result();
-            if (parsed.isOk()) return parsed;
+            var parsed = rule.parse(input);
+            if (parsed.isValid()) return parsed;
             errors.add(parsed);
         }
 
         if (errors.isEmpty()) {
-            return new Err<>(new CompileException("No rules present", input));
+            return new CompileResult<>(new Err<>(new CompileException("No rules present", input)));
         } else {
-            return errors.get(errors.size() - 1).mapErr(err -> new CompileException("Failed to apply disjunction", input, err));
+            return new CompileResult<>(new Err<>(new CompileException("Failed to apply disjunction", input)), errors);
         }
-    }
-
-    private Result<String, CompileException> generate1(Node node) {
-        var errors = new ArrayList<Result<String, CompileException>>();
-        for (Rule rule : rules) {
-            var generated = rule.generate(node).result();
-            if (generated.isOk()) return generated;
-            errors.add(generated);
-        }
-
-        if (errors.isEmpty()) {
-            return new Err<>(new NodeException("No rules present", node));
-        } else {
-            return errors.get(errors.size() - 1).mapErr(err -> new NodeException("Failed to apply disjunction", node, err));
-        }
-    }
-
-    @Override
-    public CompileResult<Node> parse(String input) {
-        return new CompileResult<>(parse1(input));
     }
 
     @Override
     public CompileResult<String> generate(Node node) {
-        return new CompileResult<>(generate1(node));
+        var errors = new ArrayList<CompileResult<String>>();
+        for (Rule rule : rules) {
+            var generated = rule.generate(node);
+            if (generated.isValid()) return generated;
+            errors.add(generated);
+        }
+
+        if (errors.isEmpty()) {
+            return new CompileResult<>(new Err<>(new NodeException("No rules present", node)));
+        } else {
+            return new CompileResult<>(new Err<>(new NodeException("Failed to apply disjunction", node)), errors);
+        }
     }
 }
