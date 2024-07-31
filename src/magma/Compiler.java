71b7904ab7bd17c40c18d68017c5cb647470c5fc
SirMathhman <sirmathhman@gmail.com>
2024-07-31 11:21:42 -0400

    Compile multiple

--- src/magma/Compiler.java
+++ src/magma/Compiler.java
@@ -1,5 +1,7 @@
 package magma;
 
+import java.util.ArrayList;
+
 public class Compiler {
     public static final String PACKAGE_KEYWORD_WITH_SPACE = "package ";
     public static final String STATEMENT_END = ";";
@@ -13,10 +15,35 @@ public class Compiler {
         return IMPORT_KEYWORD_WITH_SPACE + parent + "." + child + STATEMENT_END;
     }
 
-    String compile(String input) throws ParseException {
+    private static String compileRootMember(String input) throws ParseException {
         if (input.isEmpty() || input.startsWith(PACKAGE_KEYWORD_WITH_SPACE)) return "";
         if (input.startsWith(IMPORT_KEYWORD_WITH_SPACE)) return input;
 
         throw new ParseException("Invalid root", input);
+    }
+
+    private static ArrayList<String> splitRootMembers(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var length = input.length();
+        for (int i = 0; i < length; i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        return segments;
+    }
+
+    String compile(String input) throws ParseException {
+        var rootMembers = splitRootMembers(input);
+        var output = new StringBuilder();
+        for (var rootMember : rootMembers) {
+            output.append(compileRootMember(rootMember));
+        }
+
+        return output.toString();
     }
 }
--- test/magma/CompilerTest.java
+++ test/magma/CompilerTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class CompilerTest {
-
     private static void assertCompile(String input, String output) {
         try {
             assertEquals(output, new Compiler().compile(input));
@@ -17,9 +16,16 @@ class CompilerTest {
         }
     }
 
+    @Test
+    void rootStatementMultiple() {
+        var renderedPackage = Compiler.renderPackage("test");
+        var renderedImport = Compiler.renderImport("test", "Test");
+        assertCompile(renderedPackage + renderedImport, renderedImport);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"first", "second"})
-    void packageNames(String name) {
+    void packageName(String name) {
         assertCompile(Compiler.renderPackage(name), "");
     }
 
