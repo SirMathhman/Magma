7b12e80cfdc2296e86dcd9ce19ac0436ad802590
SirMathhman <sirmathhman@gmail.com>
2024-07-31 11:44:24 -0400

    Formatting

--- src/magma/Compiler.java
+++ src/magma/Compiler.java
@@ -6,7 +6,6 @@ public class Compiler {
     public static final String PACKAGE_KEYWORD_WITH_SPACE = "package ";
     public static final String IMPORT_KEYWORD_WITH_SPACE = "import ";
     public static final String CLASS_KEYWORD_WITH_SPACE = "class ";
-    public static final String EMPTY_CONTENT = " " + Splitter.BLOCK_START + Splitter.BLOCK_END;
     public static final String PUBLIC_KEYWORD_WITH_SPACE = "public ";
     public static final String EXPORT_KEYWORD_WITH_SPACE = "export ";
 
@@ -28,18 +27,17 @@ public class Compiler {
 
     private static Optional<String> compileClass(String input) throws ParseException {
         var classIndex = input.indexOf(CLASS_KEYWORD_WITH_SPACE);
+
         if (classIndex == -1) return Optional.empty();
-
         var oldModifiers = input.substring(0, classIndex);
-
         var truncatedRight = input.substring(classIndex + CLASS_KEYWORD_WITH_SPACE.length());
-
         var startIndex = truncatedRight.indexOf(Splitter.BLOCK_START);
-        if (startIndex == -1) return Optional.empty();
 
+        if (startIndex == -1) return Optional.empty();
         var name = truncatedRight.substring(0, startIndex).strip();
         var contentAndEnd = truncatedRight.substring(startIndex + 1);
-        if(!contentAndEnd.endsWith(String.valueOf(Splitter.BLOCK_END))) return Optional.empty();
+
+        if (!contentAndEnd.endsWith(String.valueOf(Splitter.BLOCK_END))) return Optional.empty();
         var content = contentAndEnd.substring(0, contentAndEnd.length() - 1);
         var compiledContent = compileClassMembers(content);
 
@@ -48,7 +46,7 @@ public class Compiler {
     }
 
     private static String compileClassMembers(String content) throws ParseException {
-        if(content.isEmpty()) return "";
+        if (content.isEmpty()) return "";
         throw new ParseException("Unknown class member", content);
     }
 
