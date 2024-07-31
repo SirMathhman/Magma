ca0b307595eabc9e1b216eb10801f4103e297712
SirMathhman <sirmathhman@gmail.com>
2024-07-31 10:45:23 -0400

    OK

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -1,17 +1,22 @@
 package magma;
 
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            var source = Application.ROOT_DIRECTORY.resolve("src")
-                    .resolve("magma")
-                    .resolve("Main.java");
-
+            var source = resolveMain();
             var application = new Application(source);
             application.run();
         } catch (ApplicationException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static Path resolveMain() {
+        return Application.ROOT_DIRECTORY.resolve("src")
+                .resolve("magma")
+                .resolve("Main.java");
     }
 }
