cbde5864ada34371f1c311e68f34569a5601b177
SirMathhman <sirmathhman@gmail.com>
2024-08-06 17:58:37 -0400

    Cleanup nesting

--- src/magma/app/compile/pass/MagmaFormatter.java
+++ src/magma/app/compile/pass/MagmaFormatter.java
@@ -6,13 +6,46 @@ import magma.app.compile.Passer;
 import magma.app.compile.lang.common.Blocks;
 import magma.app.compile.lang.common.PrefixedStatements;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static magma.app.compile.lang.common.Blocks.CHILDREN;
 import static magma.app.compile.lang.common.PrefixedStatements.BEFORE_BLOCK;
 
 public class MagmaFormatter implements Passer {
+    private static Optional<Tuple<Node, Integer>> postVisitTry(Node node, int state) {
+        if (node.is(PrefixedStatements.TRY)) return Optional.of(new Tuple<>(node.withString(BEFORE_BLOCK, " "), state));
+        return Optional.empty();
+    }
+
+    private static Optional<Tuple<Node, Integer>> postVisitBlock(Node node, int state) {
+        if (!node.is(Blocks.BLOCK)) return Optional.empty();
+
+        var blockIndent = state <= 0 ? "" : "\t".repeat(state - 1);
+        var newNode = node
+                .mapNodeList(CHILDREN, oldChildren -> attachAllIndent(state, oldChildren))
+                .withString(Blocks.AFTER_CHILDREN, "\n" + blockIndent);
+
+        return Optional.of(new Tuple<>(newNode, state - 1));
+    }
+
+    private static List<Node> attachAllIndent(int state, List<Node> children) {
+        return IntStream.range(0, children.size())
+                .mapToObj(index -> attachIndent(state, index, children))
+                .toList();
+    }
+
+    private static Node attachIndent(int state, int index, List<Node> children) {
+        var newChild = children.get(index);
+        if (index == 0 && state == 0) {
+            return newChild;
+        } else {
+            var indent = state < 0 ? "\n" : "\n" + "\t".repeat(state);
+            return newChild.withString(Blocks.BEFORE_CHILD, indent);
+        }
+    }
+
     @Override
     public Optional<Tuple<Node, Integer>> preVisit(Node node, int state) {
         if (node.is(Blocks.BLOCK)) {
@@ -24,29 +57,6 @@ public class MagmaFormatter implements Passer {
 
     @Override
     public Optional<Tuple<Node, Integer>> postVisit(Node node, int state) {
-        if(node.is(PrefixedStatements.TRY)) {
-            return Optional.of(new Tuple<>(node.withString(BEFORE_BLOCK, " "), state));
-        }
-
-        if (node.is(Blocks.BLOCK)) {
-            var oldChildren = node.findNodeList(CHILDREN).orElseThrow();
-            var newChildren = new ArrayList<Node>();
-            for (int i = 0; i < oldChildren.size(); i++) {
-                var newChild = oldChildren.get(i);
-                if (i == 0 && state == 0) {
-                    newChildren.add(newChild);
-                } else {
-                    var indent = state < 0 ? "\n" : "\n" + "\t".repeat(state);
-                    newChildren.add(newChild.withString(Blocks.BEFORE_CHILD, indent));
-                }
-            }
-
-            var blockIndent = state <= 0 ? "" : "\t".repeat(state - 1);
-            return Optional.of(new Tuple<>(node
-                    .withNodeList(CHILDREN, newChildren)
-                    .withString(Blocks.AFTER_CHILDREN, "\n" + blockIndent), state - 1));
-        }
-
-        return Optional.empty();
+        return postVisitTry(node, state).or(() -> postVisitBlock(node, state));
     }
 }
