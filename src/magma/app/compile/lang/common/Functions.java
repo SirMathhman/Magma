590c38fe2bed34c25cef478b277b087f2a021cde
SirMathhman <sirmathhman@gmail.com>
2024-08-08 11:13:07 -0400

    Return statements were not stripped

--- src/magma/app/compile/lang/common/Functions.java
+++ src/magma/app/compile/lang/common/Functions.java
@@ -1,18 +1,20 @@
 package magma.app.compile.lang.common;
 
-import magma.app.compile.split.ParamSplitter;
 import magma.app.compile.rule.NodeListRule;
 import magma.app.compile.rule.NodeRule;
 import magma.app.compile.rule.PrefixRule;
 import magma.app.compile.rule.Rule;
+import magma.app.compile.rule.StripRule;
 import magma.app.compile.rule.SuffixRule;
 import magma.app.compile.rule.TypeRule;
+import magma.app.compile.split.ParamSplitter;
 
 public class Functions {
     public static final String PARAMS = "params";
 
     public static TypeRule createReturnRule(Rule value) {
-        return new TypeRule("return", new PrefixRule("return ", new SuffixRule(new NodeRule("value", value), ";")));
+        var valueProperty = new NodeRule("value", value);
+        return new TypeRule("return", new StripRule(new PrefixRule("return ", new SuffixRule(valueProperty, ";"))));
     }
 
     public static NodeListRule createParamsRule(Rule definition) {
