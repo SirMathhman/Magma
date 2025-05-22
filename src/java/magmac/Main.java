5d40c7443fa88c86d780feeece8e3af26e01f518
SirMathhman <sirmathhman@gmail.com>
2025-05-22 08:48:48 -0400

    Segments

--- src/java/magmac/Main.java
+++ src/java/magmac/Main.java
@@ -3,16 +3,40 @@ package magmac;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main() {
         try {
-            var umlContent = "@startuml\nclass Main {\n}\n@enduml";
+            var source = Paths.get(".", "src", "java", "magmac", "Main.java");
             var diagramPath = Paths.get(".", "diagram.puml");
+
+            var input = Files.readString(source);
+            var output = Main.compile(input);
+
+            var umlContent = "@startuml\n" + output + "\n@enduml";
             Files.writeString(diagramPath, umlContent);
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String input) {
+
+
+
+        return Arrays.stream(Main.splitIntoCommentSegments(input))
+                .filter(value -> !value.isEmpty())
+                .map(value -> "'" + value + "\n")
+                .collect(Collectors.joining());
+    }
+
+    private static String[] splitIntoCommentSegments(String input) {
+        return input.replace("@startuml", "startuml")
+                .replace("@enduml", "enduml")
+                .split(Pattern.quote("\n"));
     }
 }
