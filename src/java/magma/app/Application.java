288424ed02604e7d387f810883e8e6b8a5229c21
SirMathhman <sirmathhman@gmail.com>
2025-05-25 23:57:59 -0400

    Remove headers

--- src/java/magma/app/Application.java
+++ src/java/magma/app/Application.java
@@ -31,24 +31,14 @@ record Application(Sources sources, Targets targets) {
 
     Option<IOError> runWith(Platform platform) {
         return this.sources.listSources()
-                .flatMapNode((Iterable<Source> children) -> {
-                    return this.runWithChildren(platform, children);
-                })
+                .flatMapNode((Iterable<Source> children) -> this.runWithChildren(platform, children))
                 .findError();
     }
 
     private Result<CompileState, IOError> runWithChildren(Platform platform, Iterable<Source> children) {
-        CompileState state = ImmutableCompileState.createEmpty().mapContext((Context context) -> {
-            return context.withPlatform(platform);
-        });
-        var initial = children.iter().foldWithInitial(state, (CompileState current, Source source) -> {
-            return current.mapContext((Context context1) -> {
-                return context1.addSource(source);
-            });
-        });
-        var folded = children.iter().foldWithInitialToResult(initial, (CompileState state1, Source source) -> {
-            return this.runWithSource(state1, source);
-        });
+        CompileState state = ImmutableCompileState.createEmpty().mapContext((Context context) -> context.withPlatform(platform));
+        var initial = children.iter().foldWithInitial(state, (CompileState current, Source source) -> current.mapContext((Context context1) -> context1.addSource(source)));
+        var folded = children.iter().foldWithInitialToResult(initial, (CompileState state1, Source source) -> this.runWithSource(state1, source));
 
         if (!state.context().hasPlatform(Platform.PlantUML) || !(folded instanceof Ok(var result))) {
             return folded;
@@ -56,9 +46,7 @@ record Application(Sources sources, Targets targets) {
 
         var diagramPath = Files.get(".", "diagram.puml");
         var joinedDependencies = result.registry().iterDependencies()
-                .map((Dependency dependency) -> {
-                    return dependency.name() + " --> " + dependency.child() + "\n";
-                })
+                .map((Dependency dependency) -> dependency.name() + " --> " + dependency.child() + "\n")
                 .collect(new Joiner(""))
                 .orElse("");
 
@@ -71,9 +59,7 @@ record Application(Sources sources, Targets targets) {
     }
 
     private Result<CompileState, IOError> runWithSource(CompileState state, Source source) {
-        return source.read().flatMapNode((String input) -> {
-            return this.runWithInput(state, source, input);
-        });
+        return source.read().flatMapNode((String input) -> this.runWithInput(state, source, input));
     }
 
     private Result<CompileState, IOError> runWithInput(CompileState state1, Source source, String input) {
@@ -85,35 +71,20 @@ record Application(Sources sources, Targets targets) {
             return new Ok<CompileState, IOError>(compiledState);
         }
 
-        var segment = state1.context().iterSources()
-                .map((Source source1) -> {
-                    return Application.formatSource(source1);
-                })
-                .collect(new Joiner(", "))
-                .orElse("");
-
         var otherOutput = compiled.right();
         var joinedImports = compiledState.registry().queryImports()
-                .map((Import anImport) -> {
-                    return anImport.generate();
-                })
+                .map((Import anImport) -> anImport.generate())
                 .collect(new Joiner(""))
                 .orElse("");
 
         var joined = joinedImports + compiledState.registry().output() + otherOutput;
-        var cleared = state1.mapRegistry((Registry registry) -> {
-            return registry.reset();
-        });
-        return this.writeTarget(source, cleared, "/*[" + segment + "\n]*/\n" + joined);
+        var cleared = state1.mapRegistry((Registry registry) -> registry.reset());
+        return this.writeTarget(source, cleared, joined);
     }
 
     private Result<CompileState, IOError> writeTarget(Source source, CompileState cleared, String output) {
         return this.targets().writeSource(source.createLocation(), output)
-                .<Result<CompileState, IOError>>map((IOError error) -> {
-                    return new Err<CompileState, IOError>(error);
-                })
-                .orElseGet(() -> {
-                    return new Ok<CompileState, IOError>(cleared);
-                });
+                .<Result<CompileState, IOError>>map((IOError error) -> new Err<CompileState, IOError>(error))
+                .orElseGet(() -> new Ok<CompileState, IOError>(cleared));
     }
 }
