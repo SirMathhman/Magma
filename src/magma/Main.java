ef4e5203b6c1d26792d0a389ad28d9c3c0bd09c7
Gavin Bromberg <10675613+SirMathhman@users.noreply.github.com>
2025-06-05 23:01:17 -0400

    Copy Main.java content to Main.mgs

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -1,10 +1,14 @@
 package magma;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main() throws Exception {
-        Files.createFile(Paths.get("src/magma/Main.mgs"));
+        Path source = Paths.get("src/magma/Main.java");
+        Path target = Paths.get("src/magma/Main.mgs");
+        byte[] content = Files.readAllBytes(source);
+        Files.write(target, content);
     }
 }
