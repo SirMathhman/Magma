e5b61691035114387828def5c871f9cd49d81727
SirMathhman <sirmathhman@gmail.com>
2025-06-07 16:47:06 -0400

    Rebuild

--- src/main/node/magma/app/TypeMapper.ts
+++ src/main/node/magma/app/TypeMapper.ts
@@ -1,11 +1,11 @@
-import ArrayList from "../../java/util/ArrayList";
-import List from "../../java/util/List";
+import JdkList from "../list/JdkList";
+import ListLike from "../list/ListLike";
 export default class TypeMapper {
     toTsParams(params: string): string {
         if (params.isBlank()) {
             return "";
         }
-        let out: List<string> = new ArrayList<>();
+        let out: ListLike<string> = JdkList.create();
         // TODO
         let parts: var = p.trim().split("\\s+");
         let (parts.length: if = /* TODO */;
@@ -13,7 +13,14 @@ export default class TypeMapper {
         let type: var = parts.length > 1 ? parts[parts.length - 2] : "any";
         out.add(name + ": " + toTsType(type));
         // TODO
-        return String.join(/* TODO */, /* TODO */, out);
+        let result: var = new StringBuilder();
+        let i: (var = 0;
+        i < out.size();
+        // TODO
+        // TODO
+        result.append(out.get(i));
+        // TODO
+        return result.toString();
     }
 
     toTsType(javaType: string): string {
@@ -38,10 +45,17 @@ export default class TypeMapper {
     mapGeneric(javaType: string, start: number, end: number): string {
         let base: var = javaType.substring(0, start).trim();
         let params: var = javaType.substring(/* TODO */, end);
-        let mapped: List<string> = new ArrayList<>();
+        let mapped: ListLike<string> = JdkList.create();
         // TODO
         mapped.add(toTsType(p.trim()));
         // TODO
-        return base + "<" + String.join(/* TODO */, /* TODO */, mapped);
+        let joined: var = new StringBuilder();
+        let i: (var = 0;
+        i < mapped.size();
+        // TODO
+        // TODO
+        joined.append(mapped.get(i));
+        // TODO
+        return /* TODO */;
     }
 }
