04b4a0a3bf7ebf6a44c3487667493c33033811a9
SirMathhman <sirmathhman@gmail.com>
2025-06-07 03:02:55 -0400

    OK

---
+++ src/main/node/com/example/Main.ts
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple command line interface for the Transpiler.
+ */
+export default class Main {
+    main(args: string[]): void {
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+    }
+
+    transpileFile(srcRoot: any, outRoot: any, javaFile: any): void {
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+    }
+}
---
+++ src/main/node/com/example/Transpiler.ts
@@ -0,0 +1,140 @@
+/**
+ * Very small prototype that converts a fragment of Java to TypeScript.
+ */
+export default class Transpiler {
+
+    /**
+     * Transpiles the given Java source code to TypeScript.
+     * Currently removes the package declaration and rewrites simple class
+     * definitions. Modifiers before the {@code class} keyword are replaced with
+     * {@code export default}.
+     *
+     * @param javaSource the Java source text
+     * @return transpiled TypeScript
+     */
+    toTypeScript(javaSource: string): string {
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+        // TODO
+    }
