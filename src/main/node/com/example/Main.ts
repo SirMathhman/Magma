7b6a72b15b47c1b74e369e5b7fcc37c0a2ca9e73
SirMathhman <sirmathhman@gmail.com>
2025-06-07 13:46:45 -0400

    Imports

--- src/main/node/com/example/Main.ts
+++ src/main/node/com/example/Main.ts
@@ -1,29 +1,28 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
+import IOException from "../../java/io/IOException";
+import Files from "../../java/nio/file/Files";
+import Path from "../../java/nio/file/Path";
+import ArrayList from "../../java/util/ArrayList";
+import List from "../../java/util/List";
 /**
  * Simple command line interface for the Transpiler.
  */
 export default class Main {
     main(args: string[]): void {
-        let error: Option<string> = /* TODO */;
+        let error: Option<string> = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
     }
 
     run(): Option<string> {
-        let srcRoot: any = /* TODO */;
-        let outRoot: any = /* TODO */;
-        let files: Result<List<any>> = /* TODO */;
+        let srcRoot: any = /* TODO */(/* TODO */);
+        let outRoot: any = /* TODO */(/* TODO */);
+        let files: Result<List<any>> = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
         // TODO
-        let err: Option<string> = /* TODO */;
+        let err: Option<string> = /* TODO */(/* TODO */, /* TODO */, /* TODO */);
         if (/* TODO */) {
             // TODO
         }
@@ -32,8 +31,8 @@ export default class Main {
     }
 
     listJavaFiles(srcRoot: any): Result<List<any>> {
-        let javaFiles: List<any> = /* TODO */;
-        let stream: any = /* TODO */;
+        let javaFiles: List<any> = /* TODO */();
+        let stream: any = /* TODO */(/* TODO */);
         // TODO
         if (/* TODO */) {
             // TODO
@@ -47,12 +46,12 @@ export default class Main {
 
     transpileFile(srcRoot: any, outRoot: any, javaFile: any): Option<string> {
         // TODO
-        let javaSrc: string = /* TODO */;
-        let ts: string = /* TODO */;
-        let rel: any = /* TODO */;
-        let name: string = /* TODO */;
-        let withoutExt: string = /* TODO */;
-        let outFile: any = /* TODO */;
+        let javaSrc: string = /* TODO */(/* TODO */);
+        let ts: string = /* TODO */(/* TODO */);
+        let rel: any = /* TODO */(/* TODO */);
+        let name: string = /* TODO */();
+        let withoutExt: string = /* TODO */(/* TODO */, /* TODO */);
+        let outFile: any = /* TODO */(/* TODO */);
         /* TODO */(/* TODO */);
         /* TODO */(/* TODO */, /* TODO */);
         return /* TODO */;
--- src/main/node/com/example/Transpiler.ts
+++ src/main/node/com/example/Transpiler.ts
@@ -13,58 +13,60 @@ export default class Transpiler {
      * @return transpiled TypeScript
      */
     toTypeScript(javaSource: string): string {
-        let withoutPackage: string = /* TODO */;
-        let lines: string[] = /* TODO */;
-        let ts: any = /* TODO */;
+        let pkg: string = /* TODO */(/* TODO */);
+        let withoutPackage: string = /* TODO */(/* TODO */);
+        let withImports: string = /* TODO */(/* TODO */, /* TODO */);
+        let lines: string[] = /* TODO */(/* TODO */);
+        let ts: any = /* TODO */();
         // TODO
-        let classIdx: number = /* TODO */;
-        let enumIdx: number = /* TODO */;
-        let ifaceIdx: number = /* TODO */;
-        let brace: number = /* TODO */;
+        let classIdx: number = /* TODO */(/* TODO */);
+        let enumIdx: number = /* TODO */(/* TODO */);
+        let ifaceIdx: number = /* TODO */(/* TODO */);
+        let brace: number = /* TODO */(/* TODO */);
         let !: any = /* TODO */;
-        let afterClass: string = /* TODO */;
+        let afterClass: string = /* TODO */(/* TODO */);
         /* TODO */(/* TODO */);
         let !: any = /* TODO */;
-        let afterIface: string = /* TODO */;
+        let afterIface: string = /* TODO */(/* TODO */);
         /* TODO */(/* TODO */);
         let !: any = /* TODO */;
-        let afterEnum: string = /* TODO */;
+        let afterEnum: string = /* TODO */(/* TODO */);
         /* TODO */(/* TODO */);
         // TODO
         /* TODO */(/* TODO */);
         // TODO
         // TODO
-        let withMethods: string = /* TODO */;
-        let withFields: string = /* TODO */;
-        let withArrows: string = /* TODO */;
+        let withMethods: string = /* TODO */(/* TODO */);
+        let withFields: string = /* TODO */(/* TODO */);
+        let withArrows: string = /* TODO */(/* TODO */);
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let lines: string[] = /* TODO */;
-        let out: any = /* TODO */;
+        let lines: string[] = /* TODO */(/* TODO */);
+        let out: any = /* TODO */();
         let i: number = /* TODO */;
         while (/* TODO */) {
             // TODO
         }
         /* TODO */(/* TODO */, /* TODO */, /* TODO */, /* TODO */, /* TODO */);
-        let indent: string = /* TODO */;
-        let beforeBrace: string = /* TODO */;
-        let parenStart: number = /* TODO */;
-        let parenEnd: number = /* TODO */;
+        let indent: string = /* TODO */(/* TODO */, /* TODO */);
+        let beforeBrace: string = /* TODO */(/* TODO */, /* TODO */);
+        let parenStart: number = /* TODO */(/* TODO */);
+        let parenEnd: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let signatureStart: string = /* TODO */;
-        let params: string = /* TODO */;
-        let sigTokens: string[] = /* TODO */;
+        let signatureStart: string = /* TODO */(/* TODO */, /* TODO */);
+        let params: string = /* TODO */(/* TODO */, /* TODO */);
+        let sigTokens: string[] = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
         let name: string = /* TODO */;
         let returnType: string = /* TODO */;
-        let tsParams: string = /* TODO */;
-        let tsReturn: string = /* TODO */;
-        let stub: any = /* TODO */;
+        let tsParams: string = /* TODO */(/* TODO */);
+        let tsReturn: string = /* TODO */(/* TODO */);
+        let stub: any = /* TODO */();
         /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
@@ -74,7 +76,7 @@ export default class Transpiler {
         let i: any = /* TODO */;
         // TODO
         // TODO
-        let body: string = /* TODO */;
+        let body: string = /* TODO */();
         if (/* TODO */) {
             // TODO
         }
@@ -116,28 +118,28 @@ export default class Transpiler {
         /* TODO */(/* TODO */);
         // TODO
         /* TODO */(/* TODO */);
-        let lines: string[] = /* TODO */;
-        let out: any = /* TODO */;
+        let lines: string[] = /* TODO */(/* TODO */);
+        let out: any = /* TODO */();
         // TODO
-        let trimmed: string = /* TODO */;
+        let trimmed: string = /* TODO */();
         if (/* TODO */) {
             // TODO
         }
-        let indent: string = /* TODO */;
-        let withoutSemi: string = /* TODO */;
-        let eq: number = /* TODO */;
+        let indent: string = /* TODO */(/* TODO */, /* TODO */);
+        let withoutSemi: string = /* TODO */(/* TODO */, /* TODO */);
+        let eq: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let tokens: string[] = /* TODO */;
+        let tokens: string[] = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
         let name: string = /* TODO */;
         let type: string = /* TODO */;
-        let modArray: string[] = /* TODO */;
-        let modifiers: string = /* TODO */;
-        let tsType: string = /* TODO */;
+        let modArray: string[] = /* TODO */(/* TODO */, /* TODO */);
+        let modifiers: string = /* TODO */(/* TODO */);
+        let tsType: string = /* TODO */(/* TODO */);
         /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
@@ -158,8 +160,8 @@ export default class Transpiler {
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let lines: string[] = /* TODO */;
-        let out: any = /* TODO */;
+        let lines: string[] = /* TODO */(/* TODO */);
+        let out: any = /* TODO */();
         // TODO
         if (/* TODO */) {
             // TODO
@@ -168,10 +170,10 @@ export default class Transpiler {
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let lines: string[] = /* TODO */;
-        let out: any = /* TODO */;
+        let lines: string[] = /* TODO */(/* TODO */);
+        let out: any = /* TODO */();
         // TODO
-        let trimmed: string = /* TODO */;
+        let trimmed: string = /* TODO */();
         if (/* TODO */) {
             // TODO
         }
@@ -181,50 +183,54 @@ export default class Transpiler {
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */, /* TODO */);
-        let eq: number = /* TODO */;
+        let eq: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let dest: string = /* TODO */;
-        let tokens: string[] = /* TODO */;
+        let dest: string = /* TODO */(/* TODO */, /* TODO */);
+        let rhs: string = /* TODO */(/* TODO */);
+        let tokens: string[] = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let open: number = /* TODO */;
-        let close: number = /* TODO */;
+        let open: number = /* TODO */(/* TODO */);
+        let close: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let head: string = /* TODO */;
+        let head: string = /* TODO */(/* TODO */, /* TODO */);
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */, /* TODO */);
-        let open: number = /* TODO */;
-        let close: number = /* TODO */;
+        return /* TODO */;
+        // TODO
+        /* TODO */(/* TODO */);
+        let open: number = /* TODO */(/* TODO */);
+        let close: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let args: string = /* TODO */;
-        let count: number = /* TODO */;
-        let parts: java.util.List<string> = /* TODO */;
+        let args: string = /* TODO */(/* TODO */, /* TODO */);
+        let count: number = /* TODO */(/* TODO */, /* TODO */);
+        let parts: java.util.List<string> = /* TODO */();
         let i: any = /* TODO */;
         // TODO
         // TODO
         /* TODO */(/* TODO */);
         // TODO
-        let joined: string = /* TODO */;
+        let joined: string = /* TODO */(/* TODO */, /* TODO */, /* TODO */);
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
-        let out: java.util.List<string> = /* TODO */;
+        let out: java.util.List<string> = /* TODO */();
         // TODO
-        let parts: string[] = /* TODO */;
+        let parts: string[] = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
@@ -235,8 +241,8 @@ export default class Transpiler {
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let genericStart: number = /* TODO */;
-        let genericEnd: number = /* TODO */;
+        let genericStart: number = /* TODO */(/* TODO */);
+        let genericEnd: number = /* TODO */(/* TODO */);
         if (/* TODO */) {
             // TODO
         }
@@ -252,16 +258,78 @@ export default class Transpiler {
         // TODO
         // TODO
         /* TODO */(/* TODO */, /* TODO */, /* TODO */);
-        let base: string = /* TODO */;
-        let params: string = /* TODO */;
-        let mapped: java.util.List<string> = /* TODO */;
+        let base: string = /* TODO */(/* TODO */, /* TODO */);
+        let params: string = /* TODO */(/* TODO */, /* TODO */);
+        let mapped: java.util.List<string> = /* TODO */();
         // TODO
         /* TODO */(/* TODO */);
         // TODO
         return /* TODO */;
         // TODO
         /* TODO */(/* TODO */);
-        let trimmed: string = /* TODO */;
+        // TODO
+        let trimmed: string = /* TODO */();
+        if (/* TODO */) {
+            // TODO
+        }
+        // TODO
+        return /* TODO */;
+        // TODO
+        /* TODO */(/* TODO */, /* TODO */);
+        let lines: string[] = /* TODO */(/* TODO */);
+        let out: any = /* TODO */();
+        let i: any = /* TODO */;
+        // TODO
+        // TODO
+        let line: string = /* TODO */;
+        let trimmed: string = /* TODO */();
+        // TODO
+        // TODO
+        /* TODO */(/* TODO */);
+        let imp: string = /* TODO */(/* TODO */, /* TODO */);
+        let replacement: string = /* TODO */(/* TODO */, /* TODO */);
+        /* TODO */(/* TODO */);
+        /* TODO */(/* TODO */);
+        // TODO
+        let j: number = /* TODO */;
+        while (/* TODO */) {
+            // TODO
+        }
+        // TODO
+        // TODO
+        /* TODO */(/* TODO */);
+        // TODO
+        return /* TODO */;
+        // TODO
+        /* TODO */(/* TODO */, /* TODO */);
+        let parts: string[] = /* TODO */(/* TODO */);
+        let (parts.length: any = /* TODO */;
+        let className: string = /* TODO */;
+        let importPkgParts: string[] = /* TODO */(/* TODO */, /* TODO */);
+        let currentParts: string[] = /* TODO */(/* TODO */);
+        let i: number = /* TODO */;
+        while (/* TODO */) {
+            // TODO
+        }
+        let path: any = /* TODO */();
+        let j: any = /* TODO */;
+        // TODO
+        // TODO
+        /* TODO */(/* TODO */);
+        // TODO
+        let j: any = /* TODO */;
+        // TODO
+        // TODO
+        /* TODO */(/* TODO */);
+        // TODO
+        /* TODO */(/* TODO */);
+        if (/* TODO */) {
+            // TODO
+        }
+        return /* TODO */;
+        // TODO
+        /* TODO */(/* TODO */);
+        let trimmed: string = /* TODO */();
         if (/* TODO */) {
             // TODO
         }
