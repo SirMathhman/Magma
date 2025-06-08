ffdb744f489d7f9a4ce46b9f08267a478513f609
SirMathhman <sirmathhman@gmail.com>
2025-06-07 22:23:37 -0400

    Hmmm

--- src/main/node/magma/list/ListLike.ts
+++
@@ -1,8 +0,0 @@
-/** Minimal list abstraction mirroring java.util.List but without Iterable. */
-export interface ListLike<T> {
-    void add(T value);
-    T get(int index);
-    void set(int index, T value);
-    int size();
-    ListIter<T> iterator();
-}
