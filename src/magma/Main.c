8e1c4b7c8a78c890d372e0cb766328b4f86a3af6
SirMathhman <sirmathhman@gmail.com>
2025-06-09 09:55:36 -0400

    Separate content

--- src/magma/Main.c
+++ src/magma/Main.c
@@ -1,49 +1,68 @@
-/*
-
-public class Main {
-    public static void main(String[] args) {
+/*public class Main */{}
+/*public static void main(String[] args) {
         try {
-            final var source = Paths.get(".", "src", "magma", "Main.java");*//*
-            final var input = Files.readString(source);*//*
-            final var target = source.resolveSibling("Main.c");*//*
-
-            final var segments = new ArrayList<String>();*//*
-            var buffer = new StringBuilder();*//*
-            for (var i = 0;*//* i < input.length();*//* i++) {
-                final var c = input.charAt(i);*//*
-                buffer.append(c);*//*
-                if (c == ';*//*') {
-                    segments.add(buffer.toString());*//*
-                    buffer = new StringBuilder();*//*
-                }
-            }
-            segments.add(buffer.toString());*//*
-
-            final var output = new StringBuilder();*//*
+            final var source = Paths.get(".", "src", "magma", "Main.java");
+            final var input = Files.readString(source);
+            final var target = source.resolveSibling("Main.c");
+            final var segments = divide(input);
+            final var output = new StringBuilder();
             for (var segment : segments) {
-                output.append(compileRootSegment(segment));*//*
+                output.append(compileRootSegment(segment));
             }
 
-            Files.writeString(target, output.toString());*//*
+            Files.writeString(target, output.toString());
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
-            e.printStackTrace();*//*
+            e.printStackTrace();
         }
     }
 
+    private static List<String> divide(String input) {
+        final var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (var i = 0; i < input.length(); i++) {
+            final var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+            else {
+                if (c == '{') {
+                    depth++;
+                }
+                if (c == '}') {
+                    depth--;
+                }
+            }
+        }
+        segments.add(buffer.toString());
+        return segments;
+    }
+
     private static String compileRootSegment(String input) {
-        final var stripped = input.strip();*//*
+        final var stripped = input.strip();
         if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
-            return "";*//*
+            return "";
         }
 
-        return generatePlaceholder(input);*//*
+        final var contentStart = stripped.indexOf('{');
+        if (contentStart >= 0) {
+            final var beforeContent = stripped.substring(0, contentStart);
+            final var withEnd = stripped.substring(contentStart + "{".length()).strip();
+            if (withEnd.endsWith("}")) {
+                final var content = withEnd.substring(0, withEnd.length() - "}".length());
+                return generatePlaceholder(beforeContent) + "{}\n" + generatePlaceholder(content);
+            }
+        }
+
+        return generatePlaceholder(input);
     }
 
     private static String generatePlaceholder(String input) {
         return "start" + input
                 .replace("start", "start")
-                .replace("end", "end") + "end";*//*
+                .replace("end", "end") + "end";
     }
-}
 */
--- src/magma/Main.java
+++ src/magma/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,19 +12,7 @@ public class Main {
             final var source = Paths.get(".", "src", "magma", "Main.java");
             final var input = Files.readString(source);
             final var target = source.resolveSibling("Main.c");
-
-            final var segments = new ArrayList<String>();
-            var buffer = new StringBuilder();
-            for (var i = 0; i < input.length(); i++) {
-                final var c = input.charAt(i);
-                buffer.append(c);
-                if (c == ';') {
-                    segments.add(buffer.toString());
-                    buffer = new StringBuilder();
-                }
-            }
-            segments.add(buffer.toString());
-
+            final var segments = divide(input);
             final var output = new StringBuilder();
             for (var segment : segments) {
                 output.append(compileRootSegment(segment));
@@ -36,10 +25,44 @@ public class Main {
         }
     }
 
+    private static List<String> divide(String input) {
+        final var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (var i = 0; i < input.length(); i++) {
+            final var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+            else {
+                if (c == '{') {
+                    depth++;
+                }
+                if (c == '}') {
+                    depth--;
+                }
+            }
+        }
+        segments.add(buffer.toString());
+        return segments;
+    }
+
     private static String compileRootSegment(String input) {
         final var stripped = input.strip();
         if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
             return "";
+        }
+
+        final var contentStart = stripped.indexOf('{');
+        if (contentStart >= 0) {
+            final var beforeContent = stripped.substring(0, contentStart);
+            final var withEnd = stripped.substring(contentStart + "{".length()).strip();
+            if (withEnd.endsWith("}")) {
+                final var content = withEnd.substring(0, withEnd.length() - "}".length());
+                return generatePlaceholder(beforeContent) + "{}\n" + generatePlaceholder(content);
+            }
         }
 
         return generatePlaceholder(input);
