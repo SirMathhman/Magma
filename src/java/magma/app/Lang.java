1fe1acd4c7c940b2b5009b9f561c66bf2758309a
SirMathhman <sirmathhman@gmail.com>
2025-06-13 23:12:06 -0400

    Cleanup

--- src/java/magma/app/Lang.java
+++ src/java/magma/app/Lang.java
@@ -4,29 +4,29 @@ import magma.app.node.CompoundNode;
 import magma.app.rule.DivideRule;
 import magma.app.rule.InfixRule;
 import magma.app.rule.PrefixRule;
-import magma.app.rule.factory.PropertiesCompoundNodeFactory;
 import magma.app.rule.Rule;
 import magma.app.rule.StringRule;
 import magma.app.rule.StripRule;
 import magma.app.rule.SuffixRule;
+import magma.app.rule.factory.PropertiesCompoundNodeFactory;
 
 public class Lang {
     public static Rule<CompoundNode> createJavaRootRule() {
-        return new DivideRule<CompoundNode>("children", createImportRule(), new PropertiesCompoundNodeFactory());
+        return new DivideRule<>("children", createImportRule(), new PropertiesCompoundNodeFactory());
     }
 
     public static Rule<CompoundNode> createPlantRootRule() {
-        return new DivideRule<CompoundNode>("children", createDependencyRule(), new PropertiesCompoundNodeFactory());
+        return new DivideRule<>("children", createDependencyRule(), new PropertiesCompoundNodeFactory());
     }
 
     static Rule<CompoundNode> createImportRule() {
-        final var parent = new StringRule<CompoundNode>("parent", new PropertiesCompoundNodeFactory());
-        final var destination = new StringRule<CompoundNode>("destination", new PropertiesCompoundNodeFactory());
-        final var rule = new PrefixRule<CompoundNode>("import ", new SuffixRule<CompoundNode>(new InfixRule<CompoundNode>(parent, ".", destination), ";"));
-        return new StripRule<CompoundNode>(rule);
+        final var parent = new StringRule<>("parent", new PropertiesCompoundNodeFactory());
+        final var destination = new StringRule<>("destination", new PropertiesCompoundNodeFactory());
+        final var rule = new PrefixRule<>("import ", new SuffixRule<>(new InfixRule<>(parent, ".", destination), ";"));
+        return new StripRule<>(rule);
     }
 
     static Rule<CompoundNode> createDependencyRule() {
-        return new SuffixRule<CompoundNode>(new InfixRule<CompoundNode>(new StringRule<CompoundNode>("source", new PropertiesCompoundNodeFactory()), " --> ", new StringRule<CompoundNode>("destination", new PropertiesCompoundNodeFactory())), "\n");
+        return new SuffixRule<>(new InfixRule<>(new StringRule<>("source", new PropertiesCompoundNodeFactory()), " --> ", new StringRule<>("destination", new PropertiesCompoundNodeFactory())), "\n");
     }
 }
