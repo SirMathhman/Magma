78661800d750cc01137bb1b536e61eb76d3c9bce
SirMathhman <sirmathhman@gmail.com>
2025-06-13 21:02:29 -0400

    Cleanup

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,9 +4,11 @@ import magma.app.State;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -15,16 +17,7 @@ public class Main {
         try (var stream = Files.walk(Paths.get(".", "src", "java"))) {
             final var sources = stream.filter(Files::isRegularFile).filter(path -> path.toString().endsWith(".java")).collect(Collectors.toSet());
 
-            final var output1 = new StringBuilder();
-            for (var source : sources) {
-                final var input = Files.readString(source);
-                final var fileName = source.getFileName().toString();
-                final var name = fileName.substring(0, fileName.lastIndexOf("."));
-                output1.append("class " + name + "\n" + compile(input, name));
-            }
-
-            final var output = "@startuml\nskinparam linetype ortho\n" + output1 + "@enduml";
-
+            final var output = compileSources(sources);
             final var target = Paths.get(".", "diagram.puml");
             Files.writeString(target, output);
         } catch (IOException e) {
@@ -33,7 +26,23 @@ public class Main {
         }
     }
 
-    private static String compile(String input, String name) {
+    private static String compileSources(Set<Path> sources) throws IOException {
+        final var output1 = new StringBuilder();
+        for (var source : sources) {
+            output1.append(compileSource(source));
+        }
+
+        return "@startuml\nskinparam linetype ortho\n" + output1 + "@enduml";
+    }
+
+    private static String compileSource(Path source) throws IOException {
+        final var input = Files.readString(source);
+        final var fileName = source.getFileName().toString();
+        final var name = fileName.substring(0, fileName.lastIndexOf("."));
+        return "class " + name + "\n" + compileInput(input, name);
+    }
+
+    private static String compileInput(String input, String name) {
         final var segments = divide(input);
 
         final var output = new StringBuilder();
@@ -70,8 +79,6 @@ public class Main {
     private static State fold(State state, char c) {
         final var appended = state.append(c);
         if (c == ';') return appended.advance();
-
         return appended;
     }
-
 }
