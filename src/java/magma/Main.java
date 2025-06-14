6fec461f8c0b4840189cabe6e3a7430413454cd3
SirMathhman <sirmathhman@gmail.com>
2025-06-13 20:29:24 -0400

    Dependencies

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,12 +3,46 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            final var input = Files.readString(source);
+
+            final var segments = new ArrayList<String>();
+            var buffer = new StringBuilder();
+            for (var i = 0; i < input.length(); i++) {
+                final var c = input.charAt(i);
+                buffer.append(c);
+                if (c == ';') {
+                    segments.add(buffer.toString());
+                    buffer = new StringBuilder();
+                }
+            }
+            segments.add(buffer.toString());
+
+            final var output = new StringBuilder();
+            for (var segment : segments) {
+                final var stripped = segment.strip();
+                if (stripped.startsWith("import ")) {
+                    final var substring = stripped.substring("import ".length());
+                    if (substring.endsWith(";")) {
+                        final var substring1 = substring.substring(0, substring.length() - ";".length());
+                        final var index = substring1.lastIndexOf(".");
+                        if (index >= 0) {
+                            final var substring2 = substring1.substring(index + 1);
+                            output.append("Main --> " + substring2 + "\n");
+                        }
+                    }
+                }
+            }
+
             final var target = Paths.get(".", "diagram.puml");
-            Files.writeString(target, "@startuml\n@enduml");
+            Files.writeString(target, "@startuml\nskinparam linetype ortho\nclass Main\n" +
+                    output +
+                    "@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
