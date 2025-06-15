5c01a3a64fa7e226764a30d95ffa87a59313d4a4
SirMathhman <sirmathhman@gmail.com>
2025-06-15 03:44:14 -0400

    Remove dependency

--- src/java/magma/app/rule/or/InlineOrState.java
+++ src/java/magma/app/rule/or/InlineOrState.java
@@ -1,29 +1,28 @@
 package magma.app.rule.or;
 
-import magma.app.CompileError;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public record InlineOrState<Value>(Optional<Value> maybeValue, List<CompileError> errors) implements OrState<Value, CompileError> {
+public record InlineOrState<Value, Error>(Optional<Value> maybeValue,
+                                          List<Error> errors) implements OrState<Value, Error> {
     public InlineOrState() {
         this(Optional.empty(), new ArrayList<>());
     }
 
     @Override
-    public <Return> Return match(Function<Value, Return> whenPresent, Function<List<CompileError>, Return> whenMissing) {
+    public <Return> Return match(Function<Value, Return> whenPresent, Function<List<Error>, Return> whenMissing) {
         return this.maybeValue.map(whenPresent).orElseGet(() -> whenMissing.apply(this.errors));
     }
 
     @Override
-    public OrState<Value, CompileError> withValue(Value value) {
+    public OrState<Value, Error> withValue(Value value) {
         return new InlineOrState<>(Optional.of(value), this.errors);
     }
 
     @Override
-    public OrState<Value, CompileError> withError(CompileError error) {
+    public OrState<Value, Error> withError(Error error) {
         this.errors.add(error);
         return this;
     }
