a343e8e8db85973d8de43dda7590d396eca875fb
SirMathhman <sirmathhman@gmail.com>
2025-06-15 11:30:01 -0400

    Cleanup

--- src/java/magma/app/compile/Compiler.java
+++ src/java/magma/app/compile/Compiler.java
@@ -1,18 +1,18 @@
 package magma.app.compile;
 
 import magma.app.compile.lang.Lang;
-import magma.app.compile.string.StringResults;
 import magma.app.compile.node.PresentNodeListResult;
 import magma.app.compile.rule.DivideState;
 import magma.app.compile.rule.EmptyRule;
 import magma.app.compile.rule.OrRule;
 import magma.app.compile.rule.StringRule;
+import magma.app.compile.string.StringResults;
 
 import java.util.List;
 
 public class Compiler {
     public static StringResult<CompileError> compileRoot(String input, String name) {
-        return lex(input, new OrRule<Node>(List.of(Lang.createImportRule(), new StringRule("value")))).transform(children -> transform(name, children)).generate(Compiler::generate);
+        return lex(input, new OrRule<>(List.of(Lang.createImportRule(), new StringRule("value")))).transform(children -> transform(name, children)).generate(Compiler::generate);
     }
 
     static List<String> divide(String input) {
@@ -33,7 +33,7 @@ public class Compiler {
     }
 
     public static StringResult<CompileError> generate(List<Node> children) {
-        return children.stream().map(node -> new OrRule<Node>(List.of(Lang.createDependencyRule(), new EmptyRule())).generate(node)).<StringResult<CompileError>>reduce(StringResults.createFromValue(""), (compileErrorOkStringResult, other) -> compileErrorOkStringResult.appendMaybe(other), (_, next) -> next);
+        return children.stream().map(node -> new OrRule<>(List.of(Lang.createDependencyRule(), new EmptyRule())).generate(node)).reduce(StringResults.createFromValue(""), (compileErrorOkStringResult, other) -> compileErrorOkStringResult.appendMaybe(other), (_, next) -> next);
     }
 
     public static List<Node> transform(String name, List<Node> list) {
@@ -41,6 +41,6 @@ public class Compiler {
     }
 
     public static NodeListResult<Node, CompileError> lex(String input, Rule<Node, NodeResult<Node, CompileError>, StringResult<CompileError>> rule) {
-        return divide(input).stream().map(rule::lex).<NodeListResult<Node, CompileError>>reduce(new PresentNodeListResult<Node, CompileError>(), NodeListResult::add, (_, next) -> next);
+        return divide(input).stream().map(rule::lex).<NodeListResult<Node, CompileError>>reduce(new PresentNodeListResult<>(), NodeListResult::add, (_, next) -> next);
     }
 }
