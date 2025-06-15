cf7bdbdbfa117881cf1db0212786609bf6205380
SirMathhman <sirmathhman@gmail.com>
2025-06-15 01:04:25 -0400

    State

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,9 +1,11 @@
 package magma;
 
+import magma.app.State;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,18 +19,7 @@ public class Main {
                 final var name = fileName.substring(0, separator);
                 output.append("class " + name + "\n");
 
-                final var input = Files.readString(source);
-                final var segments = new ArrayList<String>();
-                var buffer = new StringBuilder();
-                for (var i = 0; i < input.length(); i++) {
-                    final var c = input.charAt(i);
-                    buffer.append(c);
-                    if (c == ';') {
-                        segments.add(buffer.toString());
-                        buffer = new StringBuilder();
-                    }
-                }
-                segments.add(buffer.toString());
+                final var segments = divide(Files.readString(source));
 
                 final var result = new StringBuilder();
                 for (var segment : segments) {
@@ -55,5 +46,22 @@ public class Main {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static List<String> divide(String input) throws IOException {
+        var current = new State();
+        for (var i = 0; i < input.length(); i++) {
+            final var c = input.charAt(i);
+            current = fold(current, c);
+        }
+
+        return current.advance().segments();
+    }
+
+    private static State fold(State state, char c) {
+        final var appended = state.append(c);
+        if (c == ';')
+            return appended.advance();
+        return appended;
     }
 }
