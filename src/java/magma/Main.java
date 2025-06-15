f63191f429e7b902f29f3a40d39c153aa61b40c6
SirMathhman <sirmathhman@gmail.com>
2025-06-15 00:58:38 -0400

    Hmmm

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -6,9 +6,21 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+        try (final var stream = Files.walk(Paths.get(".", "src", "java"))) {
+            final var sources = stream.filter(Files::isRegularFile).filter(path -> path.toString().endsWith(".java")).toList();
+
+            final var buffer = new StringBuilder();
+            for (var source : sources) {
+                final var fileName = source.getFileName().toString();
+                final var separator = fileName.lastIndexOf(".");
+                final var name = fileName.substring(0, separator);
+                buffer.append("class " + name + "\n");
+            }
+
             final var target = Paths.get(".", "diagram.puml");
-            Files.writeString(target, "@startuml\nskinparam linetype ortho\nclass Main\n@enduml");
+            Files.writeString(target, "@startuml\nskinparam linetype ortho\n" +
+                    buffer +
+                    "@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
