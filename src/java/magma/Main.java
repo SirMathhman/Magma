6bf1da917ad41da1f4498ac92587405ac260441f
SirMathhman <sirmathhman@gmail.com>
2025-06-15 12:17:59 -0400

    Main

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,11 +3,26 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Files.writeString(Paths.get(".", "diagram.puml"), "@startuml\n@enduml");
+        try (final var stream = Files.walk(Paths.get(".", "src", "java"))) {
+            final var sources = stream.filter(Files::isRegularFile)
+                    .filter(path -> path.toString()
+                            .endsWith(".java"))
+                    .collect(Collectors.toSet());
+
+            final var output = new StringBuilder();
+            for (var source : sources) {
+                final var fileName = source.getFileName()
+                        .toString();
+                final var separator = fileName.lastIndexOf(".");
+                final var name = fileName.substring(0, separator);
+                output.append("class " + name + "\n");
+            }
+
+            Files.writeString(Paths.get(".", "diagram.puml"), "@startuml\n" + output + "@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
