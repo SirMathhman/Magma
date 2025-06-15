8e70cc1d11a31eebdeaa0d03bd78f7d705236005
SirMathhman <sirmathhman@gmail.com>
2025-06-15 12:23:11 -0400

    State

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,9 +1,12 @@
 package magma;
 
+import magma.app.State;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -21,20 +24,7 @@ public class Main {
                 final var extensionSeparator = fileName.lastIndexOf(".");
                 final var name = fileName.substring(0, extensionSeparator);
                 output.append("class " + name + "\n");
-
-                final var input = Files.readString(source);
-                final var segments = new ArrayList<String>();
-                var buffer = new StringBuilder();
-                for (var i = 0; i < input.length(); i++) {
-                    final var c = input.charAt(i);
-                    buffer.append(c);
-                    if (c == ';') {
-                        segments.add(buffer.toString());
-                        buffer = new StringBuilder();
-                    }
-                }
-                segments.add(buffer.toString());
-
+                final var segments = divide(Files.readString(source));
                 for (var segment : segments) {
                     final var stripped = segment.strip();
                     if (stripped.startsWith("import ")) {
@@ -56,5 +46,27 @@ public class Main {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static List<String> divide(String input) {
+        return getStrings(input, new State());
+    }
+
+    private static List<String> getStrings(String input, State state) {
+        var current = state;
+        for (var i = 0; i < input.length(); i++) {
+            final var c = input.charAt(i);
+            current = fold(current, c);
+        }
+
+        return current.advance()
+                .segments();
+    }
+
+    private static State fold(State state, char c) {
+        final var appended = state.append(c);
+        if (c == ';')
+            return appended.advance();
+        return appended;
     }
 }
