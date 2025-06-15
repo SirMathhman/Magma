9d9a1e05d745ac65438bf8c10d7358fc805ecaca
SirMathhman <sirmathhman@gmail.com>
2025-06-15 00:55:26 -0400

    Content

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             final var target = Paths.get(".", "diagram.puml");
-            Files.writeString(target, "");
+            Files.writeString(target, "@startuml\nskinparam linetype ortho\nclass Main\n@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
