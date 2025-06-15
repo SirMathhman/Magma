1850bca32cc6fc45d8895e6370735aee9ca19fba
SirMathhman <sirmathhman@gmail.com>
2025-06-15 13:36:50 -0400

    Weaken

--- src/java/magma/app/compile/Lang.java
+++ src/java/magma/app/compile/Lang.java
@@ -1,5 +1,6 @@
 package magma.app.compile;
 
+import magma.app.compile.rule.Rule;
 import magma.app.compile.rule.divide.DivideRule;
 import magma.app.compile.rule.LastRule;
 import magma.app.compile.rule.PrefixRule;
@@ -8,21 +9,21 @@ import magma.app.compile.rule.StripRule;
 import magma.app.compile.rule.SuffixRule;
 
 public class Lang {
-    public static DivideRule createJavaRootRule() {
+    public static Rule createJavaRootRule() {
         return new DivideRule("children", createImportRule());
     }
 
-    public static DivideRule createPlantUMLRootRule() {
+    public static Rule createPlantUMLRootRule() {
         return new DivideRule("children", createDependencyRule());
     }
 
-    private static SuffixRule createDependencyRule() {
+    private static Rule createDependencyRule() {
         final var parent = new StringRule("parent");
         final var child = new StringRule("child");
         return new SuffixRule(new LastRule(parent, " --> ", child), "\n");
     }
 
-    private static StripRule createImportRule() {
+    private static Rule createImportRule() {
         return new StripRule(new PrefixRule("import ", new SuffixRule(new LastRule(new StringRule("parent"), ".", new StringRule("child")), ";")));
     }
 }
