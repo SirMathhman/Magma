c25ff9854fef5b4ec64f53331d6db809cd63c114
SirMathhman <sirmathhman@gmail.com>
2025-06-15 12:20:43 -0400

    With imports

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -17,12 +18,40 @@ public class Main {
             for (var source : sources) {
                 final var fileName = source.getFileName()
                         .toString();
-                final var separator = fileName.lastIndexOf(".");
-                final var name = fileName.substring(0, separator);
+                final var extensionSeparator = fileName.lastIndexOf(".");
+                final var name = fileName.substring(0, extensionSeparator);
                 output.append("class " + name + "\n");
+
+                final var input = Files.readString(source);
+                final var segments = new ArrayList<String>();
+                var buffer = new StringBuilder();
+                for (var i = 0; i < input.length(); i++) {
+                    final var c = input.charAt(i);
+                    buffer.append(c);
+                    if (c == ';') {
+                        segments.add(buffer.toString());
+                        buffer = new StringBuilder();
+                    }
+                }
+                segments.add(buffer.toString());
+
+                for (var segment : segments) {
+                    final var stripped = segment.strip();
+                    if (stripped.startsWith("import ")) {
+                        final var withoutPrefix = stripped.substring("import ".length());
+                        if (withoutPrefix.endsWith(";")) {
+                            final var withoutSuffix = withoutPrefix.substring(0, withoutPrefix.length() - ";".length());
+                            final var separator = withoutSuffix.lastIndexOf(".");
+                            if (separator >= 0) {
+                                final var child = withoutSuffix.substring(separator + ".".length());
+                                output.append(name + " --> " + child + "\n");
+                            }
+                        }
+                    }
+                }
             }
 
-            Files.writeString(Paths.get(".", "diagram.puml"), "@startuml\n" + output + "@enduml");
+            Files.writeString(Paths.get(".", "diagram.puml"), "@startuml\nskinparam linetype ortho\n" + output + "@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
