d978f0960aafce3390af25174be5e2b0dabb964a
SirMathhman <sirmathhman@gmail.com>
2025-06-17 01:43:04 -0400

    Revert "Disconnect"
    This reverts commit 02ca02c861f475aca46cc55c25cc47cd1b87bb30.

--- src/java/magma/app/compile/result/SimpleResultFactory.java
+++ src/java/magma/app/compile/result/SimpleResultFactory.java
@@ -7,7 +7,7 @@ import magma.app.compile.context.Context;
 import magma.app.compile.error.CompileError;
 import magma.app.compile.error.FormattedError;
 
-public class SimpleResultFactory<T> implements ResultFactory<T, Result<T, FormattedError>> {
+public class SimpleResultFactory<T> implements ResultFactory<T, FormattedError> {
     public SimpleResultFactory() {
     }
 
