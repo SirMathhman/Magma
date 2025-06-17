5150789dc9dabd4a90ef95b407e38bc0977c0c36
SirMathhman <sirmathhman@gmail.com>
2025-06-17 01:17:47 -0400

    Fix StripRule

--- src/java/magma/app/compile/rule/StripRule.java
+++ src/java/magma/app/compile/rule/StripRule.java
@@ -1,17 +1,13 @@
 package magma.app.compile.rule;
 
-import magma.api.result.Result;
-import magma.app.compile.error.FormattedError;
-
-public record StripRule<Node>(
-        Rule<Node, Result<Node, FormattedError>, Result<String, FormattedError>> rule) implements Rule<Node, Result<Node, FormattedError>, Result<String, FormattedError>> {
+public record StripRule<Node, Lex, Generate>(Rule<Node, Lex, Generate> rule) implements Rule<Node, Lex, Generate> {
     @Override
-    public Result<Node, FormattedError> lex(String segment) {
+    public Lex lex(String segment) {
         return this.rule.lex(segment.strip());
     }
 
     @Override
-    public Result<String, FormattedError> generate(Node node) {
+    public Generate generate(Node node) {
         return this.rule.generate(node);
     }
 }
