8ae6ef487d7c64454c9f7c23eac3a25ddbcb801f
SirMathhman <sirmathhman@gmail.com>
2025-06-16 23:18:12 -0400

    Packaging?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -5,23 +5,54 @@ import magma.state.State;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
-            final var input = Files.readString(source);
-            final var segments = divide(input);
-            final var output = compileSegments(segments);
+        final var sourceDirectory = Paths.get(".", "src", "java");
+        try (var files = Files.walk(sourceDirectory)) {
+            final var sources = files.filter(Files::isRegularFile)
+                    .filter(path -> path.toString()
+                            .endsWith("java"))
+                    .toList();
+
+            final var stringBuilder = new StringBuilder();
+            for (var source : sources)
+                stringBuilder.append(compileSource(sourceDirectory, source));
+
             final var target = Paths.get(".", "diagram.puml");
-            Files.writeString(target, "@startuml\nskinparam linetype ortho\nclass magma.Main\n" + output + "@enduml");
+            Files.writeString(target, "@startuml\nskinparam linetype ortho\n" + stringBuilder + "@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compileSource(Path sourceDirectory, Path source) throws IOException {
+        final var relativeParent = sourceDirectory.relativize(source)
+                .getParent();
+
+        final Collection<String> namespace = new ArrayList<>();
+        for (var i = 0; i < relativeParent.getNameCount(); i++)
+            namespace.add(relativeParent.getName(i)
+                    .toString());
+
+        final var fileName = source.getFileName()
+                .toString();
+        final var separator = fileName.lastIndexOf(".");
+        final var name = fileName.substring(0, separator);
+
+        final var joined = String.join(".", namespace);
+        final var input = Files.readString(source);
+        final var segments = divide(input);
+        final var joinedName = joined + "." + name;
+        final var output = compileSegments(segments, joinedName);
+        return "class " + joinedName + "\n" + output;
     }
 
     private static List<String> divide(CharSequence input) {
@@ -42,14 +73,14 @@ public class Main {
         return appended;
     }
 
-    private static StringBuilder compileSegments(Iterable<String> segments) {
+    private static StringBuilder compileSegments(Iterable<String> segments, String name) {
         final var output = new StringBuilder();
         for (var segment : segments)
-            compileImport(segment).ifPresent(output::append);
+            compileImport(segment, name).ifPresent(output::append);
         return output;
     }
 
-    private static Optional<String> compileImport(String segment) {
+    private static Optional<String> compileImport(String segment, String name) {
         final var stripped = segment.strip();
         if (!stripped.startsWith("import "))
             return Optional.empty();
@@ -59,6 +90,6 @@ public class Main {
             return Optional.empty();
 
         final var withoutEnd = withoutStart.substring(0, withoutStart.length() - ";".length());
-        return Optional.of("magma.Main --> " + withoutEnd + "\n");
+        return Optional.of(name + " --> " + withoutEnd + "\n");
     }
 }
