5d76669d69504abb2ccdfefaaa64fd5315c232b8
SirMathhman <sirmathhman@gmail.com>
2025-06-16 22:43:33 -0400

    Simple

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,17 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            final var target = Paths.get(".", "diagram.puml");
+            Files.writeString(target, "@startuml\nclass Main\n@enduml");
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
