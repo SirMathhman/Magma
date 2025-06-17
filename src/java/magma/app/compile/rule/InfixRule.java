7a02f9c9a500d4121e568faea04915c38a8f16d3
SirMathhman <sirmathhman@gmail.com>
2025-06-17 02:09:16 -0400

    Hmmm

--- src/java/magma/app/compile/rule/InfixRule.java
+++ src/java/magma/app/compile/rule/InfixRule.java
@@ -4,8 +4,6 @@ import magma.api.result.Result;
 import magma.app.compile.error.ResultFactory;
 import magma.app.compile.node.MergingNode;
 
-import java.util.Objects;
-
 public final class InfixRule<Node extends MergingNode<Node>, Error> implements Rule<Node, Result<Node, Error>, Result<String, Error>> {
     private final Rule<Node, Result<Node, Error>, Result<String, Error>> leftRule;
     private final String infix;
@@ -38,39 +36,4 @@ public final class InfixRule<Node extends MergingNode<Node>, Error> implements R
                 .flatMapValue(leftResult -> this.rightRule.generate(node)
                         .mapValue(rightResult -> leftResult + this.infix + rightResult));
     }
-
-    public Rule<Node, Result<Node, Error>, Result<String, Error>> leftRule() {
-        return this.leftRule;
-    }
-
-    public String infix() {
-        return this.infix;
-    }
-
-    public Rule<Node, Result<Node, Error>, Result<String, Error>> rightRule() {
-        return this.rightRule;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null || obj.getClass() != this.getClass())
-            return false;
-        var that = (InfixRule) obj;
-        return Objects.equals(this.leftRule, that.leftRule) && Objects.equals(this.infix, that.infix) && Objects.equals(
-                this.rightRule,
-                that.rightRule);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.leftRule, this.infix, this.rightRule);
-    }
-
-    @Override
-    public String toString() {
-        return "InfixRule[" + "leftRule=" + this.leftRule + ", " + "infix=" + this.infix + ", " + "rightRule=" + this.rightRule + ']';
-    }
-
 }
