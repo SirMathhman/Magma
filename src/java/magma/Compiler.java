3c313a332bc5709c63d7e070b001761b78891447
SirMathhman <sirmathhman@gmail.com>
2025-06-17 00:49:54 -0400

    Extract Compiler

---
+++ src/java/magma/Compiler.java
@@ -0,0 +1,86 @@
+package magma;
+
+import magma.api.result.Ok;
+import magma.api.result.Result;
+import magma.app.compile.CompileError;
+import magma.app.compile.node.MapNode;
+import magma.app.compile.node.Node;
+import magma.app.compile.rule.InfixRule;
+import magma.app.compile.rule.OrRule;
+import magma.app.compile.rule.PrefixRule;
+import magma.app.compile.rule.Rule;
+import magma.app.compile.rule.StringRule;
+import magma.app.compile.rule.StripRule;
+import magma.app.compile.rule.SuffixRule;
+import magma.app.compile.rule.TypeRule;
+import magma.app.compile.rule.divide.DivideRule;
+import magma.app.error.ApplicationError;
+import magma.app.io.Source;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Compiler {
+    static Result<String, ApplicationError> compileSource(Source source, String input) {
+        final var namespace = source.computeNamespace();
+        final var name = source.computeName();
+
+        final var joined = String.join(".", namespace);
+        final var joinedName = joined + "." + name;
+        return compileRoot(input, joinedName).mapValue(output -> "class " + joinedName + "\n" + output)
+                .mapErr(ApplicationError::new);
+    }
+
+    static Result<String, CompileError> compileRoot(String input, String source) {
+        return createJavaRootRule().lex(input)
+                .mapValue(children -> transform(source, children))
+                .flatMapValue(createPlantRootRule()::generate);
+    }
+
+    static Rule createJavaRootRule() {
+        return new DivideRule("children",
+                new OrRule(List.of(createNamespacedRule("package"),
+                        new TypeRule("import", createNamespacedRule("import")),
+                        createStructureRule("class"),
+                        createStructureRule("interface"),
+                        createStructureRule("record"))));
+    }
+
+    static Rule createStructureRule(String type) {
+        return new InfixRule(new StringRule("before-infix"), type + " ", new StringRule("after-infix"));
+    }
+
+    static Rule createPlantRootRule() {
+        return new DivideRule("children", new OrRule(List.of(createDependencyRule())));
+    }
+
+    static Node transform(String source, Node root) {
+        final var transformed = root.findNodeList("children")
+                .orElse(new ArrayList<>())
+                .stream()
+                .filter(node -> node.is("import"))
+                .map(node -> node.withString("source", source))
+                .toList();
+
+        final Node node = new MapNode();
+        return node.withNodeList("children", transformed);
+    }
+
+    static Rule createNamespacedRule(String type) {
+        return new StripRule(new PrefixRule(type + " ", new SuffixRule(new StringRule("destination"), ";")));
+    }
+
+    static Rule createDependencyRule() {
+        return new SuffixRule(new InfixRule(new StringRule("source"), " --> ", new StringRule("destination")), "\n");
+    }
+
+    Result<String, ApplicationError> compile(Map<Source, String> inputs) {
+        Result<StringBuilder, ApplicationError> maybeCurrentOutput = new Ok<>(new StringBuilder());
+        for (var entry : inputs.entrySet())
+            maybeCurrentOutput = maybeCurrentOutput.flatMapValue(currentOutput -> compileSource(entry.getKey(),
+                    entry.getValue()).mapValue(currentOutput::append));
+
+        return maybeCurrentOutput.mapValue(StringBuilder::toString);
+    }
+}
--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,18 +3,6 @@ package magma;
 import magma.api.result.Err;
 import magma.api.result.Ok;
 import magma.api.result.Result;
-import magma.app.compile.CompileError;
-import magma.app.compile.node.MapNode;
-import magma.app.compile.node.Node;
-import magma.app.compile.rule.InfixRule;
-import magma.app.compile.rule.OrRule;
-import magma.app.compile.rule.PrefixRule;
-import magma.app.compile.rule.Rule;
-import magma.app.compile.rule.StringRule;
-import magma.app.compile.rule.StripRule;
-import magma.app.compile.rule.SuffixRule;
-import magma.app.compile.rule.TypeRule;
-import magma.app.compile.rule.divide.DivideRule;
 import magma.app.error.ApplicationError;
 import magma.app.error.ThrowableError;
 import magma.app.io.PathSource;
@@ -24,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,53 +27,20 @@ public class Main {
     }
 
     private static Optional<ApplicationError> collect(Iterable<Path> sources, Path sourceDirectory) {
-        return readAll(sources, sourceDirectory).match(inputs -> {
-            return compile(inputs).match(currentOutput -> {
-                final var target = Paths.get(".", "diagram.puml");
-                final var content = "@startuml\nskinparam linetype ortho\n" + currentOutput + "@enduml";
-                return writeString(target, content).map(ThrowableError::new)
-                        .map(ApplicationError::new);
-            }, Optional::of);
-        }, Optional::of);
-    }
-
-    private static Result<String, ApplicationError> compile(Map<Source, String> inputs) {
-        Result<StringBuilder, ApplicationError> maybeCurrentOutput = new Ok<>(new StringBuilder());
-        for (var entry : inputs.entrySet())
-            maybeCurrentOutput = maybeCurrentOutput.flatMapValue(currentOutput -> {
-                return compileSource(entry.getKey(), entry.getValue()).mapValue(currentOutput::append);
-            });
-
-        return maybeCurrentOutput.mapValue(StringBuilder::toString);
+        return readAll(sources, sourceDirectory).match(inputs -> new Compiler().compile(inputs)
+                .match(currentOutput -> {
+                    final var target = Paths.get(".", "diagram.puml");
+                    final var content = "@startuml\nskinparam linetype ortho\n" + currentOutput + "@enduml";
+                    return writeString(target, content).map(ThrowableError::new)
+                            .map(ApplicationError::new);
+                }, Optional::of), Optional::of);
     }
 
     private static Result<Map<Source, String>, ApplicationError> readAll(Iterable<Path> sources, Path sourceDirectory) {
         Result<Map<Source, String>, ApplicationError> maybeInputs = new Ok<>(new HashMap<>());
         for (var source : sources)
-            maybeInputs = getMaybeInputs(sourceDirectory, source, maybeInputs);
+            maybeInputs = readFile(sourceDirectory, source, maybeInputs);
         return maybeInputs;
-    }
-
-    private static Result<Map<Source, String>, ApplicationError> getMaybeInputs(Path sourceDirectory, Path source, Result<Map<Source, String>, ApplicationError> maybeInputs) {
-        return maybeInputs.flatMapValue(inner -> readString(source).mapErr(ThrowableError::new)
-                .mapErr(ApplicationError::new)
-                .mapValue(input -> {
-                    inner.put(new PathSource(sourceDirectory, source), input);
-                    return inner;
-                }));
-    }
-
-    private static Result<List<Path>, IOException> collect(Path sourceDirectory) {
-        try (var files = Files.walk(sourceDirectory)) {
-            final var sources = files.filter(Files::isRegularFile)
-                    .filter(path -> path.toString()
-                            .endsWith("java"))
-                    .toList();
-
-            return new Ok<>(sources);
-        } catch (IOException e) {
-            return new Err<>(e);
-        }
     }
 
     private static Optional<IOException> writeString(Path target, CharSequence content) {
@@ -98,17 +52,7 @@ public class Main {
         }
     }
 
-    private static Result<String, ApplicationError> compileSource(Source source, String input) {
-        final var namespace = source.computeNamespace();
-        final var name = source.computeName();
-
-        final var joined = String.join(".", namespace);
-        final var joinedName = joined + "." + name;
-        return compileRoot(input, joinedName).mapValue(output -> "class " + joinedName + "\n" + output)
-                .mapErr(ApplicationError::new);
-    }
-
-    private static Result<String, IOException> readString(Path source) {
+    public static Result<String, IOException> readString(Path source) {
         try {
             return new Ok<>(Files.readString(source));
         } catch (IOException e) {
@@ -116,46 +60,25 @@ public class Main {
         }
     }
 
-    private static Result<String, CompileError> compileRoot(String input, String source) {
-        return createJavaRootRule().lex(input)
-                .mapValue(children -> transform(source, children))
-                .flatMapValue(createPlantRootRule()::generate);
+    static Result<Map<Source, String>, ApplicationError> readFile(Path sourceDirectory, Path source, Result<Map<Source, String>, ApplicationError> maybeInputs) {
+        return maybeInputs.flatMapValue(inner -> readString(source).mapErr(ThrowableError::new)
+                .mapErr(ApplicationError::new)
+                .mapValue(input -> {
+                    inner.put(new PathSource(sourceDirectory, source), input);
+                    return inner;
+                }));
     }
 
-    private static Rule createJavaRootRule() {
-        return new DivideRule("children",
-                new OrRule(List.of(createNamespacedRule("package"),
-                        new TypeRule("import", createNamespacedRule("import")),
-                        createStructureRule("class"),
-                        createStructureRule("interface"),
-                        createStructureRule("record"))));
-    }
+    static Result<List<Path>, IOException> collect(Path sourceDirectory) {
+        try (var files = Files.walk(sourceDirectory)) {
+            final var sources = files.filter(Files::isRegularFile)
+                    .filter(path -> path.toString()
+                            .endsWith("java"))
+                    .toList();
 
-    private static Rule createStructureRule(String type) {
-        return new InfixRule(new StringRule("before-infix"), type + " ", new StringRule("after-infix"));
-    }
-
-    private static Rule createPlantRootRule() {
-        return new DivideRule("children", new OrRule(List.of(createDependencyRule())));
-    }
-
-    private static Node transform(String source, Node root) {
-        final var transformed = root.findNodeList("children")
-                .orElse(new ArrayList<>())
-                .stream()
-                .filter(node -> node.is("import"))
-                .map(node -> node.withString("source", source))
-                .toList();
-
-        final Node node = new MapNode();
-        return node.withNodeList("children", transformed);
-    }
-
-    private static Rule createNamespacedRule(String type) {
-        return new StripRule(new PrefixRule(type + " ", new SuffixRule(new StringRule("destination"), ";")));
-    }
-
-    private static Rule createDependencyRule() {
-        return new SuffixRule(new InfixRule(new StringRule("source"), " --> ", new StringRule("destination")), "\n");
+            return new Ok<>(sources);
+        } catch (IOException e) {
+            return new Err<>(e);
+        }
     }
 }
