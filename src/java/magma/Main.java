75df4a89b408a1eb2c0c5c5656d8e758e3caa482
SirMathhman <sirmathhman@gmail.com>
2025-06-17 15:57:48 -0400

    Empty

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,17 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            final var path = Paths.get(".", "diagram.puml");
+            Files.writeString(path, "");
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
