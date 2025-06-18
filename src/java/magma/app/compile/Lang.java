5de3bc045bfb163798616d3793b8128004d2c09c
SirMathhman <sirmathhman@gmail.com>
2025-06-17 22:52:02 -0400

    Simplify

--- src/java/magma/app/compile/Lang.java
+++ src/java/magma/app/compile/Lang.java
@@ -15,35 +15,30 @@ import java.util.List;
 
 public class Lang {
     public static Rule<NodeWithEverything> createDependencyRule() {
-        return new SuffixRule<NodeWithEverything>(LocateRule.Last(new StringRule("source"),
-                " --> ",
-                new StringRule("destination")), "\n");
+        return new SuffixRule<>(LocateRule.Last(new StringRule("source"), " --> ", new StringRule("destination")),
+                "\n");
     }
 
     public static Rule<NodeWithEverything> createImportRule() {
-        return new PrefixRule<NodeWithEverything>("import ",
-                new SuffixRule<NodeWithEverything>(LocateRule.Last(new StringRule("temp"),
-                        ".",
-                        new StringRule("destination")), ";"));
+        return new PrefixRule<>("import ",
+                new SuffixRule<>(LocateRule.Last(new StringRule("temp"), ".", new StringRule("destination")), ";"));
     }
 
     public static Rule<NodeWithEverything> createPlantUMLRootSegmentRule() {
-        return new SuffixRule<NodeWithEverything>(new OrRule<NodeWithEverything>(List.of(createPlantUMLClassesRule(),
-                createImplementsRule())), "\n");
+        return new SuffixRule<>(new OrRule<>(List.of(createPlantUMLClassesRule(), createImplementsRule())), "\n");
     }
 
     private static Rule<NodeWithEverything> createImplementsRule() {
-        return new TypeRule<NodeWithEverything>("implements",
+        return new TypeRule<>("implements",
                 LocateRule.First(new StringRule("source"), " --|> ", new StringRule("destination")));
     }
 
     private static Rule<NodeWithEverything> createPlantUMLClassesRule() {
-        return new OrRule<NodeWithEverything>(List.of(createPlantUMLClassRule("class"),
-                createPlantUMLClassRule("interface")));
+        return new OrRule<>(List.of(createPlantUMLClassRule("class"), createPlantUMLClassRule("interface")));
     }
 
     public static Rule<NodeWithEverything> createStructureDefinitionsRule() {
-        return new OrRule<NodeWithEverything>(List.of(createStructureDefinitionRule("class"),
+        return new OrRule<>(List.of(createStructureDefinitionRule("class"),
                 createStructureDefinitionRule("interface"),
                 createStructureDefinitionRule("record")));
     }
@@ -52,41 +47,37 @@ public class Lang {
         final Rule<NodeWithEverything> beforeType = new StringRule("before-type");
 
         final Rule<NodeWithEverything> name = new StringRule("name");
-        final Rule<NodeWithEverything> withTypeParams = new OrRule<NodeWithEverything>(List.of(new StripRule<NodeWithEverything>(
-                new SuffixRule<NodeWithEverything>(LocateRule.First(
+        final Rule<NodeWithEverything> withTypeParams = new OrRule<>(List.of(new StripRule<>(new SuffixRule<>(LocateRule.First(
                 name,
                 "<",
                 new StringRule("type-arguments")), ">")), name));
 
-        final Rule<NodeWithEverything> withParams = new OrRule<NodeWithEverything>(List.of(LocateRule.First(
-                        withTypeParams,
+        final Rule<NodeWithEverything> withParams = new OrRule<>(List.of(LocateRule.First(withTypeParams,
                         "(",
                         new StringRule("params")),
                 withTypeParams));
-        final Rule<NodeWithEverything> afterType = new OrRule<NodeWithEverything>(List.of(LocateRule.Last(withParams,
+        final Rule<NodeWithEverything> afterType = new OrRule<>(List.of(LocateRule.Last(withParams,
                 " implements ", new NodeRule("supertype", createTypeRule())), withParams));
 
-        return new TypeRule<NodeWithEverything>(type, LocateRule.First(beforeType, type + " ", afterType));
+        return new TypeRule<>(type, LocateRule.First(beforeType, type + " ", afterType));
     }
 
     private static Rule<NodeWithEverything> createTypeRule() {
-        return new OrRule<NodeWithEverything>(List.of(createGenericRule(), createIdentifierRule()));
+        return new OrRule<>(List.of(createGenericRule(), createIdentifierRule()));
     }
 
     private static Rule<NodeWithEverything> createIdentifierRule() {
-        return new TypeRule<NodeWithEverything>("identifier", new StringRule("value"));
+        return new TypeRule<>("identifier", new StringRule("value"));
     }
 
     private static Rule<NodeWithEverything> createGenericRule() {
-        return new TypeRule<NodeWithEverything>("generic",
-                new StripRule<NodeWithEverything>(new SuffixRule<NodeWithEverything>(LocateRule.First(new StringRule(
-                                "base"),
+        return new TypeRule<>("generic", new StripRule<>(new SuffixRule<>(LocateRule.First(new StringRule("base"),
                         "<",
                         new StringRule("type-arguments")), ">")));
     }
 
     private static Rule<NodeWithEverything> createPlantUMLClassRule(String type) {
         final var afterType = new StringRule("name");
-        return new TypeRule<NodeWithEverything>(type, new PrefixRule<NodeWithEverything>(type + " ", afterType));
+        return new TypeRule<>(type, new PrefixRule<>(type + " ", afterType));
     }
 }
