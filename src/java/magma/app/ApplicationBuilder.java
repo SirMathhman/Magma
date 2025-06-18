661ee1598efeb788530fda649ff140915c2ef710
SirMathhman <sirmathhman@gmail.com>
2025-06-18 00:47:19 -0400

    Builder

--- src/java/magma/app/ApplicationBuilder.java
+++ src/java/magma/app/ApplicationBuilder.java
@@ -1,24 +1,13 @@
 package magma.app;
 
 import magma.api.io.JVMPaths;
-import magma.app.compile.Compiler;
-import magma.app.compile.StageCompiler;
-import magma.app.compile.lang.JavaLang;
-import magma.app.compile.lang.PlantLang;
-import magma.app.compile.transform.JavaPlantTransformer;
+import magma.app.compile.build.CompilerBuilder;
 
 public class ApplicationBuilder {
     public static Application create() {
         final var root = JVMPaths.get(".", "src", "java");
         final var target = JVMPaths.get(".", "diagram.puml");
-        final var compiler = createCompiler();
+        final var compiler = CompilerBuilder.createCompiler();
         return new CompileApplication(root, target, compiler);
-    }
-
-    private static Compiler createCompiler() {
-        final var sourceRule = JavaLang.createJavaRootRule();
-        final var transformer = new JavaPlantTransformer();
-        final var targetRule = PlantLang.createPlantRootRule();
-        return new StageCompiler(sourceRule, transformer, targetRule);
     }
 }
---
+++ src/java/magma/app/compile/build/CompilerBuilder.java
@@ -0,0 +1,16 @@
+package magma.app.compile.build;
+
+import magma.app.compile.Compiler;
+import magma.app.compile.StageCompiler;
+import magma.app.compile.lang.JavaLang;
+import magma.app.compile.lang.PlantLang;
+import magma.app.compile.transform.JavaPlantTransformer;
+
+public class CompilerBuilder {
+    public static Compiler createCompiler() {
+        final var sourceRule = JavaLang.createJavaRootRule();
+        final var transformer = new JavaPlantTransformer();
+        final var targetRule = PlantLang.createPlantRootRule();
+        return new StageCompiler(sourceRule, transformer, targetRule);
+    }
+}
