a6bb4dbbe2c0915a7f3491ff6567d3132523817d
SirMathhman <sirmathhman@gmail.com>
2025-06-17 20:39:05 -0400

    Indentation

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,43 +4,59 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         try {
             final var input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
-            final var segments = new ArrayList<String>();
-            var buffer = new StringBuilder();
-            for (var i = 0; i < input.length(); i++) {
-                final var c = input.charAt(i);
-                buffer.append(c);
-                if (c == ';') {
-                    segments.add(buffer.toString());
-                    buffer = new StringBuilder();
-                }
-            }
-            segments.add(buffer.toString());
-
-            final var output = new StringBuilder();
-            for (var segment : segments) {
-                final var stripped = segment.strip();
-                if (stripped.startsWith("import ")) {
-                    final var withoutPrefix = stripped.substring("import ".length());
-                    if (withoutPrefix.endsWith(";")) {
-                        final var withoutEnd = withoutPrefix.substring(0, withoutPrefix.length() - ";".length());
-                        final var separator = withoutEnd.lastIndexOf(".");
-                        if (separator >= 0) {
-                            final var name = withoutEnd.substring(separator + 1);
-                            output.append("Main --> " + name + "\n");
-                        }
-                    }
-                }
-            }
-
+            final var segments = divide(input);
+            final var output = compileRootSegments(segments);
             Files.writeString(Paths.get(".", "diagram.puml"), "@startuml\n" + output + "class Main\n@enduml");
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static List<String> divide(CharSequence input) {
+        final var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (var i = 0; i < input.length(); i++) {
+            final var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            }
+        }
+        segments.add(buffer.toString());
+        return segments;
+    }
+
+    private static String compileRootSegments(Iterable<String> segments) {
+        final var output = new StringBuilder();
+        for (var segment : segments)
+            compileRootSegment(segment.strip()).ifPresent(output::append);
+
+        return output.toString();
+    }
+
+    private static Optional<String> compileRootSegment(String input) {
+        if (!input.startsWith("import "))
+            return Optional.empty();
+
+        final var withoutPrefix = input.substring("import ".length());
+        if (!withoutPrefix.endsWith(";"))
+            return Optional.empty();
+
+        final var withoutEnd = withoutPrefix.substring(0, withoutPrefix.length() - ";".length());
+        final var separator = withoutEnd.lastIndexOf(".");
+        if (separator < 0)
+            return Optional.empty();
+
+        final var name = withoutEnd.substring(separator + 1);
+        return Optional.of("Main --> " + name + "\n");
     }
 }
