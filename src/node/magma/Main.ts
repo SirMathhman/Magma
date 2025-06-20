82e6440816411549c5400560acc29c4ffc2fbb68
SirMathhman <sirmathhman@gmail.com>
2025-06-19 21:45:32 -0400

    Root segments

--- src/node/magma/Main.ts
+++ src/node/magma/Main.ts
@@ -1,8 +1,10 @@
-/*package magma;
+/*package magma;*//*
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;*//*
+import java.nio.file.Files;*//*
+import java.nio.file.Paths;*//*
+import java.util.ArrayList;*//*
+import java.util.List;*//*
 
 public final class Main {
     private Main() {
@@ -10,22 +12,48 @@ public final class Main {
 
     public static void main(final String[] args) {
         try {
-            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
-            final var input = Files.readString(source);
+            final var source = Paths.get(".", "src", "java", "magma", "Main.java");*//*
+            final var input = Files.readString(source);*//*
+            final var segments = Main.divide(input);*//*
 
-            final var targetParent = Paths.get(".", "src", "node", "magma");
+            final var output = new StringBuilder();*//*
+            for (final var segment : segments)
+                output.append(Main.generatePlaceholder(segment));*//*
+
+
+            final var targetParent = Paths.get(".", "src", "node", "magma");*//*
             if (!Files.exists(targetParent))
-                Files.createDirectories(targetParent);
+                Files.createDirectories(targetParent);*//*
 
-            final var target = targetParent.resolve("Main.ts");
-            final var replaced = input.replace("start", "start")
-                    .replace("end", "end");
-
-            Files.writeString(target, "start" + replaced + "end");
+            final var target = targetParent.resolve("Main.ts");*//*
+            Files.writeString(target, output.toString());*//*
         } catch (final IOException e) {
             //noinspection CallToPrintStackTrace
-            e.printStackTrace();
+            e.printStackTrace();*//*
         }
+    }
+
+    private static List<String> divide(final CharSequence input) {
+        final List<String> segments = new ArrayList<>();*//*
+        final var buffer = new StringBuilder();*//*
+        final var length = input.length();*//*
+        for (var i = 0;*//* i < length;*//* i++) {
+            final var c = input.charAt(i);*//*
+            buffer.append(c);*//*
+            if (';*//*' == c) {
+                segments.add(buffer.toString());*//*
+                buffer.setLength(0);*//*
+            }
+        }
+        segments.add(buffer.toString());*//*
+        return segments;*//*
+    }
+
+    private static String generatePlaceholder(final String input) {
+        final var replaced = input.replace("start", "start")
+                .replace("end", "end");*//*
+
+        return "start" + replaced + "end";*//*
     }
 }
 */
