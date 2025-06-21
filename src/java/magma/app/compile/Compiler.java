34ec158df08a78aa9c4083b0a0e15790b785dc5c
SirMathhman <sirmathhman@gmail.com>
2025-06-20 23:27:00 -0400

    Divider

--- src/java/magma/app/compile/Compiler.java
+++ src/java/magma/app/compile/Compiler.java
@@ -1,16 +1,17 @@
 package magma.app.compile;
 
 import magma.api.Tuple;
-import magma.api.collect.list.ListLike;
 import magma.api.collect.map.MapLike;
 import magma.api.collect.stream.Joiner;
 import magma.api.optional.OptionalLike;
-import magma.app.compile.divide.DivideState;
-import magma.app.compile.divide.MutableDivideState;
+import magma.app.compile.divide.Divider;
 import magma.app.compile.lang.Lang;
 import magma.app.compile.node.Node;
 
 public class Compiler {
+    private Compiler() {
+    }
+
     public static String compileEntries(final MapLike<String, String> sources) {
         return sources.stream()
                 .map(Compiler::compileEntry)
@@ -26,7 +27,8 @@ public class Compiler {
     }
 
     private static String compile(final CharSequence input, final String source) {
-        return Compiler.divide(input).stream()
+        return Divider.divide(input)
+                .stream()
                 .map(segment -> Compiler.compileRootSegment(segment, source))
                 .flatMap(OptionalLike::stream)
                 .collect(new Joiner())
@@ -41,25 +43,5 @@ public class Compiler {
                     return Lang.createDependencyRule()
                             .generate(withSource);
                 });
-    }
-
-    private static ListLike<String> divide(final CharSequence input) {
-        final DivideState state = new MutableDivideState();
-        final var length = input.length();
-        var current = state;
-        for (var i = 0; i < length; i++) {
-            final var c = input.charAt(i);
-            current = Compiler.fold(current, c);
-        }
-
-        return current.advance()
-                .segments();
-    }
-
-    private static DivideState fold(final DivideState state, final char c) {
-        final var appended = state.append(c);
-        if (';' == c)
-            return appended.advance();
-        return appended;
     }
 }
