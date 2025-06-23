1362bbc0a5283123de1e4836f9c39ac5e9b6499b
SirMathhman <sirmathhman@gmail.com>
2025-06-22 23:03:58 -0400

    Hmmm

--- src/java/magma/app/compile/CompilerBuilder.java
+++ src/java/magma/app/compile/CompilerBuilder.java
@@ -1,31 +1,23 @@
 package magma.app.compile;
 
 import magma.api.error.list.ErrorSequence;
-import magma.app.compile.context.Context;
 import magma.app.compile.context.ContextErrorResultFactory;
 import magma.app.compile.context.SimpleContextFactory;
 import magma.app.compile.error.CompileErrorFactory;
-import magma.app.compile.error.ErrorFactory;
 import magma.app.compile.error.FormattedError;
-import magma.app.compile.factory.ResultFactory;
 import magma.app.compile.lang.JavaLang;
 import magma.app.compile.lang.PlantUMLJavaLang;
 import magma.app.compile.node.map.MapNodeFactory;
 import magma.app.compile.node.property.CompoundNode;
-import magma.app.compile.node.property.NodeFactory;
-import magma.app.compile.node.result.NodeResult;
-import magma.app.compile.string.StringResult;
 
 public class CompilerBuilder {
     private CompilerBuilder() {
     }
 
     public static Compiler createCompiler() {
-        final NodeFactory<CompoundNode> nodeFactory = new MapNodeFactory();
-        final ErrorFactory<Context, FormattedError, ErrorSequence<FormattedError>> errorFactory = new CompileErrorFactory();
-        final ResultFactory<CompoundNode, NodeResult<CompoundNode, FormattedError>, StringResult<FormattedError>, ErrorSequence<FormattedError>> resultsFactory = new ContextErrorResultFactory<>(
-                new SimpleContextFactory<>(),
-                errorFactory);
+        final var nodeFactory = new MapNodeFactory();
+        final var resultsFactory = new ContextErrorResultFactory<CompoundNode, FormattedError, ErrorSequence<FormattedError>>(
+                new SimpleContextFactory<>(), new CompileErrorFactory());
 
         return new RuleCompiler(new JavaLang<>(nodeFactory, resultsFactory),
                 new PlantUMLJavaLang<>(nodeFactory, resultsFactory));
