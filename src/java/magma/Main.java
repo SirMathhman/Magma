bb705828ef0baf5091202c6addefc22d28f7b448
SirMathhman <sirmathhman@gmail.com>
2025-06-24 20:22:03 -0400

    Cleanup

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -2,10 +2,15 @@ package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private Main() {
+    }
+
     public static void main(final String[] args) {
         final var rootDirectory = Paths.get(".", "src", "java");
         try (final var stream = Files.walk(rootDirectory)) {
@@ -14,39 +19,49 @@ public class Main {
                             .endsWith(".java"))
                     .toList();
 
-            for (final var source : sources) {
-                final var fileName = source.getFileName()
-                        .toString();
-                final var separator = fileName.lastIndexOf('.');
-                final var name = fileName.substring(0, separator);
-
-                final var relativeParent = rootDirectory.relativize(source.getParent());
-                final var namespace = new ArrayList<String>();
-                for (var i = 0; i < relativeParent.getNameCount(); i++)
-                    namespace.add(relativeParent.getName(i)
-                            .toString());
-
-                final var targetParent = Paths.get(".", "src", "windows")
-                        .resolve(relativeParent);
-
-                if (!Files.exists(targetParent))
-                    Files.createDirectories(targetParent);
-
-                final var targetContent = "#include \"" + name + ".h\"" + System.lineSeparator();
-                Files.writeString(targetParent.resolve(name + ".c"), targetContent);
-
-                final var joined = String.join("_", namespace);
-                final var withName = joined + "_" + name;
-                final var headerContent = String.join(System.lineSeparator(),
-                        "#ifndef " + withName,
-                        "#define " + withName,
-                        "#endif");
-
-                Files.writeString(targetParent.resolve(name + ".h"), headerContent);
-            }
+            Main.runWithSources(rootDirectory, sources);
         } catch (final IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static void runWithSources(final Path rootDirectory, final Iterable<Path> sources) throws IOException {
+        for (final var source : sources) {
+            final var fileName = source.getFileName()
+                    .toString();
+            final var separator = fileName.lastIndexOf('.');
+            final var name = fileName.substring(0, separator);
+
+            final var relativeParent = rootDirectory.relativize(source.getParent());
+            final var namespace = Main.computeNamespace(relativeParent);
+
+            final var targetParent = Paths.get(".", "src", "windows")
+                    .resolve(relativeParent);
+
+            if (!Files.exists(targetParent))
+                Files.createDirectories(targetParent);
+
+            final var targetContent = "#include \"" + name + ".h\"" + System.lineSeparator();
+            Files.writeString(targetParent.resolve(name + ".c"), targetContent);
+
+            final var joined = String.join("_", namespace);
+            final var withName = joined + "_" + name;
+            final var headerContent = String.join(System.lineSeparator(),
+                    "#ifndef " + withName,
+                    "#define " + withName,
+                    "#endif");
+
+            Files.writeString(targetParent.resolve(name + ".h"), headerContent);
+        }
+    }
+
+    private static List<String> computeNamespace(final Path relativeParent) {
+        final List<String> namespace = new ArrayList<>();
+        final var nameCount = relativeParent.getNameCount();
+        for (var i = 0; i < nameCount; i++)
+            namespace.add(relativeParent.getName(i)
+                    .toString());
+        return namespace;
     }
 }
