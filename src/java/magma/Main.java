1b3b3adc37efcaf38aca388b2c78f773f02740f8
SirMathhman <sirmathhman@gmail.com>
2025-06-26 19:21:15 -0400

    Main.ts

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,21 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(final String[] args) {
+        try {
+            final var targetParent = Paths.get(".", "src", "node", "magma");
+            if (!Files.exists(targetParent))
+                Files.createDirectories(targetParent);
 
+            final var target = targetParent.resolve("Main.ts");
+            Files.writeString(target, "");
+        } catch (final IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
