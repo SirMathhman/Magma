d059aeb46320097435bdb131d0243c90cec01681
SirMathhman <sirmathhman@gmail.com>
2025-06-26 19:31:45 -0400

    Rebuild

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -10,12 +10,16 @@ public class Main {
 
     public static void main(final String[] args) {
         try {
+            final var input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
+
             final var targetParent = Paths.get(".", "src", "node", "magma");
             if (!Files.exists(targetParent))
                 Files.createDirectories(targetParent);
 
             final var target = targetParent.resolve("Main.ts");
-            Files.writeString(target, "");
+            Files.writeString(target,
+                    "/*" + input.replace("/*", "stat")
+                            .replace("*/", "end") + "*/");
         } catch (final IOException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
---
+++ src/node/magma/Main.ts
@@ -0,0 +1,28 @@
+/*package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
+        try {
+            final var input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
+
+            final var targetParent = Paths.get(".", "src", "node", "magma");
+            if (!Files.exists(targetParent))
+                Files.createDirectories(targetParent);
+
+            final var target = targetParent.resolve("Main.ts");
+            Files.writeString(target, "stat" + input.replace("stat", "stat")
+                    .replace("end", "end")+ "end");
+        } catch (final IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+}
+*/
