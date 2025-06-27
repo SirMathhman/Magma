3a4eae4f858208cec597f9ac07ad0b5b68481aab
SirMathhman <sirmathhman@gmail.com>
2025-06-26 20:23:00 -0400

    OK

--- src/node/magma/Main.js
+++ src/node/magma/Main.js
@@ -4,5 +4,8 @@
 /*import java.nio.file.Files;*/
 /*import java.nio.file.Paths;*/
 /*import java.util.List;*/
-/*public */ class Main {
-}
+/*public */ var Main = /** @class */ (function () {
+    function Main() {
+    }
+    return Main;
+}());
