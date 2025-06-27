58d7948638d66774d47b746ad00dd3599e2d49a1
SirMathhman <sirmathhman@gmail.com>
2025-06-26 22:23:47 -0400

    Indents

--- src/java/magma/MutableState.java
+++ src/java/magma/MutableState.java
@@ -13,40 +13,48 @@ public class MutableState implements State {
         this.input = input;
     }
 
-    @Override public State advance() {
+    @Override
+    public State advance() {
         this.segments = this.segments.add(this.buffer);
         this.buffer = "";
         return this;
     }
 
-    @Override public State append(final char c) {
+    @Override
+    public State append(final char c) {
         this.buffer = this.buffer + c;
         return this;
     }
 
-    @Override public ListLike<String> unwrap() {
+    @Override
+    public ListLike<String> unwrap() {
         return this.segments;
     }
 
-    @Override public boolean isLevel() {
+    @Override
+    public boolean isLevel() {
         return 0 == this.depth;
     }
 
-    @Override public State enter() {
+    @Override
+    public State enter() {
         this.depth++;
         return this;
     }
 
-    @Override public State exit() {
+    @Override
+    public State exit() {
         this.depth--;
         return this;
     }
 
-    @Override public boolean isShallow() {
+    @Override
+    public boolean isShallow() {
         return 1 == this.depth;
     }
 
-    @Override public Optional<Tuple<State, Character>> pop() {
+    @Override
+    public Optional<Tuple<State, Character>> pop() {
         if (this.index >= this.input.length())
             return Optional.empty();
 
@@ -55,11 +63,13 @@ public class MutableState implements State {
         return Optional.of(new Tuple<>(this, c));
     }
 
-    @Override public Optional<Tuple<State, Character>> popAndAppendToTuple() {
+    @Override
+    public Optional<Tuple<State, Character>> popAndAppendToTuple() {
         return this.pop().map(tuple -> new Tuple<>(tuple.left().append(tuple.right()), tuple.right()));
     }
 
-    @Override public Optional<State> popAndAppendToOption() {
+    @Override
+    public Optional<State> popAndAppendToOption() {
         return this.popAndAppendToTuple().map(Tuple::left);
     }
 }
