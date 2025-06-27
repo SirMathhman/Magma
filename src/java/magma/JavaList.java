457fb3344146210181cda9dff09b0e53845a8683
SirMathhman <sirmathhman@gmail.com>
2025-06-27 10:01:44 -0400

    @Actual

--- src/java/magma/JavaList.java
+++ src/java/magma/JavaList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+@Actual
 public record JavaList<T>(List<T> list) implements ListLike<T> {
     public JavaList() {
         this(new ArrayList<>());
