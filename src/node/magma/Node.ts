24eca5239396472bf670e6edeb887aae839febf9
SirMathhman <sirmathhman@gmail.com>
2025-07-08 11:03:48 -0400

    Nuke

--- src/node/magma/Node.ts
+++
@@ -1,8 +0,0 @@
-/*public interface Node */{
-	/*Node withString(String key, String value)*/;
-	/*Optional<String> findString(String key)*/;
-	/*Stream<Map.Entry<String, String>> stream()*/;
-	/*Node merge(Node other)*/;
-	/*
-*/
-}/**/
--- src/node/magma/StringRule.ts
+++
@@ -1,7 +0,0 @@
-/*public record StringRule(String key) */{
-	/*Optional<Node> lex*/(/*final String input) {
-        return Optional.of(new MapNode().withString(this.key, input));
-    }*/
-	/*
-*/
-}/**/
