9313bb2885d1a414c8cafe48a047832229a6a7e1
SirMathhman <sirmathhman@gmail.com>
2025-07-20 19:46:02 -0400

    Copy as comment

--- src/magma/Main.c
+++ src/magma/Main.c
@@ -10,7 +10,8 @@ public class Main {
 			final var source = Paths.get(".", "src", "magma", "Main.java");
 			final var input = Files.readString(source);
 			final var target = source.resolveSibling("Main.c");
-			Files.writeString(target, "/*" + input + "*/");
+			final var replaced = input.replace("start", "start").replace("end", "end");
+			Files.writeString(target, "start" + replaced + "end");
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
--- src/magma/Main.java
+++ src/magma/Main.java
@@ -10,7 +10,8 @@ public class Main {
 			final var source = Paths.get(".", "src", "magma", "Main.java");
 			final var input = Files.readString(source);
 			final var target = source.resolveSibling("Main.c");
-			Files.writeString(target, "/*" + input + "*/");
+			final var replaced = input.replace("/*", "start").replace("*/", "end");
+			Files.writeString(target, "/*" + replaced + "*/");
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
