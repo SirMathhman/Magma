e8026bcad0e1e1f08ff6baf70cf9fedde9ffe2e7
SirMathhman <sirmathhman@gmail.com>
2025-07-20 19:53:15 -0400

    Root segments

--- src/magma/Main.c
+++ src/magma/Main.c
@@ -1,20 +1,72 @@
-/*package magma;
+/*package magma;*//*
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;*//*
+import java.nio.file.Files;*//*
+import java.nio.file.Paths;*//*
+import java.util.ArrayList;*//*
+import java.util.Collection;*//*
+import java.util.stream.Collectors;*//*
+import java.util.stream.Stream;*//*
 
 public class Main {
+	public static class DivideState {
+		private final Collection<String> segments = new ArrayList<>();*//*
+		private StringBuilder buffer = new StringBuilder();*//*
+
+		private DivideState advance() {
+			segments.add(buffer.toString());*//*
+			this.buffer = new StringBuilder();*//*
+			return this;*//*
+		}
+
+		private Stream<String> stream() {
+			return segments.stream();*//*
+		}
+
+		private DivideState append(char c) {
+			buffer.append(c);*//*
+			return this;*//*
+		}
+	}
+
 	public static void main(String[] args) {
 		try {
-			final var source = Paths.get(".", "src", "magma", "Main.java");
-			final var input = Files.readString(source);
-			final var target = source.resolveSibling("Main.c");
-			final var replaced = input.replace("start", "start").replace("end", "end");
-			Files.writeString(target, "start" + replaced + "end");
+			final var source = Paths.get(".", "src", "magma", "Main.java");*//*
+			final var input = Files.readString(source);*//*
+			final var target = source.resolveSibling("Main.c");*//*
+			final var output = compile(input);*//*
+			Files.writeString(target, output);*//*
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();
+			e.printStackTrace();*//*
 		}
+	}
+
+	private static String compile(String input) {
+		return divide(input).map(Main::generatePlaceholder).collect(Collectors.joining());*//*
+	}
+
+	private static Stream<String> divide(String input) {
+		var current = new DivideState();*//*
+		for (var i = 0;*//* i < input.length();*//* i++) {
+			final var c = input.charAt(i);*//*
+			current = fold(current, c);*//*
+		}
+
+		return current.advance().stream();*//*
+	}
+
+	private static DivideState fold(DivideState state, char c) {
+		final var appended = state.append(c);*//*
+		if (c == ';*//*') {
+			return appended.advance();*//*
+		}
+
+		return appended;*//*
+	}
+
+	private static String generatePlaceholder(String input) {
+		final var replaced = input.replace("start", "start").replace("end", "end");*//*
+		return "start" + replaced + "end";*//*
 	}
 }*/
