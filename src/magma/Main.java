7992aeff3555ac3427fcdb282e6d38fcc2a20b5c
SirMathhman <sirmathhman@gmail.com>
2025-07-20 20:05:34 -0400

    Cleanup

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -9,107 +9,110 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Main {
-	public static class DivideState {
+public final class Main {
+	private static class DivideState {
 		private final Collection<String> segments = new ArrayList<>();
 		private StringBuilder buffer = new StringBuilder();
 		private int depth = 0;
 
 		private DivideState advance() {
-			segments.add(buffer.toString());
+			this.segments.add(this.buffer.toString());
 			this.buffer = new StringBuilder();
 			return this;
 		}
 
 		private Stream<String> stream() {
-			return segments.stream();
+			return this.segments.stream();
 		}
 
-		private DivideState append(char c) {
-			buffer.append(c);
+		private DivideState append(final char c) {
+			this.buffer.append(c);
 			return this;
 		}
 
-		public boolean isLevel() {
-			return depth == 0;
+		final boolean isLevel() {
+			return 0 == this.depth;
 		}
 
-		public DivideState enter() {
-			depth++;
+		final DivideState enter() {
+			this.depth++;
 			return this;
 		}
 
-		public DivideState exit() {
-			depth--;
+		final DivideState exit() {
+			this.depth--;
 			return this;
 		}
 	}
 
-	public static void main(String[] args) {
+	private Main() {}
+
+	public static void main(final String[] args) {
 		try {
 			final var source = Paths.get(".", "src", "magma", "Main.java");
 			final var input = Files.readString(source);
 			final var target = source.resolveSibling("Main.c");
-			final var output = compile(input);
+			final var output = Main.compile(input);
 			Files.writeString(target, output);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
 		}
 	}
 
-	private static String compile(String input) {
-		return divide(input).map(Main::compileRootSegment).collect(Collectors.joining());
+	private static String compile(final CharSequence input) {
+		return Main.divide(input).map(Main::compileRootSegment).collect(Collectors.joining());
 	}
 
-	private static String compileRootSegment(String input) {
+	private static String compileRootSegment(final String input) {
 		final var strip = input.strip();
 		if (strip.startsWith("package ") || strip.startsWith("import ")) return "";
-		return compileRootSegmentValue(strip) + System.lineSeparator();
+		return Main.compileRootSegmentValue(strip) + System.lineSeparator();
 	}
 
-	private static String compileRootSegmentValue(String input) {
-		return compileClass(input).orElseGet(() -> generatePlaceholder(input));
+	private static String compileRootSegmentValue(final String input) {
+		return Main.compileClass(input).orElseGet(() -> Main.generatePlaceholder(input));
 	}
 
-	private static Optional<String> compileClass(String input) {
+	private static Optional<String> compileClass(final String input) {
 		final var classIndex = input.indexOf("class ");
-		if (classIndex < 0) {return Optional.empty();}
+		if (0 > classIndex) {return Optional.empty();}
 		final var beforeKeyword = input.substring(0, classIndex);
 		final var afterKeyword = input.substring(classIndex + "class ".length());
 
-		final var contentStart = afterKeyword.indexOf("{");
-		if (contentStart < 0) {return Optional.empty();}
+		final var contentStart = afterKeyword.indexOf('{');
+		if (0 > contentStart) {return Optional.empty();}
 		final var name = afterKeyword.substring(0, contentStart).strip();
 		final var afterName = afterKeyword.substring(contentStart + "{".length()).strip();
 
-		if (!afterName.endsWith("}")) {return Optional.empty();}
+		if (afterName.isEmpty() || '}' != afterName.charAt(afterName.length() - 1)) {return Optional.empty();}
 		final var withoutEnd = afterName.substring(0, afterName.length() - "}".length());
 
-		return Optional.of(generatePlaceholder(beforeKeyword) + "struct " + name + " {};" + System.lineSeparator() +
-											 generatePlaceholder(withoutEnd));
+		return Optional.of(Main.generatePlaceholder(beforeKeyword) + "struct " + name + " {};" + System.lineSeparator() +
+											 Main.generatePlaceholder(withoutEnd));
 
 	}
 
-	private static Stream<String> divide(String input) {
+	private static Stream<String> divide(final CharSequence input) {
 		var current = new DivideState();
-		for (var i = 0; i < input.length(); i++) {
+		final var length = input.length();
+		for (var i = 0; i < length; i++) {
 			final var c = input.charAt(i);
-			current = fold(current, c);
+			current = Main.fold(current, c);
 		}
 
 		return current.advance().stream();
 	}
 
-	private static DivideState fold(DivideState state, char c) {
+	private static DivideState fold(final DivideState state, final char c) {
 		final var appended = state.append(c);
-		if (c == ';' && appended.isLevel()) return appended.advance();
-		if (c == '{') return appended.enter();
-		if (c == '}') return appended.exit();
+		if (';' == c && appended.isLevel()) return appended.advance();
+		if ('{' == c) return appended.enter();
+		if ('}' == c) return appended.exit();
 		return appended;
 	}
 
-	private static String generatePlaceholder(String input) {
+	private static String generatePlaceholder(final String input) {
 		final var replaced = input.replace("/*", "start").replace("*/", "end");
 		return "/*" + replaced + "*/";
 	}
