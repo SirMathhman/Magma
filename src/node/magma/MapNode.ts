267d86007b373d4a97900fd2cacaae12214b27cc
SirMathhman <sirmathhman@gmail.com>
2025-07-27 23:03:02 -0400

    MapNode

---
+++ src/node/magma/MapNode.ts
@@ -0,0 +1,15 @@
+/*import java.util.HashMap;*/
+/*import java.util.Map;*/
+/*import java.util.Optional;*/
+/*public final*/class MapNode {/*
+	private final Map<String, String> strings = new HashMap<>();
+
+	MapNode withString(final String key, final String value) {
+		this.strings.put(key, value);
+		return this;
+	}
+
+	Optional<String> findString(final String key) {
+		return Optional.ofNullable(this.strings.get(key));
+	}
+}*/
