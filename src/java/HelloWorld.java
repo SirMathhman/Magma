9b5600ddb1b82ec5b66df5a41e05a7c26ccec712
SirMathhman <sirmathhman@gmail.com>
2025-07-30 21:32:12 -0400

    Add `HelloWorld` example program as a simple entry point

---
+++ src/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * A simple Hello World program
+ */
+public final class HelloWorld {
+	private HelloWorld() {}
+
+	/**
+	 * Main method that prints "Hello, World!" to the console
+	 *
+	 * @param args command line arguments (not used)
+	 */
+	public static void main(final String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
