36186195c0877878de8dd91c25e514a0e689ad05
SirMathhman <sirmathhman@gmail.com>
2025-07-31 01:01:01 -0400

    Simplify `InfixRule` by removing redundant logic in `generate` and `lex` methods. Streamline infix handling to improve readability and maintainability.

--- src/java/magma/rule/InfixRule.java
+++ src/java/magma/rule/InfixRule.java
@@ -1,73 +1,42 @@
 package magma.rule;
 
-import magma.node.MapNode;
 import magma.node.Node;
 
 import java.util.Optional;
 
 public final class InfixRule implements Rule {
-    private final Rule leftRule;
-    private final Rule rightRule;
-    private final String infix;
+	private final Rule leftRule;
+	private final Rule rightRule;
+	private final String infix;
 
-    public InfixRule(final Rule leftRule, final Rule rightRule, final String infix) {
-        this.leftRule = leftRule;
-        this.rightRule = rightRule;
-        this.infix = infix;
-    }
+	public InfixRule(final Rule leftRule, final Rule rightRule, final String infix) {
+		this.leftRule = leftRule;
+		this.rightRule = rightRule;
+		this.infix = infix;
+	}
 
-    @Override
-    public Optional<String> generate(final Node node) {
-        final Optional<String> leftResult = this.leftRule.generate(node);
-        final Optional<String> rightResult = this.rightRule.generate(node);
-        
-        if (leftResult.isPresent() && rightResult.isPresent()) {
-            return Optional.of(leftResult.get() + this.infix + rightResult.get());
-        } else if (leftResult.isPresent()) {
-            return leftResult;
-        } else if (rightResult.isPresent()) {
-            return rightResult;
-        } else {
-            return Optional.empty();
-        }
-    }
-    
-    @Override
-    public Optional<Node> lex(final String input) {
-        if (this.infix.isEmpty()) {
-            // Try to lex with left rule first, then right rule if left fails
-            Optional<Node> leftResult = this.leftRule.lex(input);
-            if (leftResult.isPresent()) {
-                return leftResult;
-            }
-            return this.rightRule.lex(input);
-        }
-        
-        int infixIndex = input.indexOf(this.infix);
-        if (infixIndex == -1) {
-            // If infix not found, try each rule separately
-            Optional<Node> leftResult = this.leftRule.lex(input);
-            if (leftResult.isPresent()) {
-                return leftResult;
-            }
-            return this.rightRule.lex(input);
-        }
-        
-        String leftPart = input.substring(0, infixIndex);
-        String rightPart = input.substring(infixIndex + this.infix.length());
-        
-        Optional<Node> leftNode = this.leftRule.lex(leftPart);
-        Optional<Node> rightNode = this.rightRule.lex(rightPart);
-        
-        if (leftNode.isPresent() && rightNode.isPresent()) {
-            // Merge the two nodes using the Node.merge method
-            return Optional.of(leftNode.get().merge(rightNode.get()));
-        } else if (leftNode.isPresent()) {
-            return leftNode;
-        } else if (rightNode.isPresent()) {
-            return rightNode;
-        }
-        
-        return Optional.empty();
-    }
+	@Override
+	public Optional<String> generate(final Node node) {
+		final Optional<String> leftResult = this.leftRule.generate(node);
+		final Optional<String> rightResult = this.rightRule.generate(node);
+
+		if (leftResult.isPresent() && rightResult.isPresent())
+			return Optional.of(leftResult.get() + this.infix + rightResult.get());
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Node> lex(final String input) {
+		final int infixIndex = input.indexOf(this.infix);
+		if (-1 == infixIndex) return Optional.empty();
+
+		final String leftPart = input.substring(0, infixIndex);
+		final String rightPart = input.substring(infixIndex + this.infix.length());
+
+		final Optional<Node> leftNode = this.leftRule.lex(leftPart);
+		final Optional<Node> rightNode = this.rightRule.lex(rightPart);
+
+		if (leftNode.isPresent() && rightNode.isPresent()) return Optional.of(leftNode.get().merge(rightNode.get()));
+		return Optional.empty();
+	}
 }
