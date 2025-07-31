db9d91cc1ff517434b21ab1360d21a7bce030beb
SirMathhman <sirmathhman@gmail.com>
2025-07-30 20:50:26 -0400

    Main

---
+++ src/magma/Main.java
@@ -0,0 +1,18 @@
+package magma;
+
+/**
+ * Main class for the Magma application
+ */
+public class Main {
+
+    /**
+     * Entry point for the application
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Magma application started");
+
+        // TODO: Add application logic here
+    }
+}
