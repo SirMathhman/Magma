9fb0619a48084da15db4bc805f1b33b57fd1c13a
SirMathhman <sirmathhman@gmail.com>
2025-08-01 15:30:28 -0400

    Hmmm

--- src/windows/magma/Main.c
+++ src/windows/magma/Main.c
@@ -51,23 +51,29 @@
 		}*/struct DivideState DivideState(){
 	struct DivideState this;
 	return this;
-}/*private*/ /*record*/ ParseState(/*List<JavaStructure> structures, List<String> functions) {
+}/*private*/ /*record*/ ParseState(/*List<JavaStructure> javaStructures, List<CStructure> cStructures, List<String> functions) {
 		private ParseState() {
-			this(Lists.empty(), Lists.empty());
+			this(Lists.empty(), Lists.empty(), Lists.empty());
 		}
 
-		ParseState addStructure(final JavaStructure generated) {
-			return new ParseState(this.structures.add(generated), this.functions);
+		ParseState addCStructure(final CStructure generated) {
+			return new ParseState(this.javaStructures, this.cStructures.add(generated), this.functions);
 		}
 
 		ParseState addFunction(final String generated) {
-			return new ParseState(this.structures, this.functions.add(generated));
+			return new ParseState(this.javaStructures, this.cStructures, this.functions.add(generated));
 		}
-	}*//*private record*/ /*Tuple<Left,*/ Right>(/*Left left, Right right) {}*//*private*/ /*record*/ JavaStructure(/*String modifiers, String name, String content) {
+
+		ParseState addJavaStructure(final JavaStructure javaStructure) {
+			return new ParseState(this.javaStructures.add(javaStructure), this.cStructures, this.functions);
+		}
+	}*//*private record*/ /*Tuple<Left,*/ Right>(/*Left left, Right right) {}*//*private*/ /*record*/ CStructure(/*String modifiers, String name, String content) {
 		private String generate() {
 			return Main.generatePlaceholder(this.modifiers()) + "struct " + this.name() + " {" + this.content() +
 						 System.lineSeparator() + "};" + System.lineSeparator();
 		}
+	}*//*private*/ /*record*/ JavaStructure(/*String modifiers, String name, String typeParameters, String content) {
+
 	}*//*private*/ Main(/*) {}*//*public static*/ /*void*/ main(/*final String[] args) {
 		try {
 			final var input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
@@ -86,7 +92,7 @@
 	}*//*private static*/ /*String*/ compile(/*final CharSequence input) {
 		final var tuple = Main.compileStatements(new ParseState(), input, Main::compileRootSegment);
 		final var newState = tuple.left;
-		final var joined = newState.structures.stream().map(JavaStructure::generate).collect(Collectors.joining()) +
+		final var joined = newState.cStructures.stream().map(CStructure::generate).collect(Collectors.joining()) +
 											 newState.functions.stream().collect(Collectors.joining());
 
 		return joined + tuple.right;
@@ -120,23 +126,35 @@
 
 		final var i = after.indexOf('{');
 		if (0 > i) return Optional.empty();
-		final var beforeContent = after.substring(0, i).strip();
-		if (!beforeContent.isEmpty() && '>' == beforeContent.charAt(beforeContent.length() - 1)) {
-			final var substring = beforeContent.substring(0, beforeContent.length() - 1);
-			if (substring.contains("<")) return Optional.of(new Tuple<>(state, ""));
-		}
-
+		final var name = after.substring(0, i).strip();
 		final var withEnd = after.substring(i + 1).strip();
 
 		if (withEnd.isEmpty() || '}' != withEnd.charAt(withEnd.length() - 1)) return Optional.empty();
 		final var content = withEnd.substring(0, withEnd.length() - 1);
 
+		final var parseState = Main.assembleStructure(state, before, name, content);
+		return Optional.of(new Tuple<>(parseState, ""));
+	}*//*private static*/ /*ParseState*/ assembleStructure(/*final ParseState state,
+																							final String modifiers,
+																							final String beforeContent,
+																							final String content) {
+		final var strip = beforeContent.strip();
+		if (!strip.isEmpty() && '>' == strip.charAt(strip.length() - 1)) {
+			final var withoutEnd = beforeContent.substring(0, strip.length() - 1);
+			final var i = withoutEnd.indexOf('<');
+			if (0 <= i) {
+				final var name = withoutEnd.substring(0, i);
+				final var typeParameters = withoutEnd.substring(i + 1);
+
+				return state.addJavaStructure(new JavaStructure(modifiers, name, typeParameters, content));
+			}
+		}
+
+
 		final var tuple = Main.compileStatements(state, content, Main::compileClassSegment);
 		final var outputContent = tuple.right;
-		final var generated = new JavaStructure(before, beforeContent, outputContent);
-
-		return Optional.of(
-				new Tuple<>(tuple.left.addStructure(generated).addFunction(Main.generateConstructor(beforeContent)), ""));
+		final var generated = new CStructure(modifiers, beforeContent, outputContent);
+		return tuple.left.addCStructure(generated).addFunction(Main.generateConstructor(beforeContent));
 	}*//*private static*/ /*String*/ generateConstructor(/*final String name) {
 		return "struct " + name + " " + name + "(){" + System.lineSeparator() + "\tstruct " + name + " this;" +
 					 System.lineSeparator() + "\treturn this;" + System.lineSeparator() + "}";
