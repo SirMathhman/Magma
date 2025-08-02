01d1e46bb466e8dc2dbd0059e022f873e296f269
SirMathhman <sirmathhman@gmail.com>
2025-08-01 20:56:54 -0400

    Reset `Main`: remove legacy C logic, clean up workspace settings, and prepare for new implementation.

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,9 +1,18 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 final class Main {
 	private Main() {}
 
 	public static void main(final String[] args) {
-
+		try {
+			Files.writeString(Paths.get(".", "src", "windows", "magma", "Main.c"), "");
+		} catch (final IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
 	}
 }
