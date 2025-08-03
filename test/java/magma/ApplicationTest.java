c1658fb4749720ec652a02a006bd1ea927e2f466
SirMathhman <sirmathhman@gmail.com>
2025-08-03 00:22:44 -0400

    Remove

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,21 +1,4 @@
 package magma;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class ApplicationTest {
-	@ParameterizedTest
-	@ValueSource(strings = {"5", "10"})
-	void number(String value) throws ApplicationException {
-		assertEquals(value, Application.run(value));
-	}
-
-	@ParameterizedTest
-	@ValueSource(strings = {"first", "second"})
-	void test(String value) {
-		assertThrows(ApplicationException.class, () -> Application.run(value));
-	}
 }
