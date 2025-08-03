434bb8555a0816256230a959dbe3f620da4f5042
SirMathhman <sirmathhman@gmail.com>
2025-08-02 21:31:39 -0400

    Parameterize `MainTest.digit` to validate multiple inputs; update `pom.xml` to include JUnit Params dependency.

--- test/java/magma/MainTest.java
+++ test/java/magma/MainTest.java
@@ -1,12 +1,14 @@
 package magma;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-	@Test
-	void digit() {
-		assertEquals("5", Main.run("5"));
+	@ParameterizedTest
+	@ValueSource(strings = {"-10", "-1", "0", "1", "10"})
+	void digit(String input) {
+		assertEquals(input, Main.run(input));
 	}
 }
