55a39f8598eedb1c89b05879e7c0a0ae7c77a9cf
SirMathhman <sirmathhman@gmail.com>
2025-08-08 10:26:18 -0400

    Refactor `CompilerTest` to consolidate integer type tests using parameterized tests.

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -1,10 +1,23 @@
 package magma;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompilerTest {
+	private record IntegerTestCase(String name, String type, String cType) {}
+
+	private static Stream<IntegerTestCase> integerTestCases() {
+		return Stream.of(new IntegerTestCase("x", "I32", "int32_t"), new IntegerTestCase("a", "I8", "int8_t"),
+										 new IntegerTestCase("b", "I16", "int16_t"), new IntegerTestCase("c", "I64", "int64_t"),
+										 new IntegerTestCase("d", "U8", "uint8_t"), new IntegerTestCase("e", "U16", "uint16_t"),
+										 new IntegerTestCase("f", "U32", "uint32_t"), new IntegerTestCase("g", "U64", "uint64_t"));
+	}
+
 	@Test
 	void invalid() {
 		assertThrows(CompileException.class, () -> Compiler.compile("?"));
@@ -15,44 +28,11 @@ class CompilerTest {
 		assertValid("", "");
 	}
 
-	@Test
-	void letI32() {
-		assertValid("let x : I32 = 0;", "#include <stdint.h>\nint32_t x = 0;");
-	}
-
-	@Test
-	void letI8() {
-		assertValid("let a : I8 = 0;", "#include <stdint.h>\nint8_t a = 0;");
-	}
-
-	@Test
-	void letI16() {
-		assertValid("let b : I16 = 0;", "#include <stdint.h>\nint16_t b = 0;");
-	}
-
-	@Test
-	void letI64() {
-		assertValid("let c : I64 = 0;", "#include <stdint.h>\nint64_t c = 0;");
-	}
-
-	@Test
-	void letU8() {
-		assertValid("let d : U8 = 0;", "#include <stdint.h>\nuint8_t d = 0;");
-	}
-
-	@Test
-	void letU16() {
-		assertValid("let e : U16 = 0;", "#include <stdint.h>\nuint16_t e = 0;");
-	}
-
-	@Test
-	void letU32() {
-		assertValid("let f : U32 = 0;", "#include <stdint.h>\nuint32_t f = 0;");
-	}
-
-	@Test
-	void letU64() {
-		assertValid("let g : U64 = 0;", "#include <stdint.h>\nuint64_t g = 0;");
+	@ParameterizedTest
+	@MethodSource("integerTestCases")
+	void letInteger(IntegerTestCase testCase) {
+		assertValid(String.format("let %s : %s = 0;", testCase.name(), testCase.type()),
+								String.format("#include <stdint.h>\n%s %s = 0;", testCase.cType(), testCase.name()));
 	}
 
 	private void assertValid(String input, String output) {
