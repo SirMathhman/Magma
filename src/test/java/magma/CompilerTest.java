a38b691825ffe7ce8740854c053336e2dfee3b9f
SirMathhman <sirmathhman@gmail.com>
2025-08-08 10:25:37 -0400

    Add support for additional integer types in `Compiler` (`I8`, `I16`, `I64`, `U8`, `U16`, `U32`, `U64`) and extend `CompilerTest` with corresponding test cases.

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -17,7 +17,42 @@ class CompilerTest {
 
 	@Test
 	void letI32() {
-		assertValid("let x :  I32 = 0;", "#include <stdint.h>\nint32_t x = 0;");
+		assertValid("let x : I32 = 0;", "#include <stdint.h>\nint32_t x = 0;");
+	}
+
+	@Test
+	void letI8() {
+		assertValid("let a : I8 = 0;", "#include <stdint.h>\nint8_t a = 0;");
+	}
+
+	@Test
+	void letI16() {
+		assertValid("let b : I16 = 0;", "#include <stdint.h>\nint16_t b = 0;");
+	}
+
+	@Test
+	void letI64() {
+		assertValid("let c : I64 = 0;", "#include <stdint.h>\nint64_t c = 0;");
+	}
+
+	@Test
+	void letU8() {
+		assertValid("let d : U8 = 0;", "#include <stdint.h>\nuint8_t d = 0;");
+	}
+
+	@Test
+	void letU16() {
+		assertValid("let e : U16 = 0;", "#include <stdint.h>\nuint16_t e = 0;");
+	}
+
+	@Test
+	void letU32() {
+		assertValid("let f : U32 = 0;", "#include <stdint.h>\nuint32_t f = 0;");
+	}
+
+	@Test
+	void letU64() {
+		assertValid("let g : U64 = 0;", "#include <stdint.h>\nuint64_t g = 0;");
 	}
 
 	private void assertValid(String input, String output) {
