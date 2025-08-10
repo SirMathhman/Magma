a7d64a4d4dea1aca7dd99f5f31318ac26bd8be16
SirMathhman <sirmathhman@gmail.com>
2025-08-10 02:34:43 -0400

    Remove alwaysFails test files and update TypeScript configuration

--- src/alwaysFails.test.js
+++
@@ -1,4 +0,0 @@
-import { alwaysFails } from './alwaysFails';
-test('alwaysFails should throw an error', function () {
-    expect(function () { return alwaysFails(); }).toThrow('This function always fails');
-});
--- test/alwaysFails.test.ts
+++ test/alwaysFails.test.ts
@@ -1,4 +1,4 @@
-import { alwaysFails } from './alwaysFails';
+import { alwaysFails } from '../src/alwaysFails';
 
 test('alwaysFails should throw an error', () => {
     expect(() => alwaysFails()).toThrow('This function always fails');
