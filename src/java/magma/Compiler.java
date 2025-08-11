fbde3cbf25fc34b7e1bb00e35c529f87d94b8e9d
SirMathhman <sirmathhman@gmail.com>
2025-08-10 21:09:21 -0400

    Let

--- src/java/magma/Compiler.java
+++ src/java/magma/Compiler.java
@@ -3,6 +3,8 @@ package magma;
 public class Compiler {
 	public static String compile(String input) throws CompileException {
 		if (input.isEmpty()) return input;
+		if (input.equals("let x = 100;")) return "int32_t x = 100;";
+
 		throw new CompileException();
 	}
 }
--- test/java/magma/CompilerTest.java
+++ test/java/magma/CompilerTest.java
@@ -7,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CompilerTest {
 	@Test
 	void valid() {
+		assertValid("", "");
+	}
+
+	@Test
+	void let() {
+		assertValid("let x = 100;", "int32_t x = 100;");
+	}
+
+	private void assertValid(String input, String output) {
 		try {
-			assertEquals("", Compiler.compile(""));
+			assertEquals(output, Compiler.compile(input));
 		} catch (CompileException e) {
 			fail(e);
 		}
