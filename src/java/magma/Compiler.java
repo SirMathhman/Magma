cb83fef4ba1b86be42fbd8ab5a3c9fe5b63a44eb
SirMathhman <sirmathhman@gmail.com>
2025-08-10 21:07:57 -0400

    Compiler

---
+++ src/java/magma/Compiler.java
@@ -0,0 +1,8 @@
+package magma;
+
+public class Compiler {
+	public static String compile(String input) throws CompileException {
+		if (input.isEmpty()) return input;
+		throw new CompileException();
+	}
+}
