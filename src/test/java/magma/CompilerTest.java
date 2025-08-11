e7384e3e70e85eaf4d31d9aecc2f6020194422df
SirMathhman <sirmathhman@gmail.com>
2025-08-10 22:42:28 -0400

    More tests

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -137,4 +137,39 @@ class CompilerTest {
 	void dereference() {
 		assertValid("let x = 100; let y = &x; let z = *y;", "int32_t x = 100; int32_t* y = &x; int32_t z = *y;");
 	}
+
+	@Test
+	void braces() {
+		assertValid("{}", "{}");
+	}
+
+	@Test
+	void statementAndBraces() {
+		assertValid("let x = 100; {}", "int32_t x = 100; {}");
+	}
+
+	@Test
+	void statementInBraces() {
+		assertValid("{let x = 100;}", "{int32_t x = 100;}");
+	}
+
+	@Test
+	void bracesNesting() {
+		assertEquals("{{{}{let x = 100;}}{}{}}", "{{{}{int32_t x = 100;}}{}{}}");
+	}
+
+	@Test
+	void testIf() {
+		assertValid("if(true){let x = 100;}", "if(true){int32_t x = 100;}");
+	}
+
+	@Test
+	void testElse() {
+		assertValid("if(true){let x = 100;} else {}", "if(true){int32_t x = 100;} else {}");
+	}
+
+	@Test
+	void testWhile() {
+		assertValid("while(true){let x = 100;}", "while(true){int32_t x = 100;}");
+	}
 }
