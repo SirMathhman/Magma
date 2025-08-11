e3accccbf93a54bb0dc91339ebddfec200eacf1e
SirMathhman <sirmathhman@gmail.com>
2025-08-10 21:14:03 -0400

    compileLet

--- src/java/magma/Compiler.java
+++ src/java/magma/Compiler.java
@@ -1,20 +1,26 @@
 package magma;
 
+import java.util.Optional;
+
 public class Compiler {
 	public static String compile(String input) throws CompileException {
 		if (input.isEmpty()) return input;
-		if (input.startsWith("let ")) {
-			final var i = input.indexOf(" = ");
-			if (i >= 0) {
-				var name = input.substring("let ".length(), i);
-				var withEnd = input.substring(i + " = ".length());
-				if (withEnd.endsWith(";")) {
-					final var slice = withEnd.substring(0, withEnd.length() - ";".length());
-					return "int32_t " + name + " = " + slice + ";";
-				}
-			}
-		}
 
-		throw new CompileException();
+		return compileLet(input).orElseThrow(CompileException::new);
+	}
+
+	private static Optional<String> compileLet(String input) {
+		if (!input.startsWith("let ")) return Optional.empty();
+		final var input1 = input.substring("let ".length());
+
+		final var i = input1.indexOf(" = ");
+		if (i < 0) return Optional.empty();
+		var name = input1.substring(0, i);
+		var withEnd = input1.substring(i + " = ".length());
+
+		if (!withEnd.endsWith(";")) return Optional.empty();
+		final var slice = withEnd.substring(0, withEnd.length() - ";".length());
+
+		return Optional.of("int32_t " + name + " = " + slice + ";");
 	}
 }
