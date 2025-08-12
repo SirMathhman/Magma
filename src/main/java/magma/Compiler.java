bc27369ef816f842e00df6aabc0e1f98131ae6e7
SirMathhman <sirmathhman@gmail.com>
2025-08-12 09:37:21 -0400

    Compiler

---
+++ src/main/java/magma/Compiler.java
@@ -0,0 +1,7 @@
+package magma;
+
+public class Compiler {
+	public static String run(String input) throws CompileException {
+		throw new CompileException("Invalid input", input);
+	}
+}
---
+++ src/test/java/magma/CompilerTest.java
@@ -0,0 +1,21 @@
+package magma;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompilerTest {
+	@Test
+	void valid() {
+		try {
+			assertEquals("", Compiler.run(""));
+		} catch (CompileException e) {
+			fail(e);
+		}
+	}
+
+	@Test
+	void invalid() {
+		assertThrows(CompileException.class, () -> Compiler.run("?"));
+	}
+}
