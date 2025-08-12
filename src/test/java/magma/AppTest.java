894782f2ba837f63aabf5c1df06ceda112d9c0da
SirMathhman <sirmathhman@gmail.com>
2025-08-11 22:59:31 -0400

    Initialize Maven project with App and AppTest classes, including basic test setup

---
+++ src/test/java/magma/AppTest.java
@@ -0,0 +1,12 @@
+package magma;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        // Empty config test
+        assertTrue(true);
+    }
+}
