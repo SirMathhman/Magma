3fa14f84d3684de36debab0493aabd06ba74e7cb
SirMathhman <sirmathhman@gmail.com>
2025-08-12 17:58:53 -0400

    Nuke

--- scripts/prettier-check.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-
-try {
-  execSync('npx prettier --check "src/**/*.{ts,js}" --write', { stdio: 'inherit' });
-} catch (err) {
-  console.error('Prettier check failed. Please format your code.');
-  process.exit(1);
-}
