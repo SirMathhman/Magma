1ec3713733e96f19a5fbb591f5454af7af7a9d63
SirMathhman <sirmathhman@gmail.com>
2025-08-12 15:04:01 -0400

    test: add additional test cases for variable transformation in compile function

--- src/compile.test.ts
+++ src/compile.test.ts
@@ -9,6 +9,14 @@ describe('compile', () => {
 		expect(compile('let x = 100;')).toBe('int32_t x = 100;');
 	});
 
+	it("transforms 'let y = 42;' to 'int32_t y = 42;'", () => {
+		expect(compile('let y = 42;')).toBe('int32_t y = 42;');
+	});
+
+	it("transforms 'let x = 7;' to 'int32_t x = 7;'", () => {
+		expect(compile('let x = 7;')).toBe('int32_t x = 7;');
+	});
+
 	it('throws an error for unsupported input', () => {
 		expect(() => compile('hello')).toThrow('Unsupported input');
 		expect(() => compile(' ')).toThrow('Unsupported input');
