a597efe55b9627a27f8ac484b0db1becadbb3fd3
SirMathhman <sirmathhman@gmail.com>
2025-08-12 21:40:23 -0400

    feat: remove alwaysThrows function and its tests

---
+++ compile.test.js
@@ -0,0 +1,11 @@
+const compile = require('./compile');
+
+describe('alwaysThrows', () => {
+  it('should return a string if input is empty', () => {
+    expect(compile('')).toBe('Input was empty.');
+  });
+
+  it('should throw an error if input is not empty', () => {
+    expect(() => compile('not empty')).toThrow('This function always throws an error unless the input is an empty string.');
+  });
+});
