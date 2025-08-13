05691caadb0c9cdb900fc66847f787108d46de9a
SirMathhman <sirmathhman@gmail.com>
2025-08-12 21:39:53 -0400

    feat: update alwaysThrows function to handle empty string input and modify tests accordingly

--- alwaysThrows.js
+++ alwaysThrows.js
@@ -1,3 +1,6 @@
-module.exports = function alwaysThrows() {
-  throw new Error("This function always throws an error.");
+module.exports = function alwaysThrows(input) {
+  if (typeof input === 'string' && input.length === 0) {
+    return "Input was empty.";
+  }
+  throw new Error("This function always throws an error unless the input is an empty string.");
 };
--- alwaysThrows.test.js
+++ alwaysThrows.test.js
@@ -1,7 +1,11 @@
 const alwaysThrows = require('./alwaysThrows');
 
 describe('alwaysThrows', () => {
-  it('should throw an error every time it is called', () => {
-    expect(() => alwaysThrows()).toThrow('This function always throws an error.');
+  it('should return a string if input is empty', () => {
+    expect(alwaysThrows('')).toBe('Input was empty.');
+  });
+
+  it('should throw an error if input is not empty', () => {
+    expect(() => alwaysThrows('not empty')).toThrow('This function always throws an error unless the input is an empty string.');
   });
 });
