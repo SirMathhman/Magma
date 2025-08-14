18fd1683b28a9ffe6ee8c76c9e93cc2bdefa689d
SirMathhman <sirmathhman@gmail.com>
2025-08-13 23:53:04 -0400

    feat: enhance compile function to handle dynamic function names

--- compile.py
+++ compile.py
@@ -1,6 +1,8 @@
 def compile(s: str):
     if s == "":
         return ""
-    if s == "fn empty() : Void => {}":
-        return "void empty(){}"
+    if s.startswith("fn ") and s.endswith("() : Void => {}"):
+        name = s[3 : s.index("() : Void => {}")].strip()
+        if name.isidentifier():
+            return f"void {name}(){{}}"
     raise RuntimeError("This function always errors")
