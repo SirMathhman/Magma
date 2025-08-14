470a69597ad16b13502ada6aeb0ea6ac5336960c
SirMathhman <sirmathhman@gmail.com>
2025-08-13 23:54:14 -0400

    feat: update compile function to support parameterized function definitions and add corresponding tests

--- compile.py
+++ compile.py
@@ -1,8 +1,17 @@
 def compile(s: str):
     if s == "":
         return ""
-    if s.startswith("fn ") and s.endswith("() : Void => {}"):
-        name = s[3 : s.index("() : Void => {}")].strip()
-        if name.isidentifier():
-            return f"void {name}(){{}}"
+    if s.startswith("fn ") and s.endswith(" : Void => {}"):
+        header = s[3 : s.index(" : Void => {}")]
+        if "(" in header and ")" in header:
+            fname, params = header.split("(", 1)
+            fname = fname.strip()
+            params = params.strip()
+            if params.endswith(")"):
+                params = params[:-1]
+            if fname.isidentifier():
+                if params == "":
+                    return f"void {fname}(){{}}"
+                if params == "value : I32":
+                    return f"void {fname}(int32_t value){{}}"
     raise RuntimeError("This function always errors")
--- test_compile.py
+++ test_compile.py
@@ -10,6 +10,12 @@ def test_compile_fn_empty():
     assert compile("fn empty() : Void => {}") == "void empty(){}"
 
 
+def test_compile_accept_i32():
+    assert (
+        compile("fn accept(value : I32) : Void => {}") == "void accept(int32_t value){}"
+    )
+
+
 def test_compile_error():
     with pytest.raises(RuntimeError, match="always errors"):
         compile("not empty")
