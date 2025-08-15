6ef6a589e26f76c8269889ecaa5bdff2d27b52eb
SirMathhman <sirmathhman@gmail.com>
2025-08-14 22:42:28 -0400

    feat: update CompilerTest to improve exception handling and add JUnit library dependencies

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -2,17 +2,21 @@ package magma;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CompilerTest {
 	@Test
 	void valid() {
-		final var actual = Compiler.compile("");
-		assertEquals("", actual);
+		try {
+			final var actual = Compiler.compile("");
+			assertEquals("", actual);
+		} catch (CompileException e) {
+			fail(e);
+		}
 	}
 
 	@Test
 	void invalid() {
-		assertEquals(CompileException.class, Compiler.compile("?"));
+		assertThrows(CompileException.class, () -> Compiler.compile("?"));
 	}
 }
