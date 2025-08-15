a660a1d0a504a424115dbe9db651d29b25a62f47
SirMathhman <sirmathhman@gmail.com>
2025-08-15 02:42:08 -0400

    test: add additional test cases for let statement compilation in ApplicationTest

--- src/test/java/magma/ApplicationTest.java
+++ src/test/java/magma/ApplicationTest.java
@@ -9,11 +9,25 @@ class ApplicationTest {
   void empty() {
     assertEquals("", Application.compile(""));
   }
-  
+
   @Test
-  void compileSimpleLetProducesC() {
+  void let() {
     String src = "let x : I32 = 0;";
     String expected = "#include <stdint.h>\r\nint32_t x = 0;";
+    assertEquals(expected, Application.compile(src));
+  }
+
+  @Test
+  void letDifferentName() {
+    String src = "let count : I32 = 10;";
+    String expected = "#include <stdint.h>\r\nint32_t count = 10;";
+    assertEquals(expected, Application.compile(src));
+  }
+
+  @Test
+  void letNegativeValue() {
+    String src = "let y : I32 = -42;";
+    String expected = "#include <stdint.h>\r\nint32_t y = -42;";
     assertEquals(expected, Application.compile(src));
   }
 }
