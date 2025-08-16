fb1572b7542dbdc62a832eedb60c29b7359b5770
SirMathhman <sirmathhman@gmail.com>
2025-08-15 20:23:51 -0400

    feat: format isLetter and isDigit functions for improved readability

--- index.c
+++ index.c
@@ -1,4 +1,21 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
-bool isLetter(char* ch){if(strlen(ch) == 0){return false;}int32_t code = ch[0];return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);}
+bool isLetter(char *ch)
+{
+  if (strlen(ch) == 0)
+  {
+    return false;
+  }
+  int32_t code = ch[0];
+  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
+bool isDigit(char *ch)
+{
+  if (strlen(ch) == 0)
+  {
+    return false;
+  }
+  int32_t code = ch[0];
+  return code >= 48 && code <= 57;
+}
