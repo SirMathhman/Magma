7c6967878695f778ea369b48b0ca03f6ef23c19b
SirMathhman <sirmathhman@gmail.com>
2025-08-17 12:46:11 -0400

    Implement code changes to enhance functionality and improve performance

--- test/alwaysThrows.test.ts
+++ test/alwaysThrows.test.ts
@@ -1,14 +1,5 @@
-import assert from 'assert';
-import path from 'path';
 import alwaysThrows from '../src/alwaysThrows';
 
-let threw = false;
-try {
-  alwaysThrows();
-} catch (e: any) {
-  threw = true;
-  assert.strictEqual(e.message, 'This function always throws');
-}
-
-assert.ok(threw, 'alwaysThrows did not throw');
-console.log('PASS');
+test('alwaysThrows throws with expected message', () => {
+  expect(() => alwaysThrows()).toThrow('This function always throws');
+});
