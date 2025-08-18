baa7f66abadf066ba616a7584f030476b7abaf10
SirMathhman <sirmathhman@gmail.com>
2025-08-17 22:56:47 -0400

    feat: add Compiler class with compile method stub

---
+++ src/main/java/Compiler.java
@@ -0,0 +1,11 @@
+public class Compiler {
+  /**
+   * Compile the given source string. Currently returns an empty string.
+   * 
+   * @param input source to compile (not used)
+   * @return empty string
+   */
+  public static String compile(String input) {
+    return "";
+  }
+}
