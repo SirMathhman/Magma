4e89a9b27bc1d847cc1becb59f437e0ba154c0a0
SirMathhman <sirmathhman@gmail.com>
2025-08-19 10:35:50 -0400

    feat: add structure test case to validate Wrapper struct in ApplicationTest

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -11,6 +11,11 @@ public class ApplicationTest {
     assertValid("fn get() => readInt(); get()", "100", 100);
   }
 
+  @Test
+  void structureTest() {
+    assertValid("struct Wrapper { field : I32 } let value = Wrapper {readInt()}; value.field", "100", 100);
+  }
+
   private void assertValid(String input, String stdin, int expected) {
     try {
       int exit = Runner.run(BEFORE_INPUT + input, stdin);
