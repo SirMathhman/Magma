bd8888ffed6a584c1f54b1881601e07ffd1a1567
SirMathhman <sirmathhman@gmail.com>
2025-08-21 20:58:03 -0400

    feat: implement CompileException and Compiler classes for error handling during compilation

---
+++ CompileException.java
@@ -0,0 +1,13 @@
+public class CompileException extends Exception {
+    public CompileException() {
+        super();
+    }
+
+    public CompileException(String message) {
+        super(message);
+    }
+
+    public CompileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ Compiler.java
@@ -0,0 +1,5 @@
+public class Compiler {
+    public void compile() throws CompileException {
+        throw new CompileException("Compilation always fails in this demo.");
+    }
+}
