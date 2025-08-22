1760558a8584b9601b6856c02e9b368e8dd9292a
SirMathhman <sirmathhman@gmail.com>
2025-08-22 11:44:43 -0400

    test: add missing newline and format function tests for consistency

--- src/test/java/magma/FunctionTest.java
+++ src/test/java/magma/FunctionTest.java
@@ -7,6 +7,11 @@ import static magma.TestUtils.*;
 public class FunctionTest {
   @Test
   void function() {
+    assertValidWithPrelude("fn get() : I32 => readInt(); get()", "100", 100);
+  }
+
+  @Test
+  void functionWithImplicitReturnType() {
     assertValidWithPrelude("fn get() => readInt(); get()", "100", 100);
   }
 }
