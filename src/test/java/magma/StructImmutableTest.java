dfe85bb37d929ef181ea40cd6822a2dd20bedf0c
SirMathhman <sirmathhman@gmail.com>
2025-08-22 23:37:29 -0400

    style: format StructImmutableTest.java for consistency

--- src/test/java/magma/StructImmutableTest.java
+++ src/test/java/magma/StructImmutableTest.java
@@ -5,9 +5,9 @@ import static magma.TestUtils.assertInvalid;
 import org.junit.jupiter.api.Test;
 
 public class StructImmutableTest {
-    @Test
-    void cannotAssignField() {
-        // Attempt to assign to a struct field should be invalid
-        assertInvalid("struct S { f : I32 } let s = S { readInt() }; s.f = 10;");
-    }
+  @Test
+  void cannotAssignField() {
+    // Attempt to assign to a struct field should be invalid
+    assertInvalid("struct S { f : I32 } let s = S { readInt() }; s.f = 10;");
+  }
 }
