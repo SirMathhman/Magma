cf03c49fa4c6de12944b39719e201378fe9dd74f
SirMathhman <sirmathhman@gmail.com>
2025-08-22 23:45:52 -0400

    style: format BlockTest.java for consistency

--- src/test/java/magma/BlockTest.java
+++ src/test/java/magma/BlockTest.java
@@ -3,8 +3,13 @@ package magma;
 import org.junit.jupiter.api.Test;
 
 public class BlockTest {
-	@Test
-	public void blockWithValue() {
-		TestUtils.assertValidWithPrelude("{5}", "", 5);
-	}
+  @Test
+  public void blockWithValue() {
+    TestUtils.assertValidWithPrelude("{5}", "", 5);
+  }
+
+  @Test
+  public void letBeforeBlockIsVisibleInsideBlock() {
+    TestUtils.assertValidWithPrelude("let x = readInt(); {x}", "7", 7);
+  }
 }
