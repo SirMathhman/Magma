fb7d61334cb364507dba886b97b9a105dd8cb395
SirMathhman <sirmathhman@gmail.com>
2025-08-24 13:25:37 -0400

    feat: add initial project files and configuration for Magma language implementation

--- src/magma/Main.java
+++ src/magma/Main.java
@@ -1,7 +1,16 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 	public static void main(String[] args) {
-
+		try {
+			Files.writeString(Paths.get(".", "main.c"), "int main(){\r\n\treturn 0;\r\n}");
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
 	}
 }
