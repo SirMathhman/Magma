024c7c010b03e8ed8601c57f189dd1133e2da947
SirMathhman <sirmathhman@gmail.com>
2025-08-25 17:00:32 -0400

    feat: enhance interpret method to handle number parsing errors

--- src/main/java/magma/Interpreter.java
+++ src/main/java/magma/Interpreter.java
@@ -1,10 +1,16 @@
 package magma;
 
+import magma.result.Err;
 import magma.result.Ok;
 import magma.result.Result;
 
 public class Interpreter {
 	public static Result<String, InterpretError> interpret(String input) {
-		return new Ok<>(input);
+		try {
+			Integer.parseInt(input);
+			return new Ok<>(input);
+		} catch (NumberFormatException e) {
+			return new Err<>(new InterpretError("Not a number", input));
+		}
 	}
 }
