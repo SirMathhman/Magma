7ed5c4863eb3bc70cab7179b53df79b041fed35b
SirMathhman <sirmathhman@gmail.com>
2025-08-24 20:39:23 -0400

    refactor: update InterpretingException to include context in constructors and remove outdated tests

--- src/main/java/org/example/InterpretingException.java
+++ src/main/java/org/example/InterpretingException.java
@@ -4,11 +4,11 @@ package org.example;
  * Thrown when interpretation of an input fails due to invalid input.
  */
 public class InterpretingException extends RuntimeException {
-  public InterpretingException(String message) {
-    super(message);
+  public InterpretingException(String message, String context) {
+    super(message + ": " + context);
   }
 
-  public InterpretingException(String message, Throwable cause) {
-    super(message, cause);
+  public InterpretingException(String message, Throwable cause, String context) {
+    super(message + ": " + context, cause);
   }
 }
--- src/test/java/org/example/IntrepreterBasicTest.java
+++ src/test/java/org/example/IntrepreterBasicTest.java
@@ -1,72 +1,12 @@
 package org.example;
 
-import static org.example.TestUtils.assertValid;
 import static org.example.TestUtils.assertInvalid;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class IntrepreterBasicTest {
   @Test
-  void interpretShouldErrorOnBareWords() {
-    assertInvalid("hello");
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = { "5U8", "5U16", "5U32", "5U64" })
-  void interpretShouldStripUSuffixes(String input) {
-    assertValid(input, "5");
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = { "5I8", "5I16", "5I32", "5I64" })
-  void interpretShouldStripISuffixes(String input) {
-    assertValid(input, "5");
-  }
-
-  @Test
-  void interpretShouldAddTwoIntegers() {
-    assertValid("5 + 7", "12");
-  }
-
-  @Test
-  void interpretShouldSubtractTwoIntegers() {
-    assertValid("5 - 7", "-2");
-  }
-
-  @Test
-  void interpretShouldReturnTrueLiteral() {
-    assertValid("true", "true");
-  }
-
-  @Test
-  void interpretShouldReturnFalseLiteral() {
-    assertValid("false", "false");
-  }
-
-  @Test
-  void interpretShouldHandleLogicalAnd() {
-    assertInvalid("1 && 0");
-  }
-
-  @Test
-  void interpretShouldHandleLogicalOr() {
-    assertInvalid("0 || 1");
-  }
-
-  @Test
-  void interpretShouldHandleBooleanAnd() {
-    assertValid("true && false", "false");
-  }
-
-  @Test
-  void interpretShouldHandleBooleanOr() {
-    assertValid("false || true", "true");
-  }
-
-  @Test
-  void interpretShouldMultiplyTwoIntegers() {
-    assertValid("5 * 7", "35");
+  void undefined() {
+    assertInvalid("test");
   }
 }
