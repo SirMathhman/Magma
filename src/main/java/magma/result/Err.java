f49929e85ed0e6a3e992061949615171f669a007
SirMathhman <sirmathhman@gmail.com>
2025-08-25 16:58:23 -0400

    feat: enhance Result type with consume method and isErr implementation

--- src/main/java/magma/result/Err.java
+++ src/main/java/magma/result/Err.java
@@ -1,3 +1,15 @@
 package magma.result;
 
-public record Err<Value, Error>(Error error) implements Result<Value, Error> {}
+import java.util.function.Consumer;
+
+public record Err<Value, Error>(Error error) implements Result<Value, Error> {
+	@Override
+	public void consume(Consumer<Value> ifOk, Consumer<Error> ifError) {
+		ifError.accept(error);
+	}
+
+	@Override
+	public boolean isErr() {
+		return true;
+	}
+}
--- src/main/java/magma/result/Ok.java
+++ src/main/java/magma/result/Ok.java
@@ -1,3 +1,15 @@
 package magma.result;
 
-public record Ok<Value, Error>(Value value) implements Result<Value, Error> {}
+import java.util.function.Consumer;
+
+public record Ok<Value, Error>(Value value) implements Result<Value, Error> {
+	@Override
+	public void consume(Consumer<Value> ifOk, Consumer<Error> ifError) {
+		ifOk.accept(value);
+	}
+
+	@Override
+	public boolean isErr() {
+		return false;
+	}
+}
