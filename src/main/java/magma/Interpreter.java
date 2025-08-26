c1b32ec486afbdd1b15368f7a30f1158d5269c24
SirMathhman <sirmathhman@gmail.com>
2025-08-26 11:45:54 -0400

    refactor: move Err, Ok, and Result classes to magma.result package

--- src/main/java/magma/Interpreter.java
+++ src/main/java/magma/Interpreter.java
@@ -1,8 +1,13 @@
 package magma;
 
+import magma.result.Err;
+import magma.result.Ok;
+import magma.result.Result;
+
 public class Interpreter {
 	public static Result<String, InterpretError> interpret(String input) {
 		try {
+			Integer.parseInt(input);
 			return new Ok<>(input);
 		} catch (NumberFormatException e) {
 			return new Err<>(new InterpretError("Undefined value", input));
