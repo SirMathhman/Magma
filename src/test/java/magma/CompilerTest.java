765077626ef75558c7f606a67a70b02a0c058f50
SirMathhman <sirmathhman@gmail.com>
2025-08-31 11:48:47 -0400

    test: update CompilerTest to validate extern function calls and adjust assertValid parameters

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -12,7 +12,7 @@ import magma.result.Ok;
 public class CompilerTest {
   @Test
   void valid() {
-    assertValid("5", "5");
+    assertValid("extern fn readInt() : I32; readInt()", "5", "5");
   }
 
   @Test
@@ -20,10 +20,10 @@ public class CompilerTest {
     assertInvalid("test");
   }
 
-  static void assertValid(String source, String expectedOutput) {
-    var result = Runner.run(source);
+  static void assertValid(String source, String stdIn, String stdOut) {
+    var result = Runner.run(source, stdIn);
     if (result instanceof Ok<String, CompileError> ok) {
-      assertEquals(expectedOutput, ok.value());
+      assertEquals(stdOut, ok.value());
     } else if (result instanceof Err<String, CompileError> err) {
       fail(err.error().getMessage());
     } else {
