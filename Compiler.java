12f4fbd688aaf1cc3752d1146ed4bcdcc68efdc3
SirMathhman <sirmathhman@gmail.com>
2025-08-31 20:08:47 -0400

    feat: add initial implementation of Compiler, Location, Source, and Unit classes

---
+++ Compiler.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+
+public class Compiler {
+  public Set<Unit> compile(Set<Unit> units) {
+    // TODO: implement compilation logic
+    return units;
+  }
+}
---
+++ Source.java
@@ -0,0 +1,2 @@
+public record Unit(Location location, String extension, String input) {
+}
