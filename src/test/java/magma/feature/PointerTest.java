c56deb7cdd9d82520ca051d8648b5ac79fca6b6b
SirMathhman <sirmathhman@gmail.com>
2025-09-02 19:21:28 -0400

    refactor: add test for invalid write through mutable pointer when pointee is not mutable in PointerTest

--- src/test/java/magma/feature/PointerTest.java
+++ src/test/java/magma/feature/PointerTest.java
@@ -3,6 +3,7 @@ package magma.feature;
 import org.junit.jupiter.api.Test;
 
 import static magma.TestUtils.assertAllValidWithPrelude;
+import static magma.TestUtils.assertAllInvalidWithPrelude;
 
 public class PointerTest {
   @Test
@@ -21,5 +22,12 @@ public class PointerTest {
   void writeThroughMutPointerDecl() {
     // let mut x : I32 = 0; let y : *mut I32 = &x; *y = readInt(); x
     assertAllValidWithPrelude("let mut x : I32 = 0; let y : *mut I32 = &x; *y = readInt(); x", "55", "55");
+  }
+
+  @Test
+  void writeThroughMutPointerDeclInvalidWhenPointeeNotMutable() {
+    // let x : I32 = 0; let y : *mut I32 = &x; *y = readInt(); x -- should be
+    // invalid
+    assertAllInvalidWithPrelude("let x : I32 = 0; let y : *mut I32 = &x; *y = readInt(); x");
   }
 }
