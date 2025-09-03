a0af5f2063a26f61fb52840db7073e2ab8c50d1b
SirMathhman <sirmathhman@gmail.com>
2025-09-03 14:24:29 -0400

    Delete

--- src/main/java/magma/CompileError.java
+++
@@ -1,7 +0,0 @@
-package magma;
-
-/**
- * Simple compile error container.
- */
-public record CompileError(String message, String source) {
-}
--- src/main/java/magma/CompilerDump.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-public class CompilerDump {
-  // Non-void helper to satisfy project checkstyle rules
-  public static Result<String, CompileError> dump() {
-    String src = "intrinsic fn readInt() : I32; readInt()";
-    return Compiler.compile(src);
-  }
-}
