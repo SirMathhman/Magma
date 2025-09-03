236d4406f362e73dcf374db363d4011c8830cc0d
SirMathhman <sirmathhman@gmail.com>
2025-09-03 00:43:47 -0400

    test: add multiplication test for three readInts in LetExtrasTest

--- src/test/java/magma/LetExtrasTest.java
+++ src/test/java/magma/LetExtrasTest.java
@@ -9,6 +9,11 @@ public class LetExtrasTest {
   }
 
   @Test
+  void letInitWithThreeReadIntsMultiplication() {
+    TestHelpers.assertValid("let x = readInt() * readInt() * readInt(); x", "10\r\n2\r\n3", "60");
+  }
+
+  @Test
   void letInitWithThreeReadIntsSubtraction() {
     TestHelpers.assertValid("let x = readInt() - readInt() - readInt(); x", "10\r\n20\r\n30", "-40");
   }
