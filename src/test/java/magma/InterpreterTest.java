0997767b2eb3fbc08212ce2af1475affb58f3203
SirMathhman <sirmathhman@gmail.com>
2025-09-03 15:32:39 -0400

    feat: update maximum method parameters to 3 and refactor InterpreterTest for improved readability

--- src/test/java/magma/InterpreterTest.java
+++ src/test/java/magma/InterpreterTest.java
@@ -8,30 +8,28 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class InterpreterTest {
   @Test
   void empty() {
-    var interpreter = new Interpreter();
-    var result = interpreter.interpret("");
-    switch (result) {
-      case Ok<String, InterpretError>(var actual) -> assertEquals("", actual);
-      case Err<String, InterpretError>(var err) -> fail(err.display());
-    }
+    assertValid("", "");
   }
 
   @Test
   void readInt() {
-    var interpreter = new Interpreter();
-    var result = interpreter.interpret("intrinsic fn readInt() : I32; readInt()", "100");
-    switch (result) {
-      case Ok<String, InterpretError>(var actual) -> assertEquals("100", actual);
-      case Err<String, InterpretError>(var err) -> fail(err.display());
-    }
+    assertValid("intrinsic fn readInt() : I32; readInt()", "100", "100");
   }
 
   @Test
   void readIntSum() {
+    assertValid("intrinsic fn readInt() : I32; readInt() + readInt()", "10\r\n20", "30");
+  }
+
+  private static void assertValid(String source, String expected) {
+    assertValid(source, "", expected);
+  }
+
+  private static void assertValid(String source, String externalInput, String expected) {
     var interpreter = new Interpreter();
-    var result = interpreter.interpret("intrinsic fn readInt() : I32; readInt() + readInt()", "10\r\n20");
+    var result = interpreter.interpret(source, externalInput);
     switch (result) {
-      case Ok<String, InterpretError>(var actual) -> assertEquals("30", actual);
+      case Ok<String, InterpretError>(var actual) -> assertEquals(expected, actual);
       case Err<String, InterpretError>(var err) -> fail(err.display());
     }
   }
