fa0b73cbc3f46f59c21bf50ca296587f510bc8d7
SirMathhman <sirmathhman@gmail.com>
2025-09-03 20:24:12 -0400

    feat: add initial implementation of Interpreter and InterpreterTest classes

---
+++ src/main/test/magma/InterpreterTest.java
@@ -0,0 +1,11 @@
+package magma;
+
+import org.junit.jupiter.api.Test;
+
+public class InterpreterTest {
+    // Empty test class as requested
+    @Test
+    void dummyTest() {
+        // No implementation
+    }
+}
