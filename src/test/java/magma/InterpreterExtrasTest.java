332bd39ddf6ffe70236cf954e23c440414e89c09
SirMathhman <sirmathhman@gmail.com>
2025-09-04 00:45:41 -0400

    feat: add test for typed let with assignment in InterpreterExtrasTest

--- src/test/java/magma/InterpreterExtrasTest.java
+++ src/test/java/magma/InterpreterExtrasTest.java
@@ -7,4 +7,9 @@ public class InterpreterExtrasTest {
     void interpretPassWithMultipleParametersReturnsFirstQuotedArg() {
         TestHelper.assertInterpretsTo("fn pass(a : *[U8], b : I32) => a; pass(\"hello\", 3)", "\"hello\"");
     }
+
+    @Test
+    void interpretTypedLetWithOtherLetAndAssignmentReturnsValue() {
+        TestHelper.assertInterpretsTo("let x : I32; let y = 10; x = 0; x", "0");
+    }
 }
