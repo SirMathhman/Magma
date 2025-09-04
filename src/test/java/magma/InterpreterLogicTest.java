bac69d23bde6dbb830e1d5f5ce0ea8575a5a015c
SirMathhman <sirmathhman@gmail.com>
2025-09-04 00:09:57 -0400

    style: format test methods in InterpreterLogicTest for consistent indentation

--- src/test/java/magma/InterpreterLogicTest.java
+++ src/test/java/magma/InterpreterLogicTest.java
@@ -1,31 +1,30 @@
 package magma;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InterpreterLogicTest {
   @Test
   void interpretLogicalAndReturnsTrue() {
-  TestHelper.assertInterpretsTo("true && true", "true");
+    TestHelper.assertInterpretsTo("true && true", "true");
   }
 
   @Test
   void interpretIfWithLogicalAndConditionReturnsThenBranch() {
-  TestHelper.assertInterpretsTo("if (true && true) 3 else 5", "3");
+    TestHelper.assertInterpretsTo("if (true && true) 3 else 5", "3");
   }
 
   @Test
   void interpretComparisonReturnsFalse() {
-  TestHelper.assertInterpretsTo("3 >= 5", "false");
+    TestHelper.assertInterpretsTo("3 >= 5", "false");
   }
 
   @Test
   void interpretNotEqualsReturnsTrue() {
-  TestHelper.assertInterpretsTo("5 != 3", "true");
+    TestHelper.assertInterpretsTo("5 != 3", "true");
   }
 
   @Test
   void interpretNotEqualsReturnsFalse() {
-  TestHelper.assertInterpretsTo("3 != 3", "false");
+    TestHelper.assertInterpretsTo("3 != 3", "false");
   }
 }
