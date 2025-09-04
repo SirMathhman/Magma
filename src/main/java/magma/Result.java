83dc87393e98429e3c2510b41bd01c50343cb654
SirMathhman <sirmathhman@gmail.com>
2025-09-03 20:22:17 -0400

    Delete

--- src/main/java/magma/Result.java
+++
@@ -1,5 +0,0 @@
-package magma;
-
-// Sealed Result type for explicit success/error handling
-public sealed interface Result<T, E> permits Ok, Err {
-}
--- src/test/java/magma/ParenthesesTest.java
+++
@@ -1,8 +0,0 @@
-package magma;
-
-public class ParenthesesTest {
-  @org.junit.jupiter.api.Test
-  public void parenthesizedZero() {
-    TestHelpers.assertValid("(0)", "0");
-  }
-}
