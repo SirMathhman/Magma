38e13980aba42b73da7784f3b5146864bacb9404
SirMathhman <sirmathhman@gmail.com>
2025-09-05 19:57:09 -0400

    style: format code in Interpreter class for consistency

--- src/main/java/magma/Interpreter.java
+++ src/main/java/magma/Interpreter.java
@@ -1,14 +1,14 @@
 package magma;
 
 public class Interpreter {
-    public Result<String, InterpretError> interpret(String input) {
-        if (input == null) {
-            return new Err<>(new InterpretError("input is null"));
-        }
-        if (input.equals("")) {
-            return new Ok<>("");
-        }
-        // simple behaviour: echo input
-        return new Ok<>(input);
-    }
+	public Result<String, InterpretError> interpret(String input) {
+		if (input == null) {
+			return new Err<>(new InterpretError("input is null"));
+		}
+		if (input.equals("")) {
+			return new Ok<>("");
+		}
+		// simple behaviour: echo input
+		return new Ok<>(input);
+	}
 }
