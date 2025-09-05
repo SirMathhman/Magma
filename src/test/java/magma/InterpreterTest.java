551eacf4ae73b4e76d0305a2be3a1a6694890fde
SirMathhman <sirmathhman@gmail.com>
2025-09-05 19:56:57 -0400

    style: format code in InterpreterTest for consistency

--- src/test/java/magma/InterpreterTest.java
+++ src/test/java/magma/InterpreterTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InterpreterTest {
-    @Test
-    void testInterpretEmptyString() {
-        Interpreter interpreter = new Interpreter();
-        Result<String, InterpretError> result = interpreter.interpret("");
-        assertTrue(result instanceof Ok);
-        assertEquals("", ((Ok<String, InterpretError>) result).getValue());
-    }
+	@Test
+	void testInterpretEmptyString() {
+		Interpreter interpreter = new Interpreter();
+		Result<String, InterpretError> result = interpreter.interpret("");
+		assertTrue(result instanceof Ok);
+		assertEquals("", ((Ok<String, InterpretError>) result).getValue());
+	}
 }
