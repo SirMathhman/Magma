45c72617cae9987bae7920e3c6891090601c49da
SirMathhman <sirmathhman@gmail.com>
2025-09-07 10:41:22 -0400

    feat: update project configuration to support Java 24 with preview features and modify main application structure

--- src/main/java/magma/App.java
+++ src/main/java/magma/App.java
@@ -1,11 +1,3 @@
-package magma;
-
-public class App {
-    public static String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(greet(args.length > 0 ? args[0] : "world"));
-    }
+void main() {
+    System.out.println("Hello World!");
 }
--- src/test/java/magma/AppTest.java
+++ src/test/java/magma/AppTest.java
@@ -2,11 +2,8 @@ package magma;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class AppTest {
     @Test
-    public void greetShouldReturnHelloName() {
-        assertEquals("Hello, Alice!", App.greet("Alice"));
+    public void test() {
     }
 }
