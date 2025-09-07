e2bc92c0dc4412fa69e92b3da4b07aa0b1d7ce08
SirMathhman <sirmathhman@gmail.com>
2025-09-07 09:57:37 -0400

    Delete

--- src/main/java/magma/compiler/CompileError.java
+++
@@ -1,8 +0,0 @@
-package magma.compiler;
-
-/**
- * Simple compile error that contains a message and the source representation
- * (the input that caused the error).
- */
-public record CompileError(String message, String source) {
-}
