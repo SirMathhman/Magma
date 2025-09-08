b53229ff214b9ac177de2f8674ea8a37c02dde07
SirMathhman <sirmathhman@gmail.com>
2025-09-07 22:48:03 -0400

    Nuke

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# Runs the Magma CLI runner. Usage: ./run.sh [path-to-script]
-SCRIPT_PATH=${1:-src/main/magma/magma/Main.mgs}
-
-mvn -q compile || exit 1
-java --enable-preview -cp "target/classes" magma.Main "$SCRIPT_PATH"
