66d8c9de1275ffc8ad30b9cefcb2ed69217808d4
SirMathhman <sirmathhman@gmail.com>
2025-09-07 23:01:07 -0400

    feat: refactor project structure and add Java to C conversion functionality

---
+++ src/java/magma/Main.java
@@ -0,0 +1,45 @@
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		final var sourceDirectory = Paths.get(".", "src", "java");
+		try (var stream = Files.walk(sourceDirectory)) {
+			final var sources = stream.filter(Files::isRegularFile)
+																.filter(path -> path.toString().endsWith(".java"))
+																.collect(Collectors.toSet());
+
+			for (var source : sources) {
+				final var input = Files.readString(source);
+
+				final var relative = sourceDirectory.relativize(source);
+				final var parent = relative.getParent();
+				final var segments = new ArrayList<String>();
+				for (var i = 0; i < parent.getNameCount(); i++) {
+					segments.add(parent.getName(i).toString());
+				}
+
+				var targetDirectory = Paths.get(".", "src", "windows");
+				for (var segment : segments) {
+					targetDirectory = targetDirectory.resolve(segment);
+				}
+
+				Files.createDirectories(targetDirectory);
+
+				final var fileName = relative.getFileName().toString();
+				final var index = fileName.lastIndexOf(".");
+				final var name = fileName.substring(0, index);
+				final var resolve = targetDirectory.resolve(name + ".c");
+				Files.writeString(resolve, "/*" + input + "*/");
+			}
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
+	}
+}
--- src/magma/Main.java
+++
@@ -1,7 +0,0 @@
-package magma;
-
-public class Main {
-	public static void main(String[] args) {
-
-	}
-}
