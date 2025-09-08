b7a4625bbeddb44e740d051d22eac3fae8f7ce15
SirMathhman <sirmathhman@gmail.com>
2025-09-07 23:07:50 -0400

    feat: improve parsing logic in Java and C conversion functions

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -46,13 +46,16 @@ public class Main {
 	private static String compile(String input) {
 		final var segments = new ArrayList<String>();
 		var buffer = new StringBuilder();
+		var depth = 0;
 		for (var i = 0; i < input.length(); i++) {
 			final var c = input.charAt(i);
 			buffer.append(c);
-			if (c == ';') {
+			if (c == ';' && depth == 0) {
 				segments.add(buffer.toString());
-				buffer = new StringBuilder();
+				buffer.setLength(0);
 			}
+			if (c == '{') depth++;
+			if (c == '}') depth--;
 		}
 		segments.add(buffer.toString());
 
--- src/windows/magma/Main.c
+++ src/windows/magma/Main.c
@@ -8,63 +8,66 @@ import java.util.stream.Collectors;*//*
 
 public class Main {
 	public static void main(String[] args) {
-		final var sourceDirectory = Paths.get(".", "src", "java");*//*
+		final var sourceDirectory = Paths.get(".", "src", "java");
 		try (var stream = Files.walk(sourceDirectory)) {
 			final var sources = stream.filter(Files::isRegularFile)
 																.filter(path -> path.toString().endsWith(".java"))
-																.collect(Collectors.toSet());*//*
+																.collect(Collectors.toSet());
 
 			for (var source : sources) {
-				final var input = Files.readString(source);*//*
+				final var input = Files.readString(source);
 
-				final var relative = sourceDirectory.relativize(source);*//*
-				final var parent = relative.getParent();*//*
-				final var segments = new ArrayList<String>();*//*
-				for (var i = 0;*//* i < parent.getNameCount();*//* i++) {
-					segments.add(parent.getName(i).toString());*//*
+				final var relative = sourceDirectory.relativize(source);
+				final var parent = relative.getParent();
+				final var segments = new ArrayList<String>();
+				for (var i = 0; i < parent.getNameCount(); i++) {
+					segments.add(parent.getName(i).toString());
 				}
 
-				var targetDirectory = Paths.get(".", "src", "windows");*//*
+				var targetDirectory = Paths.get(".", "src", "windows");
 				for (var segment : segments) {
-					targetDirectory = targetDirectory.resolve(segment);*//*
+					targetDirectory = targetDirectory.resolve(segment);
 				}
 
-				Files.createDirectories(targetDirectory);*//*
+				Files.createDirectories(targetDirectory);
 
-				final var fileName = relative.getFileName().toString();*//*
-				final var index = fileName.lastIndexOf(".");*//*
-				final var name = fileName.substring(0, index);*//*
-				final var resolve = targetDirectory.resolve(name + ".c");*//*
-				Files.writeString(resolve, compile(input));*//*
+				final var fileName = relative.getFileName().toString();
+				final var index = fileName.lastIndexOf(".");
+				final var name = fileName.substring(0, index);
+				final var resolve = targetDirectory.resolve(name + ".c");
+				Files.writeString(resolve, compile(input));
 			}
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();*//*
+			e.printStackTrace();
 		}
 	}
 
 	private static String compile(String input) {
-		final var segments = new ArrayList<String>();*//*
-		var buffer = new StringBuilder();*//*
-		for (var i = 0;*//* i < input.length();*//* i++) {
-			final var c = input.charAt(i);*//*
-			buffer.append(c);*//*
-			if (c == ';*//*') {
-				segments.add(buffer.toString());*//*
-				buffer = new StringBuilder();*//*
+		final var segments = new ArrayList<String>();
+		var buffer = new StringBuilder();
+		var depth = 0;
+		for (var i = 0; i < input.length(); i++) {
+			final var c = input.charAt(i);
+			buffer.append(c);
+			if (c == ';' && depth == 0) {
+				segments.add(buffer.toString());
+				buffer.setLength(0);
 			}
+			if (c == '{') depth++;
+			if (c == '}') depth--;
 		}
-		segments.add(buffer.toString());*//*
+		segments.add(buffer.toString());
 
-		return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());*//*
+		return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());
 	}
 
 	private static String wrap(String input) {
-		return "start" + input.replace("start", "start").replace("end", "end") + "end";*//*
+		return "start" + input.replace("start", "start").replace("end", "end") + "end";
 	}
 
 	private static String compileRootSegment(String input) {
-		return wrap(input);*//*
+		return wrap(input);
 	}
 }
 */
