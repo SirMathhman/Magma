b7f8343aa7778147068cb24e19384605248cb996
SirMathhman <sirmathhman@gmail.com>
2025-09-07 23:29:46 -0400

    feat: enhance compilation logic by refining type handling and improving parameter management

--- src/windows/magma/Main.c
+++ src/windows/magma/Main.c
@@ -1,32 +1,32 @@
 /*public */struct Main {};
 /*public static*/ void main(char** args){/*
-		final var sourceDirectory = Paths.get(".", "src", "java");*//*
-		try (var stream = Files.walk(sourceDirectory)) {
-			final var sources = stream.filter(Files::isRegularFile)
-																.filter(path -> path.toString().endsWith(".java"))
-																.collect(Collectors.toSet());
+		final Path sourceDirectory = Paths.get(".", "src", "java");*//*
+		try (Stream<Path> stream = Files.walk(sourceDirectory)) {
+			final Set<Path> sources = stream.filter(Files::isRegularFile)
+																			.filter(path -> path.toString().endsWith(".java"))
+																			.collect(Collectors.toSet());
 
-			for (var source : sources) {
-				final var input = Files.readString(source);
+			for (Path source : sources) {
+				final String input = Files.readString(source);
 
-				final var relative = sourceDirectory.relativize(source);
-				final var parent = relative.getParent();
-				final var segments = new ArrayList<String>();
-				for (var i = 0; i < parent.getNameCount(); i++) {
+				final Path relative = sourceDirectory.relativize(source);
+				final Path parent = relative.getParent();
+				final ArrayList<String> segments = new ArrayList<>();
+				for (int i = 0; i < parent.getNameCount(); i++) {
 					segments.add(parent.getName(i).toString());
 				}
 
-				var targetDirectory = Paths.get(".", "src", "windows");
-				for (var segment : segments) {
+				Path targetDirectory = Paths.get(".", "src", "windows");
+				for (String segment : segments) {
 					targetDirectory = targetDirectory.resolve(segment);
 				}
 
 				Files.createDirectories(targetDirectory);
 
-				final var fileName = relative.getFileName().toString();
-				final var index = fileName.lastIndexOf(".");
-				final var name = fileName.substring(0, index);
-				final var resolve = targetDirectory.resolve(name + ".c");
+				final String fileName = relative.getFileName().toString();
+				final int index = fileName.lastIndexOf(".");
+				final String name = fileName.substring(0, index);
+				final Path resolve = targetDirectory.resolve(name + ".c");
 				Files.writeString(resolve, compile(input));
 			}
 		}*//* catch (IOException e) {
@@ -38,11 +38,11 @@
 		return compileSegments(input, Main::compileRootSegment);*//*
 	*/}
 /*private static*/ char* compileSegments(/*String input, Function<String,*/ /*String>*/ mapper){/*
-		final var segments = new ArrayList<String>();*//*
-		var buffer = new StringBuilder();*//*
-		var depth = 0;*//*
-		for (var i = 0;*//* i < input.length();*//* i++) {
-			final var c = input.charAt(i);
+		final ArrayList<String> segments = new ArrayList<>();*//*
+		StringBuilder buffer = new StringBuilder();*//*
+		int depth = 0;*//*
+		for (int i = 0;*//* i < input.length();*//* i++) {
+			final char c = input.charAt(i);
 			buffer.append(c);
 			if (c == ';' && depth == 0) {
 				segments.add(buffer.toString());
@@ -65,18 +65,18 @@
 	}*//*private static String wrap(String input) {
 		return "start" + input.replace("start", "start").replace("end", "end") + "end";
 	}*//*private static String compileRootSegment(String input) {
-		final var stripped = input.strip();
+		final String stripped = input.strip();
 		if (stripped.startsWith("package ") || stripped.startsWith("import ")) return "";
 
-		final var classIndex = stripped.indexOf("*/struct ");
+		final int classIndex = stripped.indexOf("*/struct ");
 		if (classIndex >= 0) {};
 /*
-			final var modifiers = stripped.substring(0, classIndex);*//*
-			final var remainder = stripped.substring(classIndex + "class ".length());*//*
-			final var i = remainder.indexOf("{");
+			final String modifiers = stripped.substring(0, classIndex);*//*
+			final String remainder = stripped.substring(classIndex + "class ".length());*//*
+			final int i = remainder.indexOf("{");
 			if (i >= 0) {
-				final var name = remainder.substring(0, i).strip();
-				final var content = remainder.substring(i + "{".length());
+				final String name = remainder.substring(0, i).strip();
+				final String content = remainder.substring(i + "{".length());
 				return wrap(modifiers) + "struct " + name + " {};" + System.lineSeparator() +
 							 compileSegments(content, Main::compileClassSegment);
 			}
@@ -84,17 +84,17 @@
 
 		return wrap(stripped);
 	}*//*private static*/ char* compileClassSegment(char* input){/*
-		final var i = input.indexOf("(");*//*
+		final int i = input.indexOf("(");*//*
 		if (i >= 0) {
-			final var definition = input.substring(0, i);
-			final var withParams = input.substring(i + "(".length());
-			final var i1 = withParams.indexOf(")");
+			final String definition = input.substring(0, i);
+			final String withParams = input.substring(i + "(".length());
+			final int i1 = withParams.indexOf(")");
 			if (i1 >= 0) {
-				final var params = withParams.substring(0, i1);
-				final var content = withParams.substring(i1 + ")".length()).strip();
+				final String params = withParams.substring(0, i1);
+				final String content = withParams.substring(i1 + ")".length()).strip();
 				if (content.startsWith("{") && content.endsWith("}")) {
-					final var slice = content.substring(1, content.length() - 1);
-					final var s = compileDefinition(definition);
+					final String slice = content.substring(1, content.length() - 1);
+					final Optional<String> s = compileDefinition(definition);
 					if (s.isPresent()) {
 						return s.get() + "(" + compileDefinition(params).orElseGet(() -> wrap(params)) + "){" +
 									 compileSegments(slice, Main::compileFunctionSegment) + "}" + System.lineSeparator();
@@ -106,31 +106,31 @@
 		return wrap(input);*//*
 	*/}
 /*private static*/ /*Optional<String>*/ compileDefinition(char* input){/*
-		final var stripped = input.strip();*//*
-		final var i = stripped.lastIndexOf(" ");*//*
+		final String stripped = input.strip();*//*
+		final int i = stripped.lastIndexOf(" ");*//*
 		if (i < 0) return Optional.empty();*//*
 
-		final var beforeName = stripped.substring(0, i).strip();*//*
-		final var name = stripped.substring(i + " ".length());*//*
+		final String beforeName = stripped.substring(0, i).strip();*//*
+		final String name = stripped.substring(i + " ".length());*//*
 
-		final var i1 = beforeName.lastIndexOf(" ");*//*
+		final int i1 = beforeName.lastIndexOf(" ");*//*
 		if (i1 < 0) {
 			return Optional.of(compileType(beforeName) + " " + name);
 		}*//*
 
-		final var modifiers = beforeName.substring(0, i1);*//*
-		final var type = beforeName.substring(i1 + " ".length());*//*
+		final String modifiers = beforeName.substring(0, i1);*//*
+		final String type = beforeName.substring(i1 + " ".length());*//*
 
 		return Optional.of(wrap(modifiers) + " " + compileType(type) + " " + name);*//*
 	*/}
 /*private static*/ char* compileType(char* input){/*
-		final var stripped = input.strip();*//*
+		final String stripped = input.strip();*//*
 
 		if (stripped.equals("void")) return "void";*//*
 		if (stripped.equals("String")) return "char*";*//*
 
 		if (stripped.endsWith("[]")) {
-			final var slice = stripped.substring(0, stripped.length() - "[]".length());
+			final String slice = stripped.substring(0, stripped.length() - "[]".length());
 			return compileType(slice) + "*";
 		}*//*
 
