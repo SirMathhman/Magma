9245785198e3859b9e73bfebee5a83bb46483397
SirMathhman <sirmathhman@gmail.com>
2025-09-11 17:27:38 -0400

    test: add unit test for generic function with two type parameters

--- src/test/java/magma/GenericsTest.java
+++ src/test/java/magma/GenericsTest.java
@@ -6,11 +6,20 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 public class GenericsTest {
-
 	@Test
 	public void genericFnPassesValue() {
 		Result<String, InterpretError> r = new Interpreter().interpret(
 				"fn pass<T>(value : T) => value; pass(100)");
+		if (r instanceof Result.Err) {
+			fail("Interpreter returned error: " + ((Result.Err<String, InterpretError>) r).error());
+		}
+		assertEquals("100", ((Result.Ok<String, InterpretError>) r).value());
+	}
+
+	@Test
+	public void genericFnWithTwoTypeParamsReturnsFirst() {
+		Result<String, InterpretError> r = new Interpreter().interpret(
+				"fn pass2<T, U>(a : T, b : U) => a; pass2(100, true)");
 		if (r instanceof Result.Err) {
 			fail("Interpreter returned error: " + ((Result.Err<String, InterpretError>) r).error());
 		}
