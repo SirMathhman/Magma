d6614964971ca9dbacf0ccb836beadc0fa0b9acc
SirMathhman <sirmathhman@gmail.com>
2025-09-20 12:41:43 -0400

    refactor: remove main method and add execute method in Executor; update test to reflect changes

--- src/main/java/magma/Executor.java
+++ src/main/java/magma/Executor.java
@@ -1,11 +1,7 @@
 package magma;
 
 public class Executor {
-	public static void main(String[] args) {
-		System.out.println("Hello, Magma!");
-	}
-
-	public static int add(int a, int b) {
-		return a + b;
+	public static String execute(String input) {
+		return input;
 	}
 }
--- src/test/java/magma/ExecutorTest.java
+++ src/test/java/magma/ExecutorTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExecutorTest {
 	@Test
 	public void testAdd() {
-		assertEquals(5, Executor.add(2, 3));
+		assertEquals("hello", Executor.execute("hello"));
 	}
 }
