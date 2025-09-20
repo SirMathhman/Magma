1359ba8ffa5b7008ee93cdc5e52f2235806c5989
SirMathhman <sirmathhman@gmail.com>
2025-09-20 12:06:13 -0400

    Delete

--- src/main/java/magma/StatementContext.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-public class StatementContext {
-	public final java.util.Map<String, Long> env = new java.util.HashMap<>();
-	public final java.util.Map<String, Boolean> mut = new java.util.HashMap<>();
-	public final java.util.Map<String, String> types = new java.util.HashMap<>();
-	public final java.util.Map<String, String> refs = new java.util.HashMap<>();
-	public final java.util.Map<String, Boolean> refsMutable = new java.util.HashMap<>();
-}
