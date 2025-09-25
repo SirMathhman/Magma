9ae15d10d7465a05be65ff8c007269d69db9799f
SirMathhman <sirmathhman@gmail.com>
2025-09-24 21:43:35 -0400

    feat: remove App class and its associated tests

---
+++ src/main/java/magma/Compiler.java
@@ -0,0 +1,13 @@
+package magma;
+
+public class Compiler {
+	/**
+	 * For now, returns the input string unchanged.
+	 *
+	 * @param input the input string
+	 * @return the same input string
+	 */
+	public String compile(String input) {
+		return input;
+	}
+}
