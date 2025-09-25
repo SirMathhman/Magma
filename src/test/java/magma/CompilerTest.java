2e10fb92940b3c0fb9f7ad0d25db95621e6bba31
SirMathhman <sirmathhman@gmail.com>
2025-09-24 23:16:31 -0400

    feat: add test for Runner to validate output for input "5"

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -1,5 +1,30 @@
 package magma;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CompilerTest {
 
+	@Test
+	public void runnerReturnsOkTupleForFive() {
+		// Expect Runner.run("5", "") to compile/run and return Ok(("", 5))
+		var res = Runner.run("5", "");
+		switch (res) {
+			case Result.Ok<?, ?> ok -> {
+				// destructure the tuple using record deconstruction pattern
+				Object val = ok.value();
+				if (val instanceof Tuple(String left, Integer right)) {
+					assertEquals("", left);
+					assertEquals(5, right.intValue());
+				} else {
+					fail("Expected Tuple but was: " + (val == null ? "null" : val.getClass()));
+				}
+			}
+			case Result.Err<?, ?> err -> {
+				fail("Expected Ok but got Err: " + err.error());
+			}
+			default -> fail("Unexpected result type: " + res);
+		}
+	}
 }
