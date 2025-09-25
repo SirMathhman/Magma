0e6448d18978ec999edb0bae96d2de6c1f6cb0ea
SirMathhman <sirmathhman@gmail.com>
2025-09-24 23:17:17 -0400

    refactor: simplify result handling in CompilerTest for Runner output

--- src/test/java/magma/CompilerTest.java
+++ src/test/java/magma/CompilerTest.java
@@ -11,20 +11,12 @@ public class CompilerTest {
 		// Expect Runner.run("5", "") to compile/run and return Ok(("", 5))
 		var res = Runner.run("5", "");
 		switch (res) {
-			case Result.Ok<?, ?> ok -> {
-				// destructure the tuple using record deconstruction pattern
-				Object val = ok.value();
-				if (val instanceof Tuple(String left, Integer right)) {
-					assertEquals("", left);
-					assertEquals(5, right.intValue());
-				} else {
-					fail("Expected Tuple but was: " + (val == null ? "null" : val.getClass()));
-				}
+			case Result.Ok(var value) -> {
+				assertEquals(new Tuple<>("", 5), value);
 			}
-			case Result.Err<?, ?> err -> {
-				fail("Expected Ok but got Err: " + err.error());
+			case Result.Err(var error) -> {
+				fail(error);
 			}
-			default -> fail("Unexpected result type: " + res);
 		}
 	}
 }
