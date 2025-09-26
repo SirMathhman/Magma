fd6d8fc90612b15470effabbe17758018636f4be
SirMathhman <sirmathhman@gmail.com>
2025-09-26 15:43:15 -0400

    feat: update Main class to compile Java to TypeScript with new structure

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -8,10 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
-			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), input);
+			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
 		}
+	}
+
+	private static String compile(String input) {
+		return "/*" + replace(input) + "*/";
+	}
+
+	private static String replace(String input) {
+		return input.replace("/*", "start").replace("*/", "end");
 	}
 }
--- src/node/magma/Main.ts
+++ src/node/magma/Main.ts
@@ -1,4 +1,4 @@
-package magma;
+/*package magma;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,10 +8,19 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
-			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), input);
+			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
 		}
 	}
+
+	private static String compile(String input) {
+		return "start" + replace(input) + "end";
+	}
+
+	private static String replace(String input) {
+		return input.replace("start", "start").replace("end", "end");
+	}
 }
+*/
