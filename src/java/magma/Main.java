ae4c35cc3fac4d8a1c9f87097468fb19405bf991
SirMathhman <sirmathhman@gmail.com>
2025-09-26 15:46:46 -0400

    feat: enhance compile method to segment input and wrap with markers

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,8 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
@@ -16,10 +18,23 @@ public class Main {
 	}
 
 	private static String compile(String input) {
-		return "/*" + replace(input) + "*/";
+		final ArrayList<String> segments = new ArrayList<>();
+		StringBuilder buffer = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			final char c = input.charAt(i);
+			buffer.append(c);
+			if (c == ';') {
+				segments.add(buffer.toString());
+				buffer = new StringBuilder();
+			}
+		}
+		segments.add(buffer.toString());
+
+		return segments.stream().map(Main::wrap).collect(Collectors.joining());
 	}
 
-	private static String replace(String input) {
-		return input.replace("/*", "start").replace("*/", "end");
+	private static String wrap(String input) {
+		final String replaced = input.replace("/*", "start").replace("*/", "end");
+		return "/*" + replaced + "*/";
 	}
 }
--- src/node/magma/Main.ts
+++ src/node/magma/Main.ts
@@ -1,26 +1,41 @@
-/*package magma;
+/*package magma;*//*
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;*//*
+import java.nio.file.Files;*//*
+import java.nio.file.Paths;*//*
+import java.util.ArrayList;*//*
+import java.util.stream.Collectors;*//*
 
 public class Main {
 	public static void main(String[] args) {
 		try {
-			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));
-			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));
+			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));*//*
+			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));*//*
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();
+			e.printStackTrace();*//*
 		}
 	}
 
 	private static String compile(String input) {
-		return "start" + replace(input) + "end";
+		final ArrayList<String> segments = new ArrayList<String>();*//*
+		StringBuilder buffer = new StringBuilder();*//*
+		for (int i = 0;*//* i < input.length();*//* i++) {
+			final char c = input.charAt(i);*//*
+			buffer.append(c);*//*
+			if (c == ';*//*') {
+				segments.add(buffer.toString());*//*
+				buffer = new StringBuilder();*//*
+			}
+		}
+		segments.add(buffer.toString());*//*
+
+		return segments.stream().map(Main::wrap).collect(Collectors.joining());*//*
 	}
 
-	private static String replace(String input) {
-		return input.replace("start", "start").replace("end", "end");
+	private static String wrap(String input) {
+		final String replaced = input.replace("start", "start").replace("end", "end");*//*
+		return "start" + replaced + "end";*//*
 	}
 }
 */
