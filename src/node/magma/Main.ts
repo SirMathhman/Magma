eac526a8a0878b8e2423afbda285459953920da0
SirMathhman <sirmathhman@gmail.com>
2025-09-26 15:47:28 -0400

    feat: refine compile method to filter segments and strip whitespace

--- src/node/magma/Main.ts
+++ src/node/magma/Main.ts
@@ -1,41 +1,22 @@
-/*package magma;*//*
-
-import java.io.IOException;*//*
-import java.nio.file.Files;*//*
-import java.nio.file.Paths;*//*
-import java.util.ArrayList;*//*
-import java.util.stream.Collectors;*//*
-
-public class Main {
+/*public class Main {
 	public static void main(String[] args) {
 		try {
-			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));*//*
-			Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));*//*
-		} catch (IOException e) {
+			final String input = Files.readString(Paths.get(".", "src", "java", "magma", "Main.java"));*//*Files.writeString(Paths.get(".", "src", "node", "magma", "Main.ts"), compile(input));*//*} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();*//*
-		}
+			e.printStackTrace();*//*}
 	}
 
 	private static String compile(String input) {
-		final ArrayList<String> segments = new ArrayList<String>();*//*
-		StringBuilder buffer = new StringBuilder();*//*
-		for (int i = 0;*//* i < input.length();*//* i++) {
-			final char c = input.charAt(i);*//*
-			buffer.append(c);*//*
-			if (c == ';*//*') {
-				segments.add(buffer.toString());*//*
-				buffer = new StringBuilder();*//*
-			}
+		final ArrayList<String> segments = new ArrayList<>();*//*StringBuilder buffer = new StringBuilder();*//*for (int i = 0;*//*i < input.length();*//*i++) {
+			final char c = input.charAt(i);*//*buffer.append(c);*//*if (c == ';*//*') {
+				segments.add(buffer.toString());*//*buffer = new StringBuilder();*//*}
 		}
-		segments.add(buffer.toString());*//*
-
-		return segments.stream().map(Main::wrap).collect(Collectors.joining());*//*
-	}
+		segments.add(buffer.toString());*//*return segments.stream()
+									 .map(String::strip)
+									 .filter(segment -> !segment.startsWith("package ") && !segment.startsWith("import "))
+									 .map(Main::wrap)
+									 .collect(Collectors.joining());*//*}
 
 	private static String wrap(String input) {
-		final String replaced = input.replace("start", "start").replace("end", "end");*//*
-		return "start" + replaced + "end";*//*
-	}
-}
-*/
+		final String replaced = input.replace("start", "start").replace("end", "end");*//*return "start" + replaced + "end";*//*}
+}*/
