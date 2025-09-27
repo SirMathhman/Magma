d13fc92e5bf8ad4de4a8e08eb09a47300a6b6082
SirMathhman <sirmathhman@gmail.com>
2025-09-27 17:45:00 -0400

    feat: refactor file writing logic to use compile function for improved processing

--- index.ts
+++ index.ts
@@ -6,10 +6,26 @@ await main();
 async function main() {
 	const inputBuffer = await fs.readFile(path.join(".", "index.ts"));
 	const input = inputBuffer.toString();
-	await fs.writeFile(path.join(".", "main.c"), wrap(input));
+	await fs.writeFile(path.join(".", "main.c"), compile(input));
+}
+
+function compile(input: string): string {
+	const segments: string[] = [];
+	let buffer: string[] = [];
+	for (let index = 0; index < input.length; index++) {
+		const c = input[index];
+		if (!c) break;
+
+		buffer.push(c);
+		if (c[0] == ';') {
+			segments.push(buffer.join(""));
+			buffer = [];
+		}
+	}
+
+	return segments.map(wrap).join("");
 }
 
 function wrap(input: string): string {
 	return "/*" + input.replaceAll("/*", "start").replaceAll("*/", "end") + "*/";
 }
-
--- main.c
+++ main.c
@@ -1,16 +1,30 @@
-/*import * as path from "path";
-import * as fs from "fs/promises";
+/*import * as path from "path";*//*
+import * as fs from "fs/promises";*//*
 
-await main();
+await main();*//*
 
 async function main() {
-	const inputBuffer = await fs.readFile(path.join(".", "index.ts"));
-	const input = inputBuffer.toString();
-	await fs.writeFile(path.join(".", "main.c"), wrap(input));
+	const inputBuffer = await fs.readFile(path.join(".", "index.ts"));*//*
+	const input = inputBuffer.toString();*//*
+	await fs.writeFile(path.join(".", "main.c"), compile(input));*//*
+}
+
+function compile(input: string): string {
+	const segments: string[] = [];*//*
+	let buffer = "";*//*
+	for (let index = 0;*//* index < input.length;*//* index++) {
+		const c = input[index];*//*
+		if (!c) break;*//*
+
+		buffer += c;*//*
+		if (c[0] == ';*//*') {
+			segments.push(buffer);*//*
+			buffer = "";*//*
+		}
+	}
+
+	return segments.map(wrap).join("");*//*
 }
 
 function wrap(input: string): string {
-	return "start" + input.replaceAll("start", "start").replaceAll("end", "end") + "end";
-}
-
-*/
+	return "start" + input.replaceAll("start", "start").replaceAll("end", "end") + "end";*/
