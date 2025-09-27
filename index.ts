4df61a8e4d83e0fa4ff9ca0f70dbb3234ba10c36
SirMathhman <sirmathhman@gmail.com>
2025-09-27 17:41:33 -0400

    feat: refactor main function and wrap input for improved file handling

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
 import * as path from "path";
 import * as fs from "fs/promises";
 
-const input = await fs.readFile(path.join(".", "index.ts"));
-await fs.writeFile(path.join(".", "main.c"), input);
+await main();
+
+async function main() {
+	const inputBuffer = await fs.readFile(path.join(".", "index.ts"));
+	const input = inputBuffer.toString();
+	await fs.writeFile(path.join(".", "main.c"), wrap(input));
+}
+
+function wrap(input: string): string {
+	return "/*" + input.replaceAll("/*", "start").replaceAll("*/", "end") + "*/";
+}
+
--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-import * as path from "path";
+/*import * as path from "path";
 import * as fs from "fs/promises";
 
-const input = await fs.readFile(path.join(".", "index.ts"));
-await fs.writeFile(path.join(".", "main.c"), input);
+await main();
+
+async function main() {
+	const inputBuffer = await fs.readFile(path.join(".", "index.ts"));
+	const input = inputBuffer.toString();
+	await fs.writeFile(path.join(".", "main.c"), wrap(input));
+}
+
+function wrap(input: string): string {
+	return "start" + input.replaceAll("start", "start").replaceAll("end", "end") + "end";
+}
+
+*/
