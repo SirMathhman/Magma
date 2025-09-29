1099da3b9b339c5983c9e3d90d61f1746ef38286
SirMathhman <sirmathhman@gmail.com>
2025-09-28 21:56:05 -0400

    style: format code for improved readability and consistency

--- magma_compiler/src/ast.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, PartialEq, Clone)]
-pub enum Node {
-    Number(i64),
-    Identifier(String),
-}
--- magma_compiler/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod lexer;
-pub mod ast;
-
-pub use lexer::{Token, Lexer};
-pub use ast::Node;
