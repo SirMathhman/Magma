054b4985ede174e42fb3dc79e603386ecf7402d2
SirMathhman <sirmathhman@gmail.com>
2025-09-29 17:49:58 -0400

    feat: initialize project with basic structure and main functionality

---
+++ src/main/java/magma/Main.java
@@ -0,0 +1,20 @@
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			final Path source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
+			final String input = Files.readString(source);
+			Files.writeString(source.resolveSibling("main.c"),
+												"/*" + input.replace("/*", "start").replace("*/", "end") + "*/");
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
+	}
+}
