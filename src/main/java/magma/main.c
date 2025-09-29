72fc6bebeb878d40243154b8677cd7e75670ac09
SirMathhman <sirmathhman@gmail.com>
2025-09-29 17:56:27 -0400

    feat: implement compile and wrap methods for processing input in Main.java

--- src/main/java/magma/main.c
+++ src/main/java/magma/main.c
@@ -1,21 +1,42 @@
-/*package magma;
+/*package magma;*//*
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;*//*
+import java.nio.file.Files;*//*
+import java.nio.file.Path;*//*
+import java.nio.file.Paths;*//*
+import java.util.ArrayList;*//*
+import java.util.stream.Collectors;*//*
 
 public class Main {
 	public static void main(String[] args) {
 		try {
-			final Path source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
-			final String input = Files.readString(source);
-			Files.writeString(source.resolveSibling("main.c"),
-												"start" + input.replace("start", "start").replace("end", "end") + "end");
+			final Path source = Paths.get(".", "src", "main", "java", "magma", "Main.java");*//*
+			final String input = Files.readString(source);*//*
+			Files.writeString(source.resolveSibling("main.c"), compile(input));*//*
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();
+			e.printStackTrace();*//*
 		}
+	}
+
+	private static String compile(String input) {
+		final ArrayList<String> segments = new ArrayList<String>();*//*
+		StringBuilder buffer = new StringBuilder();*//*
+		for (int i = 0;*//* i < input.length();*//* i++) {
+			final char c = input.charAt(i);*//*
+			buffer.append(c);*//*
+			if (c == ';*//*') {
+				segments.add(buffer.toString());*//*
+				buffer = new StringBuilder();*//*
+			}
+		}
+		segments.add(buffer.toString());*//*
+
+		return segments.stream().map(Main::wrap).collect(Collectors.joining());*//*
+	}
+
+	private static String wrap(String input) {
+		return "start" + input.replace("start", "start").replace("end", "end") + "end";*//*
 	}
 }
 */
