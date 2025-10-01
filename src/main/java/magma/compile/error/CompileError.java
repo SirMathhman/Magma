e7af25f6d5cd87ac3a60fd89c60d22d179c6fcee
SirMathhman <sirmathhman@gmail.com>
2025-09-30 20:51:48 -0400

    feat: enhance CompileError handling by sorting causes and adding depth calculation

--- src/main/java/magma/compile/error/CompileError.java
+++ src/main/java/magma/compile/error/CompileError.java
@@ -2,7 +2,9 @@ package magma.compile.error;
 
 import magma.compile.context.Context;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public record CompileError(String reason, Context context, List<CompileError> causes) implements Error {
@@ -16,9 +18,11 @@ public record CompileError(String reason, Context context, List<CompileError> ca
 	}
 
 	private String format(int depth, int index) {
-		StringBuilder joiner = new StringBuilder();
-		for (int i = 0; i < causes.size(); i++) {
-			CompileError error = causes.get(i);
+		final ArrayList<CompileError> copy = new ArrayList<>(causes);
+		copy.sort(Comparator.comparingInt(CompileError::depth));
+
+		StringBuilder joiner = new StringBuilder(); for (int i = 0; i < copy.size(); i++) {
+			CompileError error = copy.get(i);
 			String format = error.format(depth + 1, i);
 			joiner.append(format);
 		}
@@ -26,5 +30,9 @@ public record CompileError(String reason, Context context, List<CompileError> ca
 		final String formattedChildren = joiner.toString();
 		final String s = depth == 0 ? "" : System.lineSeparator() + "\t".repeat(depth);
 		return s + index + ") " + reason + ": " + context.display(depth) + formattedChildren;
+	}
+
+	private int depth() {
+		return 1 + causes.stream().mapToInt(CompileError::depth).max().orElse(0);
 	}
 }
