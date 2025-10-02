b405ee4f103aa5e819e6f0e0560f1f566aaa72b0
SirMathhman <sirmathhman@gmail.com>
2025-10-02 18:19:33 -0400

    Fix if bug

--- src/main/java/magma/compile/rule/ClosingParenthesesFolder.java
+++ src/main/java/magma/compile/rule/ClosingParenthesesFolder.java
@@ -3,14 +3,13 @@ package magma.compile.rule;
 public class ClosingParenthesesFolder implements Folder {
 	@Override
 	public DivideState fold(DivideState state, char c) {
-		final DivideState appended = state.append(c);
-		if (c == '(') return appended.enter();
+		if (c == '(') return state.append(c).enter();
 		if (c == ')') {
-			final DivideState exit = appended.exit();
+			final DivideState exit = state.exit();
 			if (exit.isLevel()) return exit.advance();
-			return exit;
+			return exit.append(c);
 		}
-		return appended;
+		return state.append(c);
 	}
 
 	@Override
