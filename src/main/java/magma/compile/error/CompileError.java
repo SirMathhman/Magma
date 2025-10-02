b646c73804c58574a8a8ea3929d04e7024852252
SirMathhman <sirmathhman@gmail.com>
2025-10-02 18:15:41 -0400

    Remove ternary

--- src/main/java/magma/compile/error/CompileError.java
+++ src/main/java/magma/compile/error/CompileError.java
@@ -23,14 +23,16 @@ public record CompileError(String reason, Context context, List<CompileError> ca
 		final ArrayList<CompileError> copy = new ArrayList<>(causes);
 		copy.sort(Comparator.comparingInt(CompileError::depth));
 		final String formattedChildren = joinErrors(depth, copy);
-		final String s = depth == 0 ? "" : System.lineSeparator() + "\t".repeat(depth);
+		final String s;
+		if (depth == 0) s = "";
+		else s = System.lineSeparator() + "\t".repeat(depth);
 		return s + index + ") " + reason + ": " + context.display(depth) + formattedChildren;
 	}
 
 	private String joinErrors(int depth, List<CompileError> copy) {
 		return IntStream.range(0, copy.size())
-										.mapToObj(index -> formatChild(depth, copy, index))
-										.collect(Collectors.joining());
+				.mapToObj(index -> formatChild(depth, copy, index))
+				.collect(Collectors.joining());
 	}
 
 	private String formatChild(int depth, List<CompileError> copy, int i) {
