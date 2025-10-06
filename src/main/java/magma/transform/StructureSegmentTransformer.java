7b730f00ee2686e8bb2553259761a08c684946a9
SirMathhman <sirmathhman@gmail.com>
2025-10-06 12:28:14 -0400

    Hmmm

---
+++ src/main/java/magma/transform/StructureSegmentTransformer.java
@@ -0,0 +1,30 @@
+package magma.transform;
+
+import magma.Tuple;
+import magma.compile.Lang;
+import magma.option.None;
+import magma.option.Option;
+import magma.option.Some;
+
+import java.util.Collections;
+import java.util.List;
+
+public class StructureSegmentTransformer {
+	static Tuple<List<Lang.CRootSegment>, Option<Lang.CDefinition>> flattenStructureSegment(Lang.JStructureSegment self,
+																																													String name) {
+		return switch (self) {
+			case Lang.Invalid invalid -> new Tuple<>(List.of(invalid), new None<>());
+			case Lang.Method method -> new Tuple<>(List.of(Transformer.transformMethod(method, name)), new None<>());
+			case Lang.JStructure jClass -> new Tuple<>(StructureTransformer.flattenStructure(jClass), new None<>());
+			case Lang.Field field ->
+					new Tuple<>(Collections.emptyList(), new Some<>(Transformer.transformDefinition(field.value())));
+			case Lang.Whitespace _, Lang.LineComment _, Lang.BlockComment _ ->
+					new Tuple<>(Collections.emptyList(), new None<>());
+			case Lang.JInitialization jInitialization -> new Tuple<>(Collections.emptyList(),
+																															 new Some<>(Transformer.transformDefinition(
+																																	 jInitialization.definition())));
+			case Lang.JDefinition jDefinition ->
+					new Tuple<>(Collections.emptyList(), new Some<>(Transformer.transformDefinition(jDefinition)));
+		};
+	}
+}
--- src/main/java/magma/transform/StructureTransformer.java
+++ src/main/java/magma/transform/StructureTransformer.java
@@ -1,13 +1,10 @@
 package magma.transform;
 
-import magma.Tuple;
 import magma.compile.Lang;
-import magma.option.None;
 import magma.option.Option;
 import magma.option.Some;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class StructureTransformer {
@@ -21,7 +18,7 @@ public class StructureTransformer {
 		addRecordParamsAsFields(aClass, fields);
 
 		final String name = aClass.name();
-		children.stream().map(child -> flattenStructureSegment(child, name)).forEach(tuple -> {
+		children.stream().map(child -> StructureSegmentTransformer.flattenStructureSegment(child, name)).forEach(tuple -> {
 			segments.addAll(tuple.left());
 			if (tuple.right() instanceof Some<Lang.CDefinition>(Lang.CDefinition value)) fields.add(value);
 		});
@@ -40,23 +37,5 @@ public class StructureTransformer {
 			if (params instanceof Some<List<Lang.JDefinition>>(List<Lang.JDefinition> paramList))
 				paramList.stream().map(Transformer::transformDefinition).forEach(fields::add);
 		}
-	}
-
-	private static Tuple<List<Lang.CRootSegment>, Option<Lang.CDefinition>> flattenStructureSegment(Lang.JStructureSegment self,
-																																																	String name) {
-		return switch (self) {
-			case Lang.Invalid invalid -> new Tuple<>(List.of(invalid), new None<>());
-			case Lang.Method method -> new Tuple<>(List.of(Transformer.transformMethod(method, name)), new None<>());
-			case Lang.JStructure jClass -> new Tuple<>(flattenStructure(jClass), new None<>());
-			case Lang.Field field ->
-					new Tuple<>(Collections.emptyList(), new Some<>(Transformer.transformDefinition(field.value())));
-			case Lang.Whitespace _, Lang.LineComment _, Lang.BlockComment _ ->
-					new Tuple<>(Collections.emptyList(), new None<>());
-			case Lang.JInitialization jInitialization -> new Tuple<>(Collections.emptyList(),
-																															 new Some<>(Transformer.transformDefinition(
-																																	 jInitialization.definition())));
-			case Lang.JDefinition jDefinition ->
-					new Tuple<>(Collections.emptyList(), new Some<>(Transformer.transformDefinition(jDefinition)));
-		};
 	}
 }
