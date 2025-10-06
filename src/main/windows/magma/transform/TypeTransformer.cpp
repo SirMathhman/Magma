46d1983b0ca92e50e393ae532e8f27486d96fe99
SirMathhman <sirmathhman@gmail.com>
2025-10-06 14:08:20 -0400

    Rebuild

--- src/main/windows/magma/transform/TypeTransformer.cpp
+++ src/main/windows/magma/transform/TypeTransformer.cpp
@@ -3,3 +3,20 @@ struct TypeTransformer {};
 /*???*/ transformType_TypeTransformer(/*???*/ type) {
 	return /*???*/;
 }
+/*???*/ transformIdentifier_TypeTransformer(/*???*/ identifier) {
+	if (identifier.value().equals(""))return new_???(new_???(""));
+	return identifier;
+}
+/*???*/ transformArray_TypeTransformer(/*???*/ array) {
+	/*???*/ childType=transformType(array.child());
+	return new_???(childType);
+}
+/*???*/ transformGeneric_TypeTransformer(/*???*/ generic) {
+	List<> listOption=generic.typeArguments().orElse(new_???());
+	if (generic.base().equals("")&&listOption.size()==/*???*/)
+	{
+	/*???*/ paramType=transformType(listOption.get(/*???*/));
+	/*???*/ returnType=transformType(listOption.get(/*???*/));
+	return new_???(returnType, List.of(paramType));}
+	return new_???(generic.base(), listOption.stream().map(/*???*/).toList());
+}
