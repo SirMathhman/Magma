73034bab6f7c90d3b76e6a0f094e34c275c22be6
SirMathhman <sirmathhman@gmail.com>
2025-10-06 16:16:38 -0400

    OK

--- src/main/java/magma/list/HeadedStream.java
+++ src/main/java/magma/list/HeadedStream.java
@@ -10,6 +10,40 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public record HeadedStream<T>(Head<T> head) implements Stream<T> {
+	private class FlatMapHead<R> implements Head<R> {
+		private final Function<T, Stream<R>> mapper;
+		private Head<R> currentInnerHead;
+
+		public FlatMapHead(Function<T, Stream<R>> mapper) {
+			this.mapper = mapper;
+			currentInnerHead = null;
+		}
+
+		@Override
+		public Option<R> next() {
+			while (true) {
+				// If we have a current inner stream, try to get the next element from it
+				if (currentInnerHead != null) {
+					final Option<R> innerNext = currentInnerHead.next();
+					if (innerNext instanceof Some<R>(R value)) return innerNext;
+					// Current inner stream is exhausted, move to next outer element
+					currentInnerHead = null;
+				}
+
+				// Get the next element from the outer stream
+				final Option<T> outerNext = head.next();
+				// Outer stream is exhausted
+				if (outerNext instanceof Some<T>(T value)) {
+					// Map to inner stream and set it as current
+					final Stream<R> innerStream = mapper.apply(value);
+					if (innerStream instanceof HeadedStream<R>(Head<R> head1)) currentInnerHead = head1;
+					else throw new IllegalStateException("flatMap expects HeadedStream");
+					// Continue loop to get first element from this inner stream
+				} else return new None<R>();
+			}
+		}
+	}
+
 	@Override
 	public <R> Stream<R> map(Function<T, R> mapper) {
 		return new HeadedStream<R>(() -> head.next().map(mapper));
@@ -42,38 +76,7 @@ public record HeadedStream<T>(Head<T> head) implements Stream<T> {
 
 	@Override
 	public <R> Stream<R> flatMap(Function<T, Stream<R>> mapper) {
-		return new HeadedStream<>(new Head<R>() {
-			private Head<R> currentInnerHead = null;
-
-			@Override
-			public Option<R> next() {
-				while (true) {
-					// If we have a current inner stream, try to get the next element from it
-					if (currentInnerHead != null) {
-						final Option<R> innerNext = currentInnerHead.next();
-						if (innerNext instanceof Some<R>(R value)) return innerNext;
-						// Current inner stream is exhausted, move to next outer element
-						currentInnerHead = null;
-					}
-
-					// Get the next element from the outer stream
-					final Option<T> outerNext = head.next();
-					if (outerNext instanceof Some<T>(T value)) {
-						// Map to inner stream and set it as current
-						final Stream<R> innerStream = mapper.apply(value);
-						if (innerStream instanceof HeadedStream<R> headedStream) {
-							currentInnerHead = headedStream.head();
-						} else {
-							throw new IllegalStateException("flatMap expects HeadedStream");
-						}
-						// Continue loop to get first element from this inner stream
-					} else {
-						// Outer stream is exhausted
-						return new None<>();
-					}
-				}
-			}
-		});
+		return new HeadedStream<R>(new FlatMapHead<R>(mapper));
 	}
 
 	@Override
