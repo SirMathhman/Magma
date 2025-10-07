d358dbb3a42391f3ab20aab0b1ce34f5accd79c6
SirMathhman <sirmathhman@gmail.com>
2025-10-07 13:54:22 -0400

    Remove String from Serialization

---
+++ src/main/windows/magma/compile/rule/RootSlice.cpp
@@ -0,0 +1,50 @@
+// Generated transpiled C++ from 'src\main\java\magma\compile\rule\RootSlice.java'. This file shouldn't be edited, and rather the compiler implementation should be changed.
+struct RootSlice {};
+boolean isEmpty_RootSlice() {
+	return value.isEmpty();
+}
+boolean startsWith_RootSlice() {
+	return value.startsWith();
+}
+Slice substring_RootSlice() {
+	return new_???();
+}
+Slice strip_RootSlice() {
+	return new_???();
+}
+boolean endsWith_RootSlice() {
+	return value.endsWith();
+}
+int length_RootSlice() {
+	return value.length();
+}
+Slice substring_RootSlice() {
+	return new_???();
+}
+Option<Integer> indexOf_RootSlice() {
+	int index=value.indexOf();
+	if (index==/*???*/)
+	{
+	return new_???();}
+	return new_???();
+}
+List<Slice> split_RootSlice() {
+	return new_???();
+}
+Slice copy_RootSlice() {
+	return new_???();
+}
+Option<Character> charAt_RootSlice() {
+	if (/*???*/)
+	{
+	return new_???();}
+	else
+	return new_???();
+}
+Option<Integer> lastIndexOf_RootSlice() {
+	int index=value.lastIndexOf();
+	if (index==/*???*/)
+	{
+	return new_???();}
+	return new_???();
+}
