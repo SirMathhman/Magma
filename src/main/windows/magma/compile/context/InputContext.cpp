b0803cb454b5d6ed1f86a8d7e46f5425c5e5a552
SirMathhman <sirmathhman@gmail.com>
2025-10-07 13:44:16 -0400

    More migration

---
+++ src/main/windows/magma/compile/context/InputContext.cpp
@@ -0,0 +1,5 @@
+// Generated transpiled C++ from 'src\main\java\magma\compile\context\InputContext.java'. This file shouldn't be edited, and rather the compiler implementation should be changed.
+struct InputContext {};
+String display_InputContext() {
+	return context.value();
+}
---
+++ src/main/windows/magma/compile/rule/Slice.cpp
@@ -0,0 +1,26 @@
+// Generated transpiled C++ from 'src\main\java\magma\compile\rule\Slice.java'. This file shouldn't be edited, and rather the compiler implementation should be changed.
+struct Slice {};
+boolean isEmpty_Slice() {/*???*/
+}
+boolean startsWith_Slice() {/*???*/
+}
+Slice substring_Slice() {/*???*/
+}
+Slice strip_Slice() {/*???*/
+}
+boolean endsWith_Slice() {/*???*/
+}
+int length_Slice() {/*???*/
+}
+Slice substring_Slice() {/*???*/
+}
+Option<Integer> indexOf_Slice() {/*???*/
+}
+List<Slice> split_Slice() {/*???*/
+}
+Option<Character> charAt_Slice() {/*???*/
+}
+Option<Integer> lastIndexOf_Slice() {/*???*/
+}
+String value_Slice() {/*???*/
+}
