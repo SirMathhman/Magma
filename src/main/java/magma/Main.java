971e414f82bc1dc30b875436becd1aa86c651716
SirMathhman <sirmathhman@gmail.com>
2025-10-16 22:32:27 -0400

    Reformat

--- src/main/java/magma/Main.java
+++ src/main/java/magma/Main.java
@@ -8,13 +8,24 @@ import java.nio.file.Paths;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			final Path source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
+			final String input = Files.readString(source);
+
 			final Path target = Paths.get(".", "src", "main", "windows", "magma", "Main.c");
 			final Path targetParent = target.getParent();
+
 			if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
-			Files.writeString(target, "int main(){" + System.lineSeparator() + "\treturn 0;" + System.lineSeparator() + "}");
+			Files.writeString(target,
+												wrap(input) + "int main(){" + System.lineSeparator() + "\treturn 0;" + System.lineSeparator() +
+												"}");
 		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
 		}
+	}
+
+	private static String wrap(String input) {
+		final String replaced = input.replace("/*", "start").replace("*/", "end");
+		return "/*" + replaced + "*/";
 	}
 }
--- src/main/windows/magma/Main.c
+++ src/main/windows/magma/Main.c
@@ -1,3 +1,34 @@
-int main(){
+/*package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			final Path source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
+			final String input = Files.readString(source);
+
+			final Path target = Paths.get(".", "src", "main", "windows", "magma", "Main.c");
+			final Path targetParent = target.getParent();
+
+			if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
+			Files.writeString(target,
+												wrap(input) + "int main(){" + System.lineSeparator() + "\treturn 0;" + System.lineSeparator() +
+												"}");
+		} catch (IOException e) {
+			//noinspection CallToPrintStackTrace
+			e.printStackTrace();
+		}
+	}
+
+	private static String wrap(String input) {
+		final String replaced = input.replace("start", "start").replace("end", "end");
+		return "start" + replaced + "end";
+	}
+}
+*/int main(){
 	return 0;
 }
