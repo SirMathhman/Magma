1e50b2dcfc7f30c618bd9d053cd9c09a59117034
SirMathhman <sirmathhman@gmail.com>
2025-11-19 17:43:31 -0500

    Add Result.and and Tuple record with tests

--- src/main/java/magma/Result.java
+++ src/main/java/magma/Result.java
@@ -4,6 +4,7 @@ import magma.Result.Err;
 import magma.Result.Ok;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Generic sealed result type with success and error variants.
@@ -12,15 +13,20 @@ import java.util.function.Function;
  * @param <X> error type
  */
 public sealed interface Result<T, X> permits Ok, Err {
+	record Tuple<A, B>(A first, B second) {
+	}
+
 	/**
 	 * Success variant wrapping a value of type T.
 	 */
-	record Ok<T, X>(T value) implements Result<T, X> {}
+	record Ok<T, X>(T value) implements Result<T, X> {
+	}
 
 	/**
 	 * Error variant wrapping an error value of type X.
 	 */
-	record Err<T, X>(X error) implements Result<T, X> {}
+	record Err<T, X>(X error) implements Result<T, X> {
+	}
 
 	@SuppressWarnings("unchecked")
 	default <U> Result<U, X> map(Function<? super T, ? extends U> mapper) {
@@ -36,5 +42,17 @@ public sealed interface Result<T, X> permits Ok, Err {
 			return mapper.apply(value);
 		}
 		return (Result<U, X>) this;
+	}
+
+	@SuppressWarnings("unchecked")
+	default <R> Result<Tuple<T, R>, X> and(Supplier<Result<R, X>> other) {
+		if (this instanceof Ok<T, X>(T value)) {
+			var rRes = other.get();
+			if (rRes instanceof Ok<R, X>(R r)) {
+				return new Ok<Tuple<T, R>, X>(new Tuple<>(value, r));
+			}
+			return (Result<Tuple<T, R>, X>) rRes; // Err<R, X> to Err<Tuple<T, R>, X>
+		}
+		return (Result<Tuple<T, R>, X>) this; // Err<T, X> to Err<Tuple<T, R>, X>
 	}
 }
---
+++ src/test/java/magma/ResultTest.java
@@ -0,0 +1,41 @@
+package magma;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultTest {
+
+	@Test
+	void andCombinesTwoOkValues() {
+		Result.Ok<Integer, String> left = new Result.Ok<>(1);
+		Supplier<Result<Integer, String>> rightSupplier = () -> new Result.Ok<>(2);
+		var res = left.and(rightSupplier);
+		assertTrue(res instanceof Result.Ok);
+		var ok = (Result.Ok<Result.Tuple<Integer, Integer>, String>) res;
+		assertEquals(1, ok.value().first());
+		assertEquals(2, ok.value().second());
+	}
+
+	@Test
+	void andPropagatesLeftErr() {
+		Result.Err<Integer, String> left = new Result.Err<>("left-err");
+		Supplier<Result<Integer, String>> rightSupplier = () -> new Result.Ok<>(2);
+		var res = left.and(rightSupplier);
+		assertTrue(res instanceof Result.Err);
+		var err = (Result.Err<Result.Tuple<Integer, Integer>, String>) res;
+		assertEquals("left-err", err.error());
+	}
+
+	@Test
+	void andPropagatesRightErr() {
+		Result.Ok<Integer, String> left = new Result.Ok<>(1);
+		Supplier<Result<Integer, String>> rightSupplier = () -> new Result.Err<>("right-err");
+		var res = left.and(rightSupplier);
+		assertTrue(res instanceof Result.Err);
+		var err = (Result.Err<Result.Tuple<Integer, Integer>, String>) res;
+		assertEquals("right-err", err.error());
+	}
+}
