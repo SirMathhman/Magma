32e461813ead32e1e61a6d4fb8a72e78bcf0de69
SirMathhman <sirmathhman@gmail.com>
2025-11-18 21:18:59 -0500

    Add test case for Main.interpret method

--- src/test/java/com/magma/MainTest.java
+++ src/test/java/com/magma/MainTest.java
@@ -1,12 +1,18 @@
 package com.magma;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
     @Test
     void testMain() {
         assertTrue(true, "Basic test to verify test setup");
+    }
+
+    @Test
+    void testInterpret() {
+        assertEquals("5", Main.interpret("5"));
     }
 }
 
