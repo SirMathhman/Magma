5de71dc65f02cb6d27f8da303cd70884d6f9033a
SirMathhman <sirmathhman@gmail.com>
2025-11-19 15:59:17 -0500

    Refactor tests: add helper with switch to assert Result.Ok

--- src/test/java/magma/AppTest.java
+++ src/test/java/magma/AppTest.java
@@ -2,24 +2,25 @@ package magma;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
 	@Test
 	void interpretReturnsSameString() {
-		String sample = "100";
-		var res = App.interpret(sample);
-		assertTrue(res instanceof Result.Ok);
-		assertEquals(sample, ((Result.Ok<String, String>) res).value());
+		assertInterpretsTo("100", "100");
 	}
 
 	@Test
 	void interpretExtractsLeadingDigits() {
-		String sample = "100U8";
-		var res = App.interpret(sample);
-		assertTrue(res instanceof Result.Ok);
-		assertEquals("100", ((Result.Ok<String, String>) res).value());
+		assertInterpretsTo("100U8", "100");
+	}
+
+	private static void assertInterpretsTo(String input, String expected) {
+		var res = App.interpret(input);
+		switch (res) {
+			case Result.Ok<?, ?> ok -> assertEquals(expected, ok.value());
+			case Result.Err<?, ?> err -> fail("Expected Ok but got Err: " + err.error());
+		}
 	}
 }
