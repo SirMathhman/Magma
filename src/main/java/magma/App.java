c87fd1ce254b0857ccaf226b450c9c62b53c585e
SirMathhman <sirmathhman@gmail.com>
2025-11-19 19:39:53 -0500

    chore(java): finalize package change to magma

---
+++ src/main/java/magma/App.java
@@ -0,0 +1,11 @@
+package magma;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
