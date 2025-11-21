60c6b1fdde59000b1b77e1c88a634ec046379ad1
SirMathhman <sirmathhman@gmail.com>
2025-11-21 12:35:33 -0500

    Refactor compile logic to handle nested blocks in Main.java and Main.c

--- src/main/java/magma/Main.c
+++ src/main/java/magma/Main.c
@@ -1,24 +1,52 @@
 /*public class Main {
 	public static void main(String[] args) {
 		try {
-			final var source = Paths.get(".", "src", "main", "java", "magma", "Main.java");*//*final var input = Files.readString(source);*//*final var target = source.resolveSibling("Main.c");*//*Files.writeString(target, compile(input));*//*} catch (IOException e) {
+			final var source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
+			final var input = Files.readString(source);
+			final var target = source.resolveSibling("Main.c");
+			Files.writeString(target, compile(input));
+		} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();*//*}
+			e.printStackTrace();
+		}
 	}
 
 	private static String compile(String input) {
-		final var segments = new ArrayList<String>();*//*var buffer = new StringBuilder();*//*for (var i = 0;*//*i < input.length();*//*i++) {
-			final var c = input.charAt(i);*//*buffer.append(c);*//*if (c == ';*//*') {
-				segments.add(buffer.toString());*//*buffer = new StringBuilder();*//*}
+		final var segments = new ArrayList<String>();
+		var buffer = new StringBuilder();
+		var depth = 0;
+
+		for (var i = 0; i < input.length(); i++) {
+			final var c = input.charAt(i);
+			buffer.append(c);
+			if (c == ';' && depth == 0) {
+				segments.add(buffer.toString());
+				buffer = new StringBuilder();
+			} else {
+				if (c == '{') {
+					depth++;
+				}
+				if (c == '}') {
+					depth--;
+				}
+			}
 		}
-		segments.add(buffer.toString());*//*return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());*//*}
+		segments.add(buffer.toString());
+
+		return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());
+	}
 
 	private static String compileRootSegment(String input) {
-		final var stripped = input.strip();*//*if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
-			return "";*//*}
+		final var stripped = input.strip();
+		if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
+			return "";
+		}
 
-		return wrap(stripped);*//*}
+		return wrap(stripped);
+	}
 
 	private static String wrap(String input) {
-		final var replaced = input.replace("start", "start").replace("end", "end");*//*return "start" + replaced + "end";*//*}
+		final var replaced = input.replace("start", "start").replace("end", "end");
+		return "start" + replaced + "end";
+	}
 }*/
