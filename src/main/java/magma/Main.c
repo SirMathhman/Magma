836873f8b0f906b807c15a29c4e24efa00148cfa
SirMathhman <sirmathhman@gmail.com>
2025-11-21 12:35:01 -0500

    Remove package and import statements

--- src/main/java/magma/Main.c
+++ src/main/java/magma/Main.c
@@ -1,25 +1,24 @@
-/*package magma;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-public class Main {
+/*public class Main {
 	public static void main(String[] args) {
 		try {
-			final var source = Paths.get(".", "src", "main", "java", "magma", "Main.java");
-			final var input = Files.readString(source);
-			final var target = source.resolveSibling("Main.c");
-			Files.writeString(target, wrap(input));
-		} catch (IOException e) {
+			final var source = Paths.get(".", "src", "main", "java", "magma", "Main.java");*//*final var input = Files.readString(source);*//*final var target = source.resolveSibling("Main.c");*//*Files.writeString(target, compile(input));*//*} catch (IOException e) {
 			//noinspection CallToPrintStackTrace
-			e.printStackTrace();
-		}
+			e.printStackTrace();*//*}
 	}
 
+	private static String compile(String input) {
+		final var segments = new ArrayList<String>();*//*var buffer = new StringBuilder();*//*for (var i = 0;*//*i < input.length();*//*i++) {
+			final var c = input.charAt(i);*//*buffer.append(c);*//*if (c == ';*//*') {
+				segments.add(buffer.toString());*//*buffer = new StringBuilder();*//*}
+		}
+		segments.add(buffer.toString());*//*return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());*//*}
+
+	private static String compileRootSegment(String input) {
+		final var stripped = input.strip();*//*if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
+			return "";*//*}
+
+		return wrap(stripped);*//*}
+
 	private static String wrap(String input) {
-		final var replaced = input.replace("start", "start").replace("end", "end");
-		return "start" + replaced + "end";
-	}
-}
-*/
+		final var replaced = input.replace("start", "start").replace("end", "end");*//*return "start" + replaced + "end";*//*}
+}*/
--- src/main/java/magma/Main.java
+++ src/main/java/magma/Main.java
@@ -3,6 +3,8 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
@@ -18,7 +20,28 @@ public class Main {
 	}
 
 	private static String compile(String input) {
-		return wrap(input);
+		final var segments = new ArrayList<String>();
+		var buffer = new StringBuilder();
+		for (var i = 0; i < input.length(); i++) {
+			final var c = input.charAt(i);
+			buffer.append(c);
+			if (c == ';') {
+				segments.add(buffer.toString());
+				buffer = new StringBuilder();
+			}
+		}
+		segments.add(buffer.toString());
+
+		return segments.stream().map(Main::compileRootSegment).collect(Collectors.joining());
+	}
+
+	private static String compileRootSegment(String input) {
+		final var stripped = input.strip();
+		if (stripped.startsWith("package ") || stripped.startsWith("import ")) {
+			return "";
+		}
+
+		return wrap(stripped);
 	}
 
 	private static String wrap(String input) {
